8b026c60099fd198dbf7b7dc316f61c98f66cf2e
Tommy Hofmann <thofma@gmail.com>
2015-05-19 14:25:26 +0200

    rudimentary verbose printing and assertion levels

--- src/hecke.jl
+++ src/hecke.jl
@@ -24,6 +24,52 @@ for i in names(Nemo)
   eval(Expr(:export, i))
 end
 
+export setverbose, getverbose, @vprint, @assertl, setassert, getassert
+
+VERBOSE_LEVEL = [1]
+
+function setverbose(i::Int)
+  VERBOSE_LEVEL[end] = i
+  if i == 1
+    eval(parse("macro vprint(x); :(print(\$x)); end;"))
+  else
+    eval(parse("macro vprint(x); nothing; end;"))
+  end
+end
+
+function getverbose()
+  return VERBOSE_LEVEL[end]
+end
+
+setverbose(VERBOSE_LEVEL[end])
+
+ASSERT_LEVEL = [0]
+
+function setassert(i::Int)
+  ASSERT_LEVEL[end] = i
+end
+
+function getassert()
+  return ASSERT_LEVEL[end]
+end
+
+function setassert(i::Int)
+  ASSERT_LEVEL[end] = i
+  eval(parse("macro assertl(lvl, ex, msgs...)
+  if lvl <= getassert()
+    msg = isempty(msgs) ? ex : msgs[1]                              
+    if !isempty(msgs) && isa(msg, Expr)                             
+      msg = :(string(\$(esc(msg))))                                
+    else
+      msg = string(msg)                                           
+    end                                                             
+    :(\$(esc(ex)) ? \$(nothing) : throw(AssertionError(\$msg)))        
+  end
+  end"))
+end
+
+setassert(ASSERT_LEVEL[end])
+
 include("Sparse.jl")
 include("BigComplex.jl")
 include("conjugates.jl")
@@ -33,5 +79,6 @@ include("MaximalOrderIdeals.jl")
 include("LinearAlgebra.jl")
 include("Clgp.jl")
 include("NfOrder.jl")
+
 
 end # module
