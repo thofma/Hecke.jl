84814ff9cfb897a6b6302c9002337c697ba7d231
Tommy Hofmann <thofma@gmail.com>
2015-08-26 08:40:01 +0200

    rigorous residue computation

--- src/NfMaximalOrder.jl
+++ src/NfMaximalOrder.jl
@@ -5,6 +5,7 @@ set_verbose_level(:NfMaximalOrder, 1)
 
 include("NfMaximalOrder/NfMaximalOrder.jl")
 include("NfMaximalOrder/NfMaximalOrderIdeal.jl")
+include("NfMaximalOrder/NfMaxOrdZeta.jl")
 include("NfMaximalOrder/NfMaximalOrderFracIdeal.jl")
 include("NfMaximalOrder/NfMaximalOrderClgp.jl")
 include("NfMaximalOrder/GenNfOrdUnit.jl")
