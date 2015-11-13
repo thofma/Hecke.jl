985413b959e49c925f12db16ef6719316bcf9b6e
Tommy Hofmann <thofma@gmail.com>
2015-11-13 15:31:04 +0100

    testing $ delimiter

--- doc/mathjaxhelper.js
+++ doc/mathjaxhelper.js
@@ -2,4 +2,7 @@ MathJax.Hub.Config({
     config: ["MMLorHTML.js"],
       jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
         extensions: ["MathMenu.js", "MathZoom.js"]
+  tex2jax: {
+        inlineMath: [ ['$','$'], ['\\(','\\)'] ]
+          }
 });
