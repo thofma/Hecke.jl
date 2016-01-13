70286ae11584114becc45fd9e41d8abc78f476d6
Tommy Hofmann <thofma@gmail.com>
2016-01-13 16:21:58 +0100

    Fix documentation

--- src/NfMaximalOrder/GenNfOrd.jl
+++ src/NfMaximalOrder/GenNfOrd.jl
@@ -829,7 +829,7 @@ doc"""
 > Compute the the conjugates of `x` as elements of type `arb` and `acb`
 > respectively. Recall that we order the complex conjugates
 > $\sigma_{r+1}(x),...,\sigma_{r+2s}(x)$ such that
-> $\sigma_{i}(x) = \overline{sigma_{i + s}(x)}$ for $r + 1 \leq i \leq r + s$.
+> $\sigma_{i}(x) = \overline{\sigma_{i + s}(x)}$ for $r + 1 \leq i \leq r + s$.
 >
 > Every entry `y` of the arrays returned satisfies `radius(y) < 2^abs_tol` or
 > `radius(real(y)) < 2^abs_tol`, `radius(imag(y)) < 2^abs_tol` respectively.
