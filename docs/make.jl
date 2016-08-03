b7ee11dfc6b1c2d942e9e07b6d9064d366c81201
Tommy Hofmann <thofma@gmail.com>
2016-08-03 20:42:42 +0200

    Another change in the build script

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,11 @@ using Documenter, Hecke
 makedocs(
     modules = Hecke,
     clean   = true,
+    doctest = false,
 )
 
-deploydocs(
-    deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
-    repo = "github.com/thofma/Hecke.jl.git",
-    julia = "0.4",
-)
+#deploydocs(
+#    deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+#    repo = "github.com/thofma/Hecke.jl.git",
+#    julia = "0.4",
+#)
