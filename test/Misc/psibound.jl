100753e9982edac53679ab9f07c7bf6f203b9ee2
Claus Fieker <fieker@mathematik.uni-kl.de>
2017-03-15 08:57:59 +0100

    change test to reflect changed type name

--- test/Misc/psibound.jl
+++ test/Misc/psibound.jl
@@ -1,7 +1,7 @@
 @testset "psibound" begin
 
   @testset "psi_lower" begin
-    sp = SetPrimes(2, 100);
+    sp = PrimesSet(2, 100);
     fb = []; for x=sp push!(fb, fmpz(x)); end;
     fb = FactorBase(fb)
     @test length(find(x->issmooth(fb, fmpz(x)), 1:256)) == psi_lower(255, 100)[1][end]
