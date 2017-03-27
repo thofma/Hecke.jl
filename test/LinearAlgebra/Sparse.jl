222f5b9d072b3ecbb15f49af43a01312f5951d58
Tommy Hofmann <thofma@gmail.com>
2017-03-27 21:24:43 +0200

    Update test code

--- test/LinearAlgebra/Sparse.jl
+++ test/LinearAlgebra/Sparse.jl
@@ -5,7 +5,7 @@
       r = 200
       c = 100
       A = Matrix(ZZ, r, c, rand([0,0,0,0,0,0,0,0,0,0,1], r, c))
-      As = Smat(A)
+      As = SMat(A)
       @test sub(hnf(A), 1:c, 1:c) == fmpz_mat(hnf(As))
     end
   end
