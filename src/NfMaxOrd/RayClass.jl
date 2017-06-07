4b27286d3756409141adb23b83f8922286e163ce
CarloSircana <sircana@mathematik.uni-kl.de>
2017-06-07 15:08:56 +0200

    Fixed the ray_class_group function

--- src/NfMaxOrd/RayClass.jl
+++ src/NfMaxOrd/RayClass.jl
@@ -1,5 +1,5 @@
 
-export iscoprime, ray_class_group 
+export iscoprime, ray_class_group, conductor, isconductor, norm_group
 
 #
 # Test if two ideals $I,J$ in a maximal order are coprime.
@@ -22,31 +22,48 @@ function iscoprime(I::NfMaxOrdIdl, J::NfMaxOrdIdl)
 end 
 
 #
-# Given two integral ideals $a$,$b$, computes a $\gamma$ such 
-# that $\gamma a$ is an integral ideal coprime to $b$
-# $\gamma$ is chosen randomly
-# The output is the ideal $a*\gamma$ and the element $\gamma$
-#
-function _coprime_ideal(a::NfMaxOrdIdl, b::NfMaxOrdIdl)
+# Modify the map of the class group so that the chosen representatives are coprime to m
+# 
+
+function _coprime_ideal(C::FinGenGrpAb, mC::Map, m::NfMaxOrdIdl)
  
-  O=parent(a).order
+  O=parent(m).order
   K=nf(O)
- 
-  if iscoprime(a,b)
-    return a,K(1)
+  L=NfMaxOrdIdl[]
+  for i=1:ngens(C)
+    a=mC(C[i])
+    if iscoprime(a,m)
+      push!(L,a)
+    else  
+      J=inv(a)
+      s=K(rand(J.num,5))//J.den  # Is the bound acceptable?
+      I=s*a
+      simplify(I)
+      I = num(I)
+      while !iscoprime(I,m)
+        s=K(rand(J.num,5))//J.den  
+        I=s*a
+        simplify(I)
+        I = num(I)
+      end
+      push!(L,I)
+    end
   end
-  J=inv(a)
-  s=K(rand(J.num,5))//J.den  # Is the bound acceptable?
-  I=s*a
-  simplify(I)
-  I = num(I)
-  while !iscoprime(I,b)
-    s=K(rand(J.num,5))//J.den  
-    I=s*a
-    simplify(I)
-    I = num(I)
+  
+  function exp(a::FinGenGrpAbElem)
+    I=ideal(O,1)
+    for i=1:ngens(C)
+      if Int(a.coeff[1,i])!= 0
+        I=I*(L[i]^(Int(a.coeff[1,i])))
+      end
+    end
+    return I
   end
-  return I,s
+  
+  mp=Hecke.MapClassGrp{typeof(C)}()
+  mp.header=Hecke.MapHeader(C,Hecke.NfMaxOrdIdlSet(O),exp, mC.header.preimage)
+  
+  return mp
  
 end 
 
@@ -54,27 +71,27 @@ end
 # Function that finds the generators of the infinite part
 #
 
-function _infinite_primes(O::NfMaxOrd, p::Array{Int,1}, m::NfMaxOrdIdl)
+function _infinite_primes(O::NfMaxOrd, p::Array{InfPlc,1}, m::NfMaxOrdIdl)
 
   K=O.nf
-  S = DiagonalGroup([2 for i=1:length(p)])
+  S=DiagonalGroup([2 for i=1:length(p)])
 
   function logS(x::Array{Int, 1})
     return S([x[i] > 0 ? 0 : 1 for i=1:length(x)])
   end
 
   s = typeof(S[1])[]
-  g = nf_elem[]
+  g = elem_type(O)[]
   u, mu = sub(S, s)
   b = 10
   cnt = 0
   while true
     a = rand(m, b)
-    emb=Hecke.signs(K(a))
-    t = logS([emb[i] for i in p])
+    emb=signs(K(a),p)
+    t=S([emb[x]==1 ? 0 : 1 for x in collect(keys(emb))])
     if !Hecke.haspreimage(mu, t)[1]
       push!(s, t)
-      push!(g, K(a))
+      push!(g, a)
       u, mu = sub(S, s)
       if order(u) == order(S)
         break
@@ -88,22 +105,28 @@ function _infinite_primes(O::NfMaxOrd, p::Array{Int,1}, m::NfMaxOrdIdl)
     end
   end
   hS = Hecke.FinGenGrpAbMap(S, S, vcat([x.coeff for x=s]))   # Change of coordinates so that the canonical basis elements are mapped to the elements found above
-  r = nf_elem[]
+  r = elem_type(O)[]
   for i=1:length(p)
     y = haspreimage(hS,S[i])[2]
     push!(r, prod([g[i]^Int(y[i]) for i=1:length(p)]))
   end
   
   function exp(A::FinGenGrpAbElem)
-    return prod([r[i]^(Int(A.coeff()))])
+    s=O(1)
+    for i=1:length(p)
+      if Int(A.coeff[1,i]) == 1
+        s=s*r[i]
+      end 
+    end
+    return s
   end 
 
   function log(B::nf_elem)
-    d=Hecke.signs(B)
-    return logS([d[i] for i in p])
+    emb=Hecke.signs(B,p)
+    return S([emb[x]==1 ? 0 : 1 for x in collect(keys(emb))])
   end 
   
-  return S, log,exp
+  return S,log,exp
 
 end
 
@@ -143,26 +166,28 @@ doc"""
   from the group to the ideals of $L$
 
 """
-function ray_class_group(m::NfMaxOrdIdl, primes=[])
+function ray_class_group(m::NfMaxOrdIdl, primes::Array{InfPlc,1}=InfPlc[])
 
 #
 # We compute the group using the sequence U -> (O/m)^* _> Cl^m -> Cl -> 1
 # First of all, we compute all these groups with their own maps
-#
+#  
   O=parent(m).order
   K=nf(O)
+  
 
   C, mC = class_group(O)
+  mC=_coprime_ideal(C,mC,m)
   U, mU = unit_group(O)
   M, pi= quo(O,m)
   G, mG=unit_group(M)
-  if !isempty(primes)
-    H,lH,eH=_infinite_primes(O,primes,m)
+  
+  p = [ x for x in primes if isreal(x) ]
+  if !isempty(p)
+    H,lH,eH=_infinite_primes(O,p,m)
     T=G
     G=direct_product(G,H)
   end
-
-
 
 #
 # We start to construct the relation matrix
@@ -183,7 +208,7 @@ function ray_class_group(m::NfMaxOrdIdl, primes=[])
     if !isempty(primes)
       a=hcat(a, (lH(K(u))).coeff)
     end 
-    for j=1:cols(RG)
+    for j=1:ngens(G)
       B[i+rows(RC)+rows(RG),j]=a[1,j]
     end
   end 
@@ -192,26 +217,20 @@ function ray_class_group(m::NfMaxOrdIdl, primes=[])
 # We compute the relation between generators of Cl and (O/m)^* in Cl^m
 #
 
-  P=[K(1) for i=1: ngens(C)]
-
   for i=1: ngens(C)
-    if order(C[i])==1
-      y=K(1)
-    else 
-      x, P[i]=_coprime_ideal(mC(C[i]), m)
-      x=x^(Int(order(C[i])))
-      y=Hecke.principal_gen(x)
+    if order(C[i])!=1
+      y=Hecke.principal_gen((mC(C[i]))^(Int(order(C[i]))))
+      b=(mG\(pi(y))).coeff
+      if primes != []
+        b=hcat(b, (lH(K(y))).coeff)
+      end 
+      for j=1: ngens(G)
+        B[i,j]=-b[1,j]
+      end 
     end
-    b=(mG\(pi(O(y)))).coeff
-    if primes != []
-      b=hcat(b, (lH(K(y))).coeff)
-    end 
-    for j=1: ngens(G)
-      B[i,j]=-b[1,j]
-    end 
   end
 
-  R=hcat(B,A)
+  R=hcat(A,B)
   X=AbelianGroup(R)
 
 #
@@ -223,29 +242,19 @@ function ray_class_group(m::NfMaxOrdIdl, primes=[])
     if isone(J)
       return X([0 for i=1:ngens(X)])
     else
-    L=mC\J
-    t=K(1)
-    s=ideal(O,1)
-    for i=1:ngens(C)
-      if Int(L.coeff[1,i])!=0
-        t=t*P[i]^(Int(L.coeff[1,i]))
-        s=s*mC(C[i])^(Int(L.coeff[1,i]))
-      end
-    end 
-    I= J // s
-    simplify(I)
-    gamma=Hecke.principal_gen(I.num)
-    alpha=K(gamma)* (t^(-1))
-    y1=mG\(pi(O(num(alpha))))
-    y2=mG\(pi(O(den(alpha))))
-    y=y1.coeff - (y2.coeff)
-    if primes!=[]
-      z=lH(alpha)
-      y=vcat(y, z.coeff)
-    end 
-    return X(hcat(y, L.coeff))
-  end
-end 
+      L=mC\J
+      s=mC(L)
+      I= J // s
+      simplify(I)
+      gamma=Hecke.principal_gen(I.num)
+      y=((mG\(pi(gamma)))-(mG\(pi(O(I.den))))).coeff
+      if primes!=[]
+        z=lH(K(gamma))
+        y=hcat(y, z.coeff)
+      end 
+      return X(hcat(L.coeff,y))
+    end
+  end 
 
 #
 # Exp map
@@ -256,7 +265,7 @@ end
     b=C([a.coeff[1,i] for i=1:ngens(C)])
     if isempty(primes)
       c=G([a.coeff[1,i] for i=ngens(C)+1:ngens(X)])
-      return mC(b)*(pi\(mG(c))) 
+      return mC(b)*(pi\(mG(c)))
     else 
       c=T([a.coeff[1,i] for i=ngens(C)+1:ngens(T)+ngens(C)])
       d=H([a.coeff[1,i] for i=ngens(T)+ngens(C)+1: ngens(X)])
@@ -268,10 +277,11 @@ end
       else 
         correction=O(1)
         for i=1:ngens(H)
-          if d.coeff!=0
+          if d.coeff[1,i]==1
             correction=correction*eH(H[i])
           end
         end
+        println(signs(K(correction),primes))
         while vect!=d.coeff
           el=el+correction
           vect=(lH(K(el))).coeff
@@ -284,18 +294,145 @@ end
   mp=MapRayClassGrp{typeof(X)}()
   mp.header = Hecke.MapHeader(X, Hecke.NfMaxOrdIdlSet(O), expo, disclog)
   mp.modulus_fin=m
+  mp.modulus_inf=p
  
   return X, mp
   
 end
 
 
+Hecke.elem_type(A::Hecke.NfMaxOrdIdlSet) = NfMaxOrdIdl
+
 
 doc"""
 ***
-  function norm_group(f::Nemo.GenPoly, R::FinGenGrpAb, mR::MapRayClassGrp) -> Hecke.FinGenGrpAb, Hecke.FinGenGrpAbMap
 
- > Compute the subgroup of the Ray Class Group $R$ given by the norm of the abelian extension generated by the roots of $f$ 
+  function conductor(S::FinGenGrpAb, mS::FinGenGrpAbMap, R::FinGenGrpAb, mR::MapRayClassGrp)
+
+  Return the conductor of the congruence subgroup S, mS of the Ray Class Group R,mR 
+  
+***
+"""
+
+
+function conductor(S::FinGenGrpAb, mS::FinGenGrpAbMap, R::FinGenGrpAb, mR::MapRayClassGrp)
+
+
+  cond=mR.modulus_fin
+  O=parent(cond).order
+
+  L=factor(cond)
+  divisors=collect(keys(L))
+
+  E=divexact(order(R),order(S))
+  Sgens=[image(mR*mS,S[i]) for i=1:ngens(S)]
+  candidate=cond
+  
+  # Test the finite primes.
+  
+  while !isempty(divisors)
+    p=divisors[length(divisors)]
+    for j=1:L[p]
+      candidate=cond*inv(p)
+      simplify(candidate)
+      candidate=candidate.num
+      r, mr=ray_class_group(candidate,mR.modulus_inf)
+      T, mT=quo(r, [mr\(Sgens[i]) for i=1:ngens(S)])
+      E1=order(T)
+      if E==E1
+        cond=candidate 
+      else
+        break
+      end
+    end
+    pop!(divisors)
+  end
+
+  # Test the infinite primes.
+  
+  if !isempty(mR.modulus_inf)
+    candidate_inf=deepcopy(mR.modulus_inf)
+    cond_inf=deepcopy(mR.modulus_inf)
+    for i=1:length(infinite_primes)
+      candidate_inf=[x for x in mR.modulus_inf for x !=mR.modulus_inf[i]]
+      r,mr=ray_class_group(cond,candidate_inf)
+      T,mT=quo(r, [mr\(Rgens[i]) for i=1:ngens(R)])
+      E1=order(T)
+      if E==E1
+        cond_inf=candidate_inf
+      else 
+        candidate_inf=cond_inf
+      end
+    end
+    return cond, cond_inf
+  end
+  return cond
+  
+end 
+
+
+doc"""
+***
+
+  function isconductor(R::FinGenGrpAb, mR::Map, m::NfMaxOrdIdl, infinite_primes::Array{InfPlc,1}=[])
+
+  Check if $m$ is the conductor of the congruence subgroup $S,mS$ of the Ray Class group R,mR
+  
+***
+"""
+
+
+function isconductor(S::FinGenGrpAb, mS::FinGenGrpAbMap, R::FinGenGrpAb, mR::MapRayClassGrp, m::NfMaxOrdIdl, infinite_places::Array{InfPlc,1}=InfPlc[])
+
+  O=parent(m).order
+
+  L=factor(m)
+  divisors=collect(keys(L))
+
+  E=divexact(order(R),order(S))
+  Sgens=[image(mR*mS,S[i]) for i=1:ngens(S)]
+
+  
+  # Check the finite primes 
+  
+  for p in divisors
+    candidate=m*inv(p)
+    simplify(candidate)
+    candidate=candidate.num
+    r, mr=ray_class_group(candidate, infinite_places)
+    T, mT=quo(r, [mr\(Sgens[i]) for i=1:ngens(S)])
+    if E==order(T)
+      return false
+    end
+  end
+
+  # Check the infinite primes. To be changed.
+  
+  if !isempty(infinite_places)
+    candidate_inf=deepcopy(infinite_places)
+    for i=1:length(infinite_places)
+      deleteat!(candidate_inf,i)
+      r,mr=ray_class_group(m,candidate_inf)
+      T,mT=quo(r, [mr\(Sgens[i]) for i=1:ngens(S)])
+      if E==order(T)
+        return false
+      else 
+        candidate_inf=deepcopy(infinite_places)
+      end
+    end
+  end
+  return true
+  
+end 
+
+
+
+doc"""
+***
+  function norm_group2(f::Nemo.GenPoly, R::FinGenGrpAb, mR::MapRayClassGrp) -> Hecke.FinGenGrpAb, Hecke.FinGenGrpAbMap
+
+ > Computes the subgroup of the Ray Class Group $R$ given by the norm of the extension generated by the roots of $f$ 
+   Under GRH, it returns an error message if the extension is non-abelian
 
 ***
 """
@@ -303,8 +440,15 @@ doc"""
 function norm_group(f::Nemo.PolyElem, R::FinGenGrpAb, mR::Hecke.MapRayClassGrp)
   
   
-  O=modulus.parent.order
+  O=mR.modulus_fin.parent.order
   K=O.nf
+  
+  signs=signature(norm(f))
+
+  if signs[1] !=0 && signs[2] !=0
+    error("The extension is not normal")
+  end
+
   d=discriminant(f)
   N=num(norm(K(d)))
   N1=fmpz(norm(mR.modulus_fin))
@@ -323,13 +467,20 @@ function norm_group(f::Nemo.PolyElem, R::FinGenGrpAb, mR::Hecke.MapRayClassGrp)
   determinant=abs(det(M))
   listprimes=typeof(R[1])[]  
   new_mat=M
+
+  B=log(abs(discriminant(O)))*degree(f)+log(N)
+  B=4*B+2.5*degree(f)*degree(O)+5
+  B=B^2
   
   #
   # Adding small primes until they generate the norm group
   #
   
-  while determinant!= n
+  while determinant!= n 
     p=next_prime(p)
+    if p>B
+      error("The extension is not abelian!") #Bach bound says that the norm group must be generated by primes $\leq B$
+    end
     if !divisible(N,p) && !divisible(N1,p) 
       L=prime_decomposition(O,p)
       for i=1:length(L)
@@ -365,77 +516,6 @@ function norm_group(f::Nemo.PolyElem, R::FinGenGrpAb, mR::Hecke.MapRayClassGrp)
   end    
 
 end
-
-
-Hecke.elem_type(A::Hecke.NfMaxOrdIdlSet) = NfMaxOrdIdl
-
-
-doc"""
-***
-
-  function conductor(R::FinGenGrpAb, mR::Map, m::NfMaxOrdIdl, infinite_primes::Array{Int,1}=[])
-
-  Return the conductor of the congruence subgroup given by R 
-  
-***
-"""
-
-
-function conductor(R::FinGenGrpAb, mR::Map, m::NfMaxOrdIdl, infinite_primes::Array{Int,1}=[])
-
-  O=parent(m).order
-
-  L=factor(m)
-  divisors=collect(keys(L))
-
-  cond=m
-  E=order(R)
-  Rgens=[image(mR,R[i]) for i=1:ngens(R)]
-  
-  # I check the finite primes 
-  
-  while !isempty(divisors)
-    p=divisors[length(divisors)]
-    for j=1:L[p]
-      candidate=m*inv(p)
-      simplify(candidate)
-      candidate=candidate.num
-      S, mS=ray_class_group(candidate)
-      T, mT=quo(S, [mS\(Rgens[i]) for i=1:ngens(R)])
-      E1=order(T)
-      if E==E1
-        cond=candidate 
-      else
-        pop!(divisors)
-        break
-      end
-    end
-    pop!(divisors)
-  end
-
-  # I check the infinite primes. To be changed.
-  
-  if !isempty(infinite_primes)
-    candidate_inf=infinite_primes
-    cond_inf=infinite_primes
-    for i=1:length(infinite_primes)
-      candidate_inf=deleteat!(candidate_inf,i)
-      R,mR=ray_class_group(cond,candidate_inf)
-      T,mT=quo(S, [mS\(Rgens[i]) for i=1:ngens(R)])
-      E1=order(T)
-      if E==E1
-        cond_inf=candidate_inf
-      else 
-        candidate_inf=cond_inf
-      end
-    end
-    return cond, cond_inf
-  end
-  return cond
-  
-end 
-
-
 
 
 
