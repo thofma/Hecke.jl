003e99bf39cb9a02e8f7bd21961c8226097ac192
Johannes Schmitt <jschmitt@posteo.eu>
2017-08-09 18:14:41 +0200

    More "assure" functions for NfRelOrd.

--- src/NfRel/NfRelOrd.jl
+++ src/NfRel/NfRelOrd.jl
@@ -3,7 +3,7 @@
 
 type NfRelOrd{T, S} <: Ring
   nf::NfRel{T}
-  basis_nf::Vector{NfRel{T}}
+  basis_nf::Vector{NfRelElem{T}}
   basis_mat::GenMat{T}
   basis_mat_inv::GenMat{T}
   basis_pmat::PMat{T, S}
@@ -14,8 +14,6 @@ type NfRelOrd{T, S} <: Ring
     z = new()
     z.nf = K
     z.basis_pmat = M
-    z.basis_mat = M.matrix
-    z.basis_mat_inv = inv(M.matrix)
     return z
   end
   
@@ -23,7 +21,6 @@ type NfRelOrd{T, S} <: Ring
     z = new()
     z.nf = K
     z.basis_mat = M
-    z.basis_mat_inv = inv(M)
     z.basis_pmat = pseudo_matrix(M)
     return z
   end
@@ -49,22 +46,76 @@ nf(O::NfRelOrd) = O.nf
 #
 ################################################################################
 
-function assure_has_pseudo_basis(O::NfRelOrd)
+function assure_has_basis_pmat{T, S}(O::NfRelOrd{T, S})
+  if isdefined(O, :basis_pmat)
+    return nothing
+  end
+  if !isdefined(O, :pseudo_basis)
+    error("No pseudo_basis and no basis_pmat defined.")
+  end
+  pb = pseudo_basis(O)
+  L = nf(O)
+  M = MatrixSpace(base_ring(L), degree(O), degree(O))()
+  C = Vector{S}()
+  for i = 1:degree(O)
+    elem_to_mat_row!(M, i, L(pb[i][1]))
+    push!(C, pb[i][2])
+  end
+  O.basis_pmat = PseudoMatrix(M, C)
+  return nothing
+end
+
+function assure_has_pseudo_basis{T, S}(O::NfRelOrd{T, S})
   if isdefined(O, :pseudo_basis)
     return nothing
   end
-  pseudo_basis = Vector{Tuple{NfRelOrdElem, NfOrdFracIdl}}()
+  if !isdefined(O, :basis_pmat)
+    error("No pseudo_basis and no basis_pmat defined.")
+  end
+  P = basis_pmat(O)
+  L = nf(O)
+  pseudo_basis = Vector{Tuple{NfRelOrdElem{T}, S}}()
   for i = 1:degree(O)
-    a = elem_from_mat_row(nf(O), basis_mat(O), i)
-    push!(pseudo_basis, (O(a), basis_pmat(O).coeffs[i]))
+    a = elem_from_mat_row(L, P.matrix, i)
+    push!(pseudo_basis, (O(a), P.coeffs[i]))
   end
   O.pseudo_basis = pseudo_basis
   return nothing
 end
 
+function assure_has_basis_nf{T, S}(O::NfRelOrd{T, S})
+  if isdefined(O, :basis_nf)
+    return nothing
+  end
+  L = nf(O)
+  pb = pseudo_basis(O)
+  basis_nf = Vector{NfRelElem{T}}()
+  for i = 1:degree(O)
+    push!(basis_nf, L(pb[i][1]))
+  end
+  O.basis_nf = basis_nf
+  return nothing
+end
+
+function assure_has_basis_mat(O::NfRelOrd)
+  if isdefined(O, :basis_mat)
+    return nothing
+  end
+  O.basis_mat = basis_pmat(O).matrix
+  return nothing
+end
+
+function assure_has_basis_mat_inv(O::NfRelOrd)
+  if isdefined(O, :basis_mat_inv)
+    return nothing
+  end
+  O.basis_mat_inv = inv(basis_mat(O))
+  return nothing
+end
+
 ################################################################################
 #
-#  Pseudo basis
+#  Pseudo basis / basis pseudo-matrix
 #
 ################################################################################
 
@@ -77,13 +128,8 @@ function pseudo_basis{T}(O::NfRelOrd, copy::Type{Val{T}} = Val{true})
   end
 end
 
-################################################################################
-#
-#  (Inverse) basis matrix
-#
-################################################################################
-
 function basis_pmat{T}(O::NfRelOrd, copy::Type{Val{T}} = Val{true})
+  assure_has_basis_pmat(O)
   if copy == Val{true}
     return deepcopy(O.basis_pmat)
   else
@@ -91,7 +137,23 @@ function basis_pmat{T}(O::NfRelOrd, copy::Type{Val{T}} = Val{true})
   end
 end
 
+################################################################################
+#
+#  Basis / (inverse) basis matrix
+#
+################################################################################
+
+function basis_nf{T}(O::NfRelOrd, copy::Type{Val{T}} = Val{true})
+  assure_has_basis_nf(O)
+  if copy == Val{true}
+    return deepcopy(O.basis_nf)
+  else
+    return O.basis_nf
+  end
+end
+
 function basis_mat{T}(O::NfRelOrd, copy::Type{Val{T}} = Val{true})
+  assure_has_basis_mat(O)
   if copy == Val{true}
     return deepcopy(O.basis_mat)
   else
@@ -100,6 +162,7 @@ function basis_mat{T}(O::NfRelOrd, copy::Type{Val{T}} = Val{true})
 end
 
 function basis_mat_inv{T}(O::NfRelOrd, copy::Type{Val{T}} = Val{true})
+  assure_has_basis_mat_inv(O)
   if copy == Val{true}
     return deepcopy(O.basis_mat_inv)
   else
@@ -154,9 +217,14 @@ end
 #
 ################################################################################
 
-function Order{T}(L::NfRel{T}, M::GenMat{T})
+function Order(L::NfRel{nf_elem}, M::GenMat{nf_elem})
   # checks
-  return NfRelOrd{T, NfOrdFracIdl}(L, M)
+  return NfRelOrd{nf_elem, NfOrdFracIdl}(L, M)
+end
+
+function Order{T}(L::NfRel{NfRelElem{T}}, M::GenMat{NfRelElem{T}})
+  # checks
+  return NfRelOrd{NfRelElem{T}, NfRelOrdFracIdl{T}}(L, M)
 end
 
 function Order{T, S}(L::NfRel{T}, M::PMat{T, S})
