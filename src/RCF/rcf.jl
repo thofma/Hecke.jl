0c1c9189a3ed71e26cb1b8995a5b0271b8abbef8
CarloSircana <sircana@mathematik.uni-kl.de>
2017-09-19 14:15:23 +0200

    GenRes and NmodRing changed

--- src/RCF/rcf.jl
+++ src/RCF/rcf.jl
@@ -753,7 +753,7 @@ function extend_easy(f::Hecke.NfOrdToFqNmodMor, K::AnticNumberField)
 
   p = characteristic(z.header.codomain)
   y = f(NfOrdElem(domain(f), gen(K)))
-  Ft, t = PolynomialRing(ResidueRing(FlintZZ, p))
+  Ft, t = PolynomialRing(ResidueRing(FlintZZ, UInt(p)))
   K = number_field(domain(f))
 #  g = gcd(Ft(K.pol), Ft(K(f.P.gen_two)))
 #it would be cool to assert that g is the poly defining the codomain
