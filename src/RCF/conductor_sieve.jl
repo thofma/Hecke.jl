9c81196ad2ccc2aa48e5ba8e94f4e6c23595e19f
CarloSircana <sircana@mathematik.uni-kl.de>
2017-10-25 11:30:46 +0200

    minor changes

--- src/RCF/conductor_sieve.jl
+++ src/RCF/conductor_sieve.jl
@@ -154,9 +154,11 @@ function quadratic_normal_extensions(O::NfOrd, bound::fmpz)
     gens=[ rand(Aut) for i=1:b ]
     Aut1=Hecke._closing_under_generators_dimino(gens, (x, y) -> [ g for g in Aut if g(a) == (x*y)(a)][1], Identity, (x,y) -> x(a) == y(a))
   end
+  #Getting conductors
   conductors=tame_conductors_degree_2(O,bound)
   @vprint :QuadraticExt "Number of conductors: $(length(conductors)) \n"
   fields=[]
+  #Now, the big loop
   for k in conductors
     println("Conductor: $k ")
     @vtime :QuadraticExt 1 r,mr=tommy_ray_class_group(O,2,k)
@@ -167,9 +169,11 @@ function quadratic_normal_extensions(O::NfOrd, bound::fmpz)
     for s in ls
       C=ray_class_field(mr*inv(s[2]))
       println("\n Computing fields")
-      if conductor_min(C)==k
+      if Hecke._is_conductor_min_tame_normal(C, k)
+        @assert Hecke.conductor_min(C)==k
         @vtime :QuadraticExt 1 push!(fields,number_field(C))
-
+      else 
+        @assert  Hecke.conductor_min(C)!=k
       end
     end
     println("\n")
@@ -178,11 +182,105 @@ function quadratic_normal_extensions(O::NfOrd, bound::fmpz)
 
 end
 
-function conductors(O::NfOrd, ram_primes::Array{Int,1}, n::Int, bound::Int)
 
-  if isprime(fmpz(n))
-    return conductors_cyclic(O,ram_primes,n, bound)
+#
+# General case
+#
+
+function squarefree_for_conductors(O::NfOrd, n::Int, deg::Int ; coprime_to::Array{fmpz,1}=fmpz[])
+  
+  sqf= trues(n)
+  primes= trues(n)
+  
+  #remove primes that can be wildly ramified or
+  #that are ramified in the base field
+  for x in coprime_to
+    t=x
+    while t<= n
+      sqf[Int(t)]=false
+      primes[Int(t)]=false
+      t+=x
+    end
   end
+  
+  #sieving procedure
+  i=3
+  if !(2 in coprime_to)
+    dt=prime_decomposition_type(O,i)
+    if gcd(2^dt[1][1], deg)==1
+      j=2
+      while j<=n
+        sqf[j]=false
+        primes[j]=false
+        j+=2
+      end
+    else 
+      i=2
+      s=4
+      while s<=n
+        primes[s]=false
+        s+=2
+      end
+      s=4
+      while s<=n
+        sqf[s]=false
+        s+=4
+      end
+    end
+  end
+  b=sqrt(n)
+  while i<=b
+    if primes[i]
+      dt=prime_decomposition_type(O,i)
+      if gcd(deg,i^dt[1][1])==1
+        primes[i]=false
+        sqf[i]=false
+        j=3*i
+        s=2*i
+        while j<= n
+         primes[j]=false
+         sqf[j]=false
+         j+=s
+        end
+      else 
+        j=3*i
+        s=2*i
+        while j<= n
+          primes[j]=false
+          j+=s
+        end
+        j=i^2
+        t=2*j
+        while t<= n
+          sqf[t]=false
+          t+=j
+        end
+      end
+    end
+    i+=2
+  end
+  while i<=n
+    if primes[i]
+      dt=prime_decomposition_type(O,i)
+      if gcd(deg,i^dt[1][1])==1
+        sqf[i]=false
+        j=3*i
+        s=2*i
+        while j<= n
+         sqf[j]=false
+         j+=s
+        end
+      end
+    end
+    i+=2
+  end
+  return Int[i for i=1:length(sqf) if sqf[i]]
+  
+end
+
+function conductors_tame(O::NfOrd, n::Int, bound::Int)
+
+  #TODO: Add the p=2 case and optimize it
   
   #
   #  First, conductors coprime to the ramified primes and to the 
@@ -191,114 +289,44 @@ function conductors(O::NfOrd, ram_primes::Array{Int,1}, n::Int, bound::Int)
 
   wild_ram=collect(keys(factor(fmpz(n)).fac))
   K=nf(O)
-  inf_plc=InfPlc[]
-  if mod(n,2)==0
-    inf_plc=real_places(K)
-  end
+  
+  ram_primes=collect(keys(factor(abs(O.disc)).fac))
+  filter!(x -> !divisible(n,x) ,ram_primes)
+  sort!(ram_primes)
+  
   b1=Int(root(fmpz(bound),Int(degree(O)*(minimum(wild_ram)-1)))) #Maybe it is n-1 instead of minimun(wild_ram)-1. Check.
   coprime_to=cat(1,ram_primes, wild_ram)
-  list= _squarefree_up_to_bitarray(b1, coprime_to=coprime_to)
-  prime_list=primes_up_to(b1)
+  list= _squarefree_for_conductors(O, b1, n, coprime_to=coprime_to)
 
-  for p in prime_list
-    if (p in coprime_to) || gcd(fmpz(p-1),n)!=1
-      continue
+  extra_list=Tuple{Int, Int}[(1,1)]
+  for q in ram_primes
+    tr=prime_decomposition_type(O,Int(q))
+    f=tr[1][1]
+    nq=Int(q)^f
+    if nq> bound
+      break
     end
-    lp=prime_decomposition_type(O,p)
-    f=lp[1][1]
-    if gcd(n,p^f-1)==1
-      s=p
-      while s<=b1
-        list[s]=false
-        s+=p
-      end 
-    end
-  end
-  
-  candidates=[]
-  for i=1:b1
-    if list[i]
-      fac=keys(factor(fmpz(i)).fac)
-      el=Dict{NfOrdIdl, Int}()
-      for p in fac
-        lp=prime_decomposition(O,p)
-        for x in lp
-          el[x[1]]=1
-        end
-      end
-      D=Dict{NfOrdIdl,Int}
-      push!(candidates, [el,D, i^degree(O)])    
-    end
-  end
-  
-  #
-  #  We now need to take care of the ramified primes and of wild ramification
-  #
-
-  for p in ram_primes
-    if !(p in wild_ram)
-      lp=prime_decomposition_type(O,p)
-      f=lp[1][1]
-      if gcd(n,p^f-1)==1
+    l=length(extra_list)
+    for i=1:l
+      n=extra_list[i][2]*nq
+      if n> bound
         continue
       end
-      lp=prime_decomposition(O,p)
-      l1=length(candidates)
-      for i=1:l1
-        ex=p^(divexact(degree(O),lp[1][2]))
-        if candidates[i][3]*ex>bound
-          continue
-        end
-        x=copy(candidates[i][1])
-        for j=1:length(lp)
-          x[lp[j][1]]=1
-        end
-        push!(candidates, [x, Dict{NfOrdIdl, Int},candidates[i][3]*ex])
-      end
-    end  
-  end
-
-  for p in wild_ram
-    if p in ram_primes
-      lp=prime_decomposition(O,p)
-      f=divexact(length(lp),lp[1][2])
-      if gcd(n,p^f-1)==1
-        l1=length(candidates)
-        for i=1:l1
-          ex=p^(divexact(degree(O),lp[1][2]))
-          i=0
-          while candidates[i][3]*ex^i<=bound
-            x=copy(candidates[i][1])
-          for j=1:length(lp)
-            x[lp[j][1]]=1
-          end
-          push!(candidates, [x, Dict{NfOrdIdl, Int},candidates[i][3]*ex])
-          end
-          x=copy(candidates[i][1])
-          for j=1:length(lp)
-            x[lp[j][1]]=1
-          end
-          push!(candidates, [x, Dict{NfOrdIdl, Int},candidates[i][3]*ex])
-        end
-      else
-        l1=length(candidates)
-        for i=1:l1
-          ex=p^(divexact(degree(O),lp[1][2]))
-          if candidates[i][3]*ex>bound
-            continue
-          end
-          x=copy(candidates[i][1])
-          for j=1:length(lp)
-            x[lp[j][1]]=1
-          end
-          push!(candidates, [x, Dict{NfOrdIdl, Int},candidates[i][3]*ex])
-        end
-      end
-    else
-      
+      push!(extra_list, (extra_list[i][1]*q, n))
     end
   end
-
+  deleteat!(extra_list,1)
+  
+  l=length(cond_list)
+  for (el,norm) in extra_list
+    for i=1:l
+      if list[i]^n*norm>bound
+        continue
+      end
+      push!(list, list[i]*el)
+    end
+  end
+  return list
 end
 
 ################################################################################
