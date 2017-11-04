bc663180c343c7ac251fe5dd7933ea73c1850e7b
Johannes Schmitt <jschmitt@posteo.eu>
2017-11-04 13:28:37 +0100

    More tests for relative ideals.

--- test/NfRel/NfRelOrdIdl.jl
+++ test/NfRel/NfRelOrdIdl.jl
@@ -13,14 +13,22 @@
   PM1 = pseudo_hnf(Hecke.PseudoMatrix(matrix(K, [3 0; 0 3]), coeff_ideals), :lowerleft, true)
   PM2 = pseudo_hnf(Hecke.PseudoMatrix(matrix(K, [6 0; 0 6]), coeff_ideals), :lowerleft, true)
   PM3 = pseudo_hnf(Hecke.PseudoMatrix(matrix(K, [9 0; 0 9]), coeff_ideals), :lowerleft, true)
+  I = ideal(OL, PM)
+  I1 = frac_ideal(OL, I, OK(1))
   A = ideal(OL, PM1)
+  A1 = frac_ideal(OL, A, OK(1))
   B = ideal(OL, PM2)
   C = ideal(OL, PM3)
+  C1 = frac_ideal(OL, C, OK(1))
 
   @test A == B + C
   @test B == intersection(A, B)
   @test K(2)*C == A*B
-  @test inv(C)*C == ideal(OL, PM)
+  @test inv(C)*C1 == I1
   @test norm(A) == OK(9)*OK
+  @test norm(I) == OK(1)*OK
+  D = divexact(C, B)
+  D.num = K(2)*num(D)
+  @test D == A1
 end
 
