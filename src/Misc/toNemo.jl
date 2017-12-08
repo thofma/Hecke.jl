5c832edb979b69276992573357b0d5c8643d00cb
Claus Fieker <fieker@mathematik.uni-kl.de>
2017-12-08 13:57:27 +0100

    toNemo for Dict{nf_elem, fmpz} - > for FacElem

--- src/Misc/toNemo.jl
+++ src/Misc/toNemo.jl
@@ -1,8 +1,24 @@
 ################################################################################
 #
-#  Conversion to Nemo/hecke for storage
+#  Conversion to Nemo/Hecke for storage
 #
 ################################################################################
+
+function toNemo(f::IOStream, a::Dict{T, S}; name::String="R") where {T, S}
+  print(f, name, " = Dict{$T,$S}(\n")
+  k = collect(keys(a))
+  for i=1:length(k)-1
+    print(f, k[i], " => ", a[k[i]], ", \n")
+  end
+  print(f, k[end], " => ", a[k[end]], ");\n")
+end
+
+
+function toNemo(s::String, a::Dict; name::String="R", mode::String ="w") 
+  f = open(s, mode)
+  toNemo(f, a, name = name)
+  close(f)
+end
 
 function toNemo(f::IOStream, a::Array{T, 1}; name::String="R") where T
   print(f, name, " = [\n")
