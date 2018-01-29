9dee7f8dadbfe7faa3a44b282f93c7c7aec7a280
Johannes Schmitt <jschmitt@posteo.eu>
2018-01-29 21:16:38 +0100

    Make use of solve_ut and use ResidueRing for fmpz too.

--- src/AlgAss/AlgAss.jl
+++ src/AlgAss/AlgAss.jl
@@ -36,7 +36,7 @@ function find_one(A::AlgAss)
   rref!(Mc)
   @assert !iszero(Mc[n, n])
   n != 1 && @assert iszero(Mc[n + 1, n + 1])
-  cc = solve(sub(Mc, 1:n, 1:n), sub(Mc, 1:n, (n + 1):(n + 1)))
+  cc = solve_ut(sub(Mc, 1:n, 1:n), sub(Mc, 1:n, (n + 1):(n + 1)))
   one = [ cc[i, 1] for i = 1:n ]
   return one
 end
@@ -101,13 +101,8 @@ function AlgAss(O::NfOrd, I::NfOrdIdl, p::Union{Integer, fmpz})
   n = degree(O)
   BO = Hecke.basis(O)
 
-  pisfmpz = typeof(p) == fmpz
-  if pisfmpz
-    # Can't use ResidueRing for fmpz since there is no lufact for Generic.Res
-    Fp = FiniteField(p, 1, "")[1]
-  else
-    Fp = ResidueRing(FlintZZ, p)
-  end
+  pisfmpz = (typeof(p) == fmpz)
+  Fp = ResidueRing(FlintZZ, p)
   BOmod = [ mod(v, I) for v in BO ]
   B = zero_matrix(Fp, n, n)
   for i = 1:n
@@ -116,41 +111,38 @@ function AlgAss(O::NfOrd, I::NfOrdIdl, p::Union{Integer, fmpz})
       B[i, j] = Fp(b[j])
     end
   end
-  r = rref!(B)
+  if pisfmpz
+    r, B = _rref(B)
+  else
+    r = rref!(B)
+  end
   r == 0 && error("Cannot construct zero dimensional algebra.")
   b = Vector{fmpz}(n)
   basis = Vector{NfOrdElem}(r)
   for i = 1:r
     for j = 1:n
-      if pisfmpz
-        b[j] = coeff(B[i, j], 0)
-      else
-        b[j] = fmpz(B[i, j])
-      end
+      b[j] = fmpz(B[i, j])
     end
     basis[i] = O(b)
   end
 
-  _, p, L, U = lufact(transpose(B))
-  U = _remove_non_pivot_cols(U, r)
+  if pisfmpz
+    _, p, L, U = _lufact(transpose(B))
+  else
+    _, p, L, U = lufact(transpose(B))
+  end
   mult_table = Array{elem_type(Fp), 3}(r, r, r)
   d = zero_matrix(Fp, n, 1)
-  dd = zero_matrix(Fp, r, 1)
-
   for i = 1:r
     for j = 1:r
       c = elem_in_basis(mod(basis[i]*basis[j], I))
-      @assert dot(c, BOmod) == mod(basis[i]*basis[j], I)
       for k = 1:n
         d[p[k], 1] = c[k]
       end
-      d = solve(L, d)
+      d = solve_lt(L, d)
+      d = solve_ut(U, d)
       for k = 1:r
-        dd[k, 1] = d[k, 1]
-      end
-      dd = solve(U, dd)
-      for k = 1:r
-        mult_table[i, j, k] = deepcopy(dd[k, 1])
+        mult_table[i, j, k] = deepcopy(d[k, 1])
       end
     end
   end
@@ -162,11 +154,7 @@ function AlgAss(O::NfOrd, I::NfOrdIdl, p::Union{Integer, fmpz})
   else
     A = AlgAss(Fp, mult_table)
   end
-  if pisfmpz
-    f = (v -> sum([ coeff(v.coeffs[i], 0)*basis[i] for i = 1:r ]))
-  else
-    f = (v -> sum([ fmpz(v.coeffs[i])*basis[i] for i = 1:r ]))
-  end
+  f = (v -> sum([ fmpz(v.coeffs[i])*basis[i] for i = 1:r ]))
   return A, f
 end
 
@@ -309,35 +297,19 @@ function kernel_of_frobenius(A::AlgAss)
   return [ A(v) for v in V ]
 end
 
-function _remove_non_pivot_cols(M::MatElem, r::Int)
-  if r == rows(M)
-    return M
-  end
-  N = zero_matrix(base_ring(M), r, r)
-  i = 1
-  while i <= r
-    for j = i:cols(M)
-      if iszero(M[i, j])
-        continue
-      end
-      for k = 1:i
-        N[k, i] = deepcopy(M[k, j])
-      end
-      break
-    end
-    i += 1
-  end
-  return N
-end
-
 # This only works if base_ring(A) is a field
 # Constructs the algebra e*A
 function subalgebra(A::AlgAss, e::AlgAssElem, idempotent::Bool = false)
   @assert parent(e) == A
   R = base_ring(A)
+  isgenres = (typeof(R) <: Generic.ResRing)
   n = dim(A)
   B = representation_mat(e)
-  r = rref!(B)
+  if isgenres
+    r, B = _rref(B)
+  else
+    r = rref!(B)
+  end
   r == 0 && error("Cannot construct zero dimensional algebra.")
   basis = Vector{AlgAssElem}(r)
   for i = 1:r
@@ -347,27 +319,24 @@ function subalgebra(A::AlgAss, e::AlgAssElem, idempotent::Bool = false)
   # The basis matrix of e*A with respect to A is
   basis_mat_of_eA = sub(B, 1:r, 1:n)
 
-  _, p, L, U = lufact(transpose(B))
-  U = _remove_non_pivot_cols(U, r)
+  if isgenres
+    _, p, L, U = _lufact(transpose(B))
+  else
+    _, p, L, U = lufact(transpose(B))
+  end
   mult_table = Array{elem_type(R), 3}(r, r, r)
   c = A()
   d = zero_matrix(R, n, 1)
-  dd = zero_matrix(R, r, 1)
   for i = 1:r
     for j = 1:r
       c = basis[i]*basis[j]
       for k = 1:n
         d[p[k], 1] = c.coeffs[k]
       end
-      #TODO: Use that L and U are already triangular
-      d = solve(L, d)
+      d = solve_lt(L, d)
+      d = solve_ut(U, d)
       for k = 1:r
-        dd[k, 1] = d[k, 1]
-      end
-      @assert all([ iszero(d[k, 1]) for k = r + 1:n ])
-      dd = solve(U, dd)
-      for k = 1:r
-        mult_table[i, j, k] = deepcopy(dd[k, 1])
+        mult_table[i, j, k] = deepcopy(d[k, 1])
       end
     end
   end
@@ -375,13 +344,9 @@ function subalgebra(A::AlgAss, e::AlgAssElem, idempotent::Bool = false)
     for k = 1:n
       d[p[k], 1] = e.coeffs[k]
     end
-    d = solve(L, d)
-    for k = 1:r
-      dd[k, 1] = d[k, 1]
-    end
-    @assert all([ iszero(d[k, 1]) for k = r + 1:n ])
-    dd = solve(U, dd)
-    eA = AlgAss(R, mult_table, [ dd[i, 1] for i = 1:r ])
+    d = solve_lt(L, d)
+    d = solve_ut(U, d)
+    eA = AlgAss(R, mult_table, [ d[i, 1] for i = 1:r ])
   else
     eA = AlgAss(R, mult_table)
   end
@@ -421,11 +386,9 @@ function issimple(A::AlgAss, compute_algebras::Type{Val{T}} = Val{true}) where T
     if degree(f) < 2
       continue
     end
-    @assert issquarefree(f)
 
     fac = factor(f)
     f1 = first(keys(fac.fac))
-    @assert length(fac) == degree(f)
     f2 = divexact(f, f1)
     g, u, v = gcdx(f1, f2)
     @assert g == 1
