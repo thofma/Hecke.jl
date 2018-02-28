4f6e8ff5463602411f9f7e60e22e75fc89982723
Claus Fieker <fieker@mathematik.uni-kl.de>
2018-02-28 10:23:09 +0100

    cleanup, and rounding down of precision
    remove some old stuff,
    set_precision -> setprecision and done "properly"
    down rounding of the minkowski_mat

--- src/conjugates.jl
+++ src/conjugates.jl
@@ -109,21 +109,17 @@ function conjugates(c::roots_ctx, p::Int)
       c.r[i] = setprecision(_h, prec)
     end
   end
-  return set_precision(c.r, p)
+  return setprecision(c.r, p)
 end
 
-function set_precision(a::BigFloat, p::Int)
-  return setprecision(function() return a*1.0; end, p)
+function Base.setprecision(a::BigComplex, p::Int)
+  return BigComplex(setprecision(a.re, p), setprecision(a.im, p))
 end
 
-function set_precision(a::BigComplex, p::Int)
-  return setprecision(function() return BigComplex(a.re*1.0, a.im*1.0); end, p)
-end
-
-function set_precision(a::Array{BigComplex, 1}, p::Int)
+function Base.setprecision(a::Array{BigComplex, 1}, p::Int)
   b = Array{BigComplex}(0);
   for i = 1:length(a)
-    push!(b, set_precision(a[i], p))
+    push!(b, setprecision(a[i], p))
   end
   return b
 end
@@ -140,21 +136,6 @@ function minkowski(a::nf_elem, p::Int)
     z = x[c.r1+i]
     push!(m, s2*BigFloat(real(z)))
     push!(m, s2*BigFloat(imag(z)))
-  end
-  return m
-
-
-  r = conjugates(c, p)
-  m = Array{BigFloat}(0);
-  a = (parent(a).pol.parent)(a)
-  s2 = sqrt(BigFloat(2.0))
-  for i = 1:c.r1
-    push!(m, real(evaluate(a, r[i])))
-  end
-  for i = 1:c.r2
-    z = evaluate(a, r[i+c.r1])
-    push!(m, s2*real(z))
-    push!(m, s2*imag(z))
   end
   return m
 end
@@ -176,61 +157,17 @@ function Base.setprecision(x::BigFloat, p::Int)
   end
 end
 
-
-function minkowski_mat(c::roots_ctx, p::Int)
-  if isdefined(c, :minkowski_mat) 
-    if c.minkowski_mat_p == p
-      return c.minkowski_mat
-    elseif c.minkowski_mat_p > p
-      return map(x->setprecision(x, p), c.minkowski_mat)
-    end
-  end
-  old = precision(BigFloat)
-  setprecision(p)
-  m = vcat([minkowski(g^i, p) for i=0:n-1])
-  setprecision(old)
-  c.minkowski_mat = m
-  c.minkowski_mat_p = p
-  return m
-
-  r = conjugates(c, p)
-  d = Array{typeof(r[1])}(length(r))
-  one = BigComplex(BigFloat(1.0), BigFloat(0.0))
-  s2 = Base.sqrt(BigFloat(2.0))
-  for i = 1:length(r)
-    d[i] = deepcopy(one)
-  end
-
-  n = degree(c.f)
-  m = Array{BigFloat}(n, n)
-
-  for i = 1:n
-    for j = 1:c.r1
-      m[i,j] = real(d[j])
-      if i < n
-        d[j] = d[j] * r[j]
-      end
-    end
-    for j=1:c.r2
-      m[i, 2*j+c.r1-1] = s2*real(d[j+c.r1])
-      m[i, 2*j+c.r1] = s2*imag(d[j+c.r1])
-      if i < n
-        d[j+c.r1] = d[j+c.r1]*r[j+c.r1]
-      end
-    end
-  end
-  setprecision(old)
-  c.minkowski_mat = m
-  c.minkowski_mat_p = p
-  return m
-end
-
 function minkowski_mat(K::AnticNumberField, p::Int = 50)
   c = roots_ctx(K)
 
-  if false && isdefined(c, :minkowski_mat) && c.minkowski_mat_p == p
-    return c.minkowski_mat
+  if isdefined(c, :minkowski_mat) 
+    if c.minkowski_mat_p == p
+      return c.minkowski_mat
+    elseif c.minkowski_mat_p >= p
+      return map(x->setprecision(x, p), c.minkowski_mat)
+    end
   end
+
   old = precision(BigFloat)
   setprecision(p)
 
@@ -247,48 +184,6 @@ function minkowski_mat(K::AnticNumberField, p::Int = 50)
   c.minkowski_mat = m
   c.minkowski_mat_p = p
   return m
-end
-
-function mult!(c::Array{BigFloat, 2}, a::fmpz_mat, b::Array{BigFloat, 2})
-  error("dont")
-  s = Base.size(b)
-  t = Base.size(c)
-  cols(a) == s[1] || error("dimensions do not match")
-  rows(a) == t[1] || error("dimensions do not match")
-  t[2] == s[2]    || error("dimensions do not match")
-
-  R = RealRing()
-  tmp_mpz = R.z1
-  tmp_mpz_r = R.t1
-  tmp_mpfr = deepcopy(c[1,1]) #R.t2
-
-  ##CF: careful: one SHOULD use mpfr_mul_z, but this converts to
-  ##    mpfr every time. I think mpfr_mul_z should be re-written
-  ##    to directly operate. Hoever, this is not going to happen soon.
-  for i = 1:rows(a)
-    for j = 1:t[2]
-      if isdefined(c, i, j)
-        ccall((:mpfr_set_zero, :libmpfr), Void, (Ptr{BigFloat}, Int), &c[i,j], 0)
-      else
-        c[i,j] = BigFloat(0)
-      end
-
-    end
-    for j = 1:cols(a)
-      z = ccall((:fmpz_mat_entry, :libflint), Ptr{fmpz},
-        (Ptr{fmpz_mat}, Int, Int), &a, i - 1, j - 1)
-      ccall((:flint_mpz_init_set_readonly, :libflint),
-        Void, (Ptr{BigInt}, Ptr{fmpz}), &tmp_mpz, z)
-      ccall((:mpfr_set_z, :libmpfr), Void, (Ptr{BigFloat}, Ptr{BigInt}, Int32),
-        &tmp_mpz_r, &tmp_mpz, __get_rounding_mode())
-      for k = 1:s[2]
-        ccall((:mpfr_mul, :libmpfr), Int, (Ptr{BigFloat}, Ptr{BigFloat}, Ptr{BigFloat}, Int32), &tmp_mpfr, &b[j,k], &tmp_mpz_r, Base.MPFR.ROUNDING_MODE[end])
-        ccall((:mpfr_add, :libmpfr), Int, (Ptr{BigFloat}, Ptr{BigFloat}, Ptr{BigFloat}, Int32), &c[i,k], &tmp_mpfr, &c[i,k], Base.MPFR.ROUNDING_MODE[end])
-      end
-#      ccall((:flint_mpz_clear_readonly, :libflint), Void, (Ptr{BigInt}), &tmp_mpz)
-    end
-  end
-  return c
 end
 
 
