210667ff86460f798daef30e7786b17a37d20f36
Tommy Hofmann <thofma@gmail.com>
2018-05-08 16:59:18 +0200

    Ups

--- src/Misc.jl
+++ src/Misc.jl
@@ -7,7 +7,6 @@ include("Misc/coprime.jl")
 include("Misc/Magma.jl")
 include("Misc/Matrix.jl")
 include("Misc/CRT.jl")  # for crt_env and modular_env
-include("Misc/NumberField.jl")
 include("Misc/Poly.jl")
 include("Misc/Residue.jl")
 include("Misc/ResidueRingPoly.jl")
