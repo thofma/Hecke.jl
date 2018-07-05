64ce052beebee85b307b537c2810ed5d36a1f552
Claus Fieker <fieker@mathematik.uni-kl.de>
2018-07-05 15:39:16 +0200

    use hnf_modular in idempotents

--- src/NfOrd/Ideal/Arithmetic.jl
+++ src/NfOrd/Ideal/Arithmetic.jl
@@ -553,7 +553,7 @@ function idempotents(x::NfOrdIdl, y::NfOrdIdl)
     V[1 + i, d + 1 + i] = 1
   end
 
-  H = hnf(V) # upper right
+  H = hnf_modular(V, g) # upper right
 
   for i in 2:(1 + d)
     if H[1, i] != 0
