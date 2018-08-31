2597d0a625c14bec9f1f1b4f08800df901591496
Claus Fieker <fieker@mathematik.uni-kl.de>
2018-08-31 11:58:32 +0200

    try a different make.jl - inspiered by Nemo

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,33 @@
 using Documenter, Hecke, Markdown, Pkg
 
+function Markdown.plain(io::IO, ::Markdown.HorizontalRule)
+           println(io, "-"^3)
+end
+
 makedocs(
     modules = Hecke,
     clean   = true,
-    doctest = false,
+    format = :html,
+    sitename = "Hecke",
+    doctest = !false,
 )
 
 # Hack around to get syntax highlighting working
-cd(Pkg.dir("Hecke", "docs"))
+cd(joinpath(dirname(pathof(Hecke)), "..", "docs"))
+
 cp("application-f78e5cb881.palette.css", "build/application-f78e5cb881.palette.css", force = true)
 cp("application-e2807e330f.css", "build/application-e2807e330f.css", force = true)
 
 deploydocs(
-    deps = Deps.pip("pygments",
-                    "mkdocs==0.16.3",
-                    "python-markdown-math",
-                    "mkdocs-cinder"),
+     deps = nothing,
+#    deps = Deps.pip()#="pygments",
+#                    "mkdocs==0.16.3",
+#                    "python-markdown-math",
+#                    "mkdocs-cinder")=#,
     repo = "github.com/thofma/Hecke.jl.git",
-    osname = "linux",
+    target = "build",
+    make = nothing,
+#    osname = "linux",
     julia = "1.0",
 )
 
