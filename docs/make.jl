3d5f84769ca297a3a9b56171ac3b66ce4177cd93
Tommy Hofmann <thofma@gmail.com>
2018-08-31 08:28:47 +0200

    Try to build docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,4 @@
-using Documenter, Hecke
-
-function Markdown.plain(io::IO, ::Markdown.HorizontalRule)
-           println(io, "-"^3)
-end
+using Documenter, Hecke, Markdown, Pkg
 
 makedocs(
     modules = Hecke,
@@ -12,8 +8,8 @@ makedocs(
 
 # Hack around to get syntax highlighting working
 cd(Pkg.dir("Hecke", "docs"))
-cp("application-f78e5cb881.palette.css", "build/application-f78e5cb881.palette.css", remove_destination = true)
-cp("application-e2807e330f.css", "build/application-e2807e330f.css", remove_destination = true)
+cp("application-f78e5cb881.palette.css", "build/application-f78e5cb881.palette.css", force = true)
+cp("application-e2807e330f.css", "build/application-e2807e330f.css", force = true)
 
 deploydocs(
     deps = Deps.pip("pygments",
@@ -21,7 +17,7 @@ deploydocs(
                     "python-markdown-math",
                     "mkdocs-cinder"),
     repo = "github.com/thofma/Hecke.jl.git",
-    osname = "osx",
-    julia = "0.6",
+    osname = "linux",
+    julia = "1.0",
 )
 
