d3b05b8e95e18f3a399eb668587bc0909d30df4d
Claus Fieker <fieker@mathematik.uni-kl.de>
2018-11-23 14:07:10 +0100

    change precision handling

--- src/NfOrd/Unit/Reduction.jl
+++ src/NfOrd/Unit/Reduction.jl
@@ -46,12 +46,11 @@ end
 #
 ################################################################################
 
-function scaled_log_matrix(u::Array{T, 1}, prec::Int = 32) where T
+function scaled_log_matrix(u::Array{T, 1}, pr::Int = 32) where T
 
   r,s = signature(_base_ring(u[1]))
   A = zero_matrix(FlintZZ, length(u), r + s)
-  pr = max(prec, maximum(nbits(maxabs_exp(U))+nbits(length(U.fac)) for U = u))
-  @vprint :UnitGroup 1 "starting prec in scaled_log_matrix: $prec\n"
+  @vprint :UnitGroup 1 "starting prec in scaled_log_matrix: $pr\n"
 
   for i in 1:length(u)
     c = conjugates_arb_log(u[i], pr)
@@ -61,22 +60,22 @@ function scaled_log_matrix(u::Array{T, 1}, prec::Int = 32) where T
     end
 
     if any(x->radius(x) > 1e-9, c)  # too small...
-      @vprint :UnitGroup 1 "increasing prec in scaled_log_matrix, now: $prec\n"
-      prec *= 2
-      if prec > 2^30
+      @vprint :UnitGroup 1 "increasing prec in scaled_log_matrix, now: $pr\n"
+      pr *= 2
+      if pr > 2^30
         error("cannot do lll on units")
         break
       end
-      return scaled_log_matrix(u, prec)
+      return scaled_log_matrix(u, pr)
     end
     for j in 1:length(c)
       tt = fmpz()
       t = ccall((:arb_mid_ptr, :libarb), Ptr{arf_struct}, (Ref{arb}, ), c[j])
-      l = ccall((:arf_get_fmpz_fixed_si, :libarb), Int, (Ref{fmpz}, Ptr{arf_struct}, Int), tt, t, -prec)
+      l = ccall((:arf_get_fmpz_fixed_si, :libarb), Int, (Ref{fmpz}, Ptr{arf_struct}, Int), tt, t, -pr)
       A[i, j] = tt
     end
   end
-  return A, prec
+  return A, pr
 end
 
 function row_norm(A::fmpz_mat, i::Int)
@@ -104,6 +103,7 @@ function reduce(u::Array{T, 1}, prec::Int = 32) where T
     @vprint :UnitGroup 1 "reducing norms of logs from 2^$(nbits(pA)) -> 2^$(nbits(pL)), rat is $(Float64(1.0*pA//pL))\n"
     u = transform(u, transpose(U))
     if nbits(pL) >= nbits(pA)
+    #  u = [compact_presentation(x, decom = factor(1*maximal_order(base_ring(x)))) for x = u]
       return u
     end
     @vprint :UnitGroup 1 "trying to reduce further...\n"
