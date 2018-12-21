05f3208fce4f4aecf860b730c5f85ac6e49099cb
Tommy Hofmann <thofma@gmail.com>
2018-12-21 10:11:10 +0100

    Tweak smallest_field.jl

--- examples/smallest_field.jl
+++ examples/smallest_field.jl
@@ -43,18 +43,84 @@ println("========================================")
 println("Discriminant bound: $dbound")
 println("========================================")
 
-set_verbose_level(:Fields, 1)
+set_verbose_level(:FieldsNonFancy, 1)
 
 l = []
 
-while length(l) == 0
+global complex_fields = []
+global real_fields = []
+global all = []
+
+global found_complex = false
+global found_real = false
+
+if mod(n, 2) != 0
+  found_complex = true
+end
+
+global only_real = false
+
+while true
+  global complex_fields
+  global real_fields
+  global found_complex
+  global found_real
+  global only_real
   global dbound
-  global l = fields(n, i, dbound)
+
+  @show only_real
+
+  global l = fields(n, i, dbound, only_real = only_real)
+
+  for v in l
+    x = v[1]
+    if !found_complex
+      if signature(x)[2] != 0
+        push!(complex_fields, v)
+      end
+    end
+
+    if !found_real
+      if signature(x)[2] == 0
+        push!(real_fields, v)
+      end
+    end
+  end
+
+  if !found_complex && length(complex_fields) > 0
+    append!(all, complex_fields)
+    found_complex = true
+    println("== FOUND SOMETHING COMPLEX ==")
+    empty!(complex_fields)
+  end
+
+  if !found_real && length(real_fields) > 0
+    append!(all, real_fields)
+    found_real = true
+    println("== FOUND SOMETHING REAL ==")
+    empty!(real_fields)
+  end
+
+  if found_complex
+    if !found_real
+      only_real = true
+      println("== I AM IN ONLY_REAL = TRUE MODE ==")
+    else
+      println("== EXITING ==")
+      break
+    end
+  end
+
   dbound = 100 * dbound
   println("========================================")
   println("Increasing discriminant bound: $dbound")
   println("========================================")
+  println("only_real = $only_real")
+  println("found_complex = $found_complex")
+  println("found_real = $found_real")
 end
+
+l = all
 
 ll = map(v -> v[1], l)
 ffields = [ (x, discriminant(maximal_order(x))) for x in ll ]
