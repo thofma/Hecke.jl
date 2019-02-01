10e529a135e679406d19a7d82add0ff7316c08ea
Tommy Hofmann <thofma@gmail.com>
2019-02-01 11:19:50 +0100

    Add a fast version of sparse saturation

--- test/Sparse/HNF.jl
+++ test/Sparse/HNF.jl
@@ -1,7 +1,7 @@
 @testset "Hermite normal form" begin
   for i in 1:10
-    r = 200
-    c = 100
+    r = 20
+    c = 10
     A = matrix(FlintZZ, rand([0,0,0,0,0,0,0,0,0,0,1], r, c))
     As = sparse_matrix(A)
     @test hnf(A) == fmpz_mat(hnf(As))
