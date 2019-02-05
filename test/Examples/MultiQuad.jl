3bcb2d48c076b163d4d58560d483aa862058a4e4
Tommy Hofmann <thofma@gmail.com>
2019-02-05 16:53:21 +0100

    Enable example tests again

--- test/Examples/MultiQuad.jl
+++ test/Examples/MultiQuad.jl
@@ -2,13 +2,13 @@
 
   include(joinpath(Hecke.pkgdir, "examples", "MultiQuad.jl"))
 
-  #@time c = MultiQuad.multi_quad(fmpz[3,5,7], 10)
-  #d = MultiQuad.simplify(c)
-  #@test MultiQuad.Hecke.class_group_get_pivot_info(d) == (4, BitSet([2, 4]))
-  #e = MultiQuad.saturate(d, 2)
-  #@test MultiQuad.Hecke.class_group_get_pivot_info(e) == (4, BitSet([2, 4]))
-  #f = MultiQuad.saturate(e, 2)
-  #@test MultiQuad.Hecke.class_group_get_pivot_info(f) == (1, BitSet([]))
+  c = MultiQuad.multi_quad(fmpz[3,5,7], 10)
+  d = MultiQuad.simplify(c)
+  @test MultiQuad.Hecke.class_group_get_pivot_info(d) == (4, BitSet([2, 4]))
+  e = MultiQuad.saturate(d, 2)
+  @test MultiQuad.Hecke.class_group_get_pivot_info(e) == (4, BitSet([2, 4]))
+  f = MultiQuad.saturate(e, 2)
+  @test MultiQuad.Hecke.class_group_get_pivot_info(f) == (1, BitSet([]))
 
 end  
 
