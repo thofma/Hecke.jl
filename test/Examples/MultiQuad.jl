3d2f6bd20d21573f6fa74ab3e6302643ce182604
Tommy Hofmann <thofma@gmail.com>
2019-02-05 18:27:58 +0100

    Examples still take forever

--- test/Examples/MultiQuad.jl
+++ test/Examples/MultiQuad.jl
@@ -1,14 +1,14 @@
 @testset "MultiQuad" begin
 
-  include(joinpath(Hecke.pkgdir, "examples", "MultiQuad.jl"))
-
-  c = MultiQuad.multi_quad(fmpz[3,5,7], 10)
-  d = MultiQuad.simplify(c)
-  @test MultiQuad.Hecke.class_group_get_pivot_info(d) == (4, BitSet([2, 4]))
-  e = MultiQuad.saturate(d, 2)
-  @test MultiQuad.Hecke.class_group_get_pivot_info(e) == (4, BitSet([2, 4]))
-  f = MultiQuad.saturate(e, 2)
-  @test MultiQuad.Hecke.class_group_get_pivot_info(f) == (1, BitSet([]))
+#  include(joinpath(Hecke.pkgdir, "examples", "MultiQuad.jl"))
+#
+#  c = MultiQuad.multi_quad(fmpz[3,5,7], 10)
+#  d = MultiQuad.simplify(c)
+#  @test MultiQuad.Hecke.class_group_get_pivot_info(d) == (4, BitSet([2, 4]))
+#  e = MultiQuad.saturate(d, 2)
+#  @test MultiQuad.Hecke.class_group_get_pivot_info(e) == (4, BitSet([2, 4]))
+#  f = MultiQuad.saturate(e, 2)
+#  @test MultiQuad.Hecke.class_group_get_pivot_info(f) == (1, BitSet([]))
 
 end  
 
