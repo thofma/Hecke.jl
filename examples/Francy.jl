c3e6de4cea04a31deab527420a8e9223180105e0
Claus Fieker <fieker@mathematik.uni-kl.de>
2019-03-12 09:27:08 +0100

    sorting and splitting and it seems to work ...

--- examples/Francy.jl
+++ examples/Francy.jl
@@ -1,69 +1,122 @@
 module Francy
 
-export canvas, graph, shape, link, menu, callback
+export canvas, graph, shape, link, menu, callback, message
+import Base.push!, Base.show
 
 using JSON, IJulia
-Base.istextmime(::MIME"application/vnd.francy+json") = true
+
+const FrancyMimeString = "application/vnd.francy+json"
+const FrancyMime = MIME"application/vnd.francy+json"
+
+Base.istextmime(::FrancyMime) = true
 
 #once..
-IJulia.register_mime(MIME("application/vnd.francy+json"));
+IJulia.register_mime(MIME(FrancyMimeString))
 
-function Base.show(io::IO, ::MIME"application/vnd.francy+json", s::String)
-  print(io, s)
-end
-
+#useful for debugging Trigger...
+#IJulia.set_verbose(true)
 
 id_cnt = 0
 function create_id()
   global id_cnt += 1
-  return "Id$id_cnt"
+  return "id$id_cnt"
 end
 
-mutable struct Canvas
+abstract type FrancyType end
+
+mutable struct Canvas <: FrancyType
   c::Dict
 end
 
-function Base.show(io::IO, t::MIME"application/vnd.francy+json", c::Canvas)
-  show(io, t, JSON.json(c.c))
+mutable struct Graph <: FrancyType
+  g::Dict
+end
+
+mutable struct Message <: FrancyType
+  m::Dict
+end
+
+mutable struct Menu <: FrancyType
+  m::Dict
+end
+
+mutable struct Chart <: FrancyType
+  c::Dict
+end
+
+mutable struct Callback <: FrancyType
+  c::Dict
+end
+
+mutable struct Shape <: FrancyType
+  s::Dict
+end
+
+mutable struct Link <: FrancyType
+  l::Dict
+end
+
+function show(io::IO, t::FrancyMime, c::Canvas)
+  C = Dict(:version => "1.0.4",
+       :mime => FrancyMimeString,
+       :canvas => c.c
+       )
+  print(io, JSON.json(C))
 end
 
 canvas_cache = Dict{String, WeakRef}()
 
-function canvas(t::String)
-  C = Canvas(Dict(:version => "1.0.4",
-       :mime => "application/vnd.francy+json",
-       :canvas =>
-    Dict(:width => 800,
+function canvas(t::String; width = 800, height = 600)
+  C = Canvas(Dict(:width => width,
          :id => create_id(),
-         :height => 600,
+         :height => height,
          :title => t,
          :zoomToFit => true,
          :texTypesetting => false,
          :menus => Dict(),
          :messages => Dict(),
          :graph => Dict()
-    )))
+    ))
   
   global canvas_cache
-  canvas_cache[C.c[:canvas][:id]] = WeakRef(C)
-  global last_c
-  last_c = C
+  canvas_cache[C.c[:id]] = WeakRef(C)
   return C
 end
 
+#= type can be
+  :directed
+  :tree
+  :undirected
+=#
 function graph()
-  return Dict(:type => :directed,
+  return Graph(Dict(:type => :directed,
               :id => create_id(),
-              :simulation => true,
+              :simulation => false,
               :collapsed => true,
               :links => Dict(),
               :nodes => Dict(),
               :messages => Dict(), 
-              )
+              ))
 end
 
-function shape(n::String, type::Symbol = :circle; callbacks = Dict())
-  return Dict(:type => type,
+function push!(C::Canvas, G::Graph)
+  if length(C.c[:graph]) > 0
+    error("Graph already present")
+  end
+  C.c[:graph] = G.g
+end
+
+#= type can be
+ :triangle
+ :diamond
+ :circle
+ :square
+ :cross
+ :star
+ :wye
+=#
+function shape(n::String, type::Symbol = :circle)
+  return Shape(Dict(:type => type,
               :id => create_id(),
               :size => 10,
               :x => 0,
@@ -74,66 +127,109 @@ function shape(n::String, type::Symbol = :circle; callbacks = Dict())
               :parent => "",
               :menus => Dict(),
               :messages => Dict(),
-              :callbacks => callbacks
-              )
+              :callbacks => Dict()
+              ))
 end
 
-function link(s::Dict, t::Dict)
-  return Dict(:id => create_id(),
-              :source => s[:id],
-              :target => t[:id],
-              :length => 0,
-              :weight => 0,
+function link(s::Shape, t::Shape; title = "", length = 0, weight = 0)
+  return Link(Dict(:id => create_id(),
+              :source => s.s[:id],
+              :target => t.s[:id],
+              :length => length,
+              :weight => weight,
               :color => "",
+              :title => title,
               :invisible => false
-              )
+              ))
 end
 
-function add_node(g::Dict, n::Dict)
-  g[:nodes][n[:id]] = n
+function push!(G::Graph, S::Shape)
+  G.g[:nodes][S.s[:id]] = S.s
 end
 
-function add_link(g::Dict, l::Dict)
-  g[:links][l[:id]] = l
+function push!(G::Graph, L::Link)
+  G.g[:links][L.l[:id]] = L.l
 end
-
 
 function callback(f::String, a::String)
-  return Dict(:id => create_id(),
+  return Callback(Dict(:id => create_id(),
               :func => f,
               :trigger => :click,
               :knownArgs => a,
               :requiredArgs => Dict()
-              )
+              ))
 end
 
-function menu(t::String, c::Dict)
-  return Dict(:id => create_id(),
+function menu(t::String, C::Callback)
+  return Menu(Dict(:id => create_id(),
               :title => t,
-              :callback => c)
+              :callback => C.c))
 end
 
-function add_menu(a::Dict, m::Dict)
-  a[:menus][m[:id]] = m
+function add(k::Symbol, a::Dict, m::Dict)
+  a[k][m[:id]] = m
 end
 
-function add_message(a::Dict, m::Dict)
-  a[:messages][m[:id]] = m
-end
+push!(C::Canvas, M::Menu) = add(:menus, C.c, M.m)
+push!(G::Graph, M::Menu) = add(:menus, G.g, M.m)
+push!(S::Shape, M::Menu) = add(:menus, S.s, M.m)
 
-function add_callback(n::Dict, c::Dict)
-  n[:callbacks][c[:id]] = c
-end
+push!(C::Canvas, M::Message) = add(:messages, C.c, M.m)
+push!(G::Graph, M::Message) = add(:messages, G.g, M.m)
+push!(S::Shape, M::Message) = add(:messages, S.s, M.m)
 
-function message(s::String)
-  return Dict(:id => create_id(),
-              :type => :info,
+push!(S::Shape, C::Callback) = add(:callbacks, S.s, C.c)
+
+
+#= type can be
+  :info
+  :error
+  :success
+  :warning
+  :default
+=#
+function message(s::String, t::Symbol = :default)
+  return Message(Dict(:id => create_id(),
+              :type => t,
               :title => s,
-              :value => ""
-              )
+              :value => "v:$s"
+              ))
 end
 
-using Hecke
+function id(C::Canvas)
+  return C.c[:id]
+end
+
+function canvas(id::Symbol)
+  global canvas_cache
+  return canvas_cache[string(id)].value
+end
+
+#=
+ THE MAGIC!
+Trigger from java script when a call back is executed...
+it gets a string in json which is composed of the info.
+Experimentally, the above works, the interface needs more work...
+=#
+
+
+function Trigger(a::String)  #needs to be in global scope
+  # incomplete: there can be more args.
+  b = JSON.parse(a)
+  c = b["func"] * "(" * b["knownArgs"] * ")"
+  res = Core.eval(Main, Meta.parse(c))
+end  
+
+export Trigger
+
+end
+
+using Main.Francy
+
+
+module FrancyExample
+
+using Hecke, Main.Francy
 function divisors(n::fmpz)
   lf = factor(n).fac
   z = Base.Iterators.ProductIterator(Tuple([[p^i for i=0:k] for (p,k) = lf]))
@@ -146,14 +242,16 @@ end
 
 function graphic_divisors(n::fmpz)
   c = canvas("Divisors of $n")
+  I = Francy.id(c)
   d = divisors(n)
   no = [shape(string(d[i])) for i=1:length(d)]
   for n = 1:length(no)
-    add_callback(no[n], callback("isprime", "(:" * c.c[:canvas][:id] * ", $(d[n]))"))
+    push!(no[n], menu("Prime?", callback("FrancyExample.isprime", "(:" * I * ", $(d[n]))")))
+    push!(no[n], menu("Odd?", callback("FrancyExample.isodd", "(:" * I * ", $(d[n]))")))
   end
   g = graph()
   for n = no
-    add_node(g, n)
+    push!(g, n)
   end
   for i=1:length(d)
     for j=1:length(d)
@@ -161,64 +259,42 @@ function graphic_divisors(n::fmpz)
         continue
       end
       if divides(d[i], d[j])[1]
-        add_link(g, link(no[i], no[j]))
+        push!(g, link(no[i], no[j], length = div(d[j], d[i])))
       end
     end
   end
-  c.c[:canvas][:graph] = g
+  push!(c, g)
   return c
 end
 
-function canvas(s::Symbol)
-  global canvas_cache
-  return canvas_cache[string(s)].value
-end
-
-function Main.isprime(t::Tuple)
+function isprime(t::Tuple)
   n = fmpz(t[2])
-  f = isprime(n)
+  f = Hecke.isprime(n)
   c = canvas(t[1])
-  add_message(c.c[:canvas], message("$n is prime: $f"))
-  display("application/vnd.francy+json", c)
+  push!(c, message("$n is prime: $f"))
+  display(Francy.FrancyMimeString, c)
   return true
 end
 
-function Trigger(a::String)
-  b = JSON.parse(a)
-#  global last_c
-  c = b["func"] * "(" * b["knownArgs"] * ")"
-  res = eval(Meta.parse(c))
-#  add_message(last_c.c[:canvas], message("2:res is $c"))
-#  display("application/vnd.francy+json", last_c)
-end  
+function isodd(t::Tuple)
+  n = fmpz(t[2])
+  f = Hecke.isodd(n)
+  c = canvas(t[1])
+  push!(c, message("$n is odd $f"))
+  display(Francy.FrancyMimeString, c)
+  return true
+end
 
-export Trigger
+export graphic_divisors
 
 end
 
+using Main.FrancyExample
+
 #=
 Hecke.example("Francy.jl")
-using Main.Francy
 
-c = canvas("test");
-
-g = graph();
-
-n1 = shape("1");
-cb = callback("console.error(1)", "");
-m = Francy.menu("maybe", cb)
-Francy.add_menu(n1, m);
-Francy.add_menu(n1, Francy.menu("or not", cb))
-
-n2 = shape("2");
-l = link(n1, n2);
-Francy.add_node(g, n1);
-Francy.add_node(g, n2);
-Francy.add_link(g, l);
-c.c[:canvas][:graph] = g;
-
-
-display("application/vnd.francy+json", c)
+Francy.graphic_divisors(fmpz(12))
 
 =#
 
