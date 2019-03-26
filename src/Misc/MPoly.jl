4ec0c3ebfa1d2c7f3fbed23c1a279b644e9acbc6
Claus Fieker <fieker@mathematik.uni-kl.de>
2019-03-26 15:49:41 +0100

    add, provisionly, conversion fmpq_mpoly ->
      fmpz_poly
      fmpq_poly
      nmod_mpoly

--- src/Misc/MPoly.jl
+++ src/Misc/MPoly.jl
@@ -14,3 +14,44 @@ function add!(c::fmpq_mpoly, a::fmpq_mpoly, b::fmpq_mpoly)
         c, a, b, c.parent)
   return c
 end
+
+#TODO: makes only sense if f is univ (uses only one var)
+function (Rx::FmpzPolyRing)(f::fmpq_mpoly)
+  fp = Rx()
+  R = base_ring(Rx)
+  d = denominator(f)
+  @assert d == 1
+  for t = terms(f)
+    e = total_degree(t)
+    @assert length(t) == 1
+    c = coeff(t, 1)
+    setcoeff!(fp, e, numerator(c*d))
+  end
+  return fp
+end
+
+function (Rx::FmpqPolyRing)(f::fmpq_mpoly)
+  fp = Rx()
+  R = base_ring(Rx)
+  for t = terms(f)
+    e = total_degree(t)
+    @assert length(t) == 1
+    c = coeff(t, 1)
+    setcoeff!(fp, e, c)
+  end
+  return fp
+end
+
+function (Rx::GFPPolyRing)(f::fmpq_mpoly)
+  fp = Rx()
+  R = base_ring(Rx)
+  d = denominator(f)
+  for t = terms(f)
+    e = total_degree(t)
+    @assert length(t) == 1
+    c = coeff(t, 1)
+    setcoeff!(fp, e, R(numerator(c*d)))
+  end
+  return fp * inv(R(d))
+end
+
