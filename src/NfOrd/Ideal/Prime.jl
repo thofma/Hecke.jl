e8cb3a426769abf2efed09a8a494d42059b94b6f
Claus Fieker <fieker@mathematik.uni-kl.de>
2019-03-26 15:50:34 +0100

    ... and remove them from Ideal/Prime.jl

--- src/NfOrd/Ideal/Prime.jl
+++ src/NfOrd/Ideal/Prime.jl
@@ -1363,33 +1363,6 @@ function prime_ideals_over(O::NfOrd, P::NfOrdIdl)
   return p_critical_primes
 end
 
-function (Rx::FmpzPolyRing)(f::fmpq_mpoly)
-  fp = Rx()
-  R = base_ring(Rx)
-  d = denominator(f)
-  @assert d == 1
-  for t = terms(f)
-    e = total_degree(t)
-    @assert length(t) == 1
-    c = coeff(t, 1)
-    setcoeff!(fp, e, numerator(c*d))
-  end
-  return fp
-end
-
-
-function (Rx::GFPPolyRing)(f::fmpq_mpoly)
-  fp = Rx()
-  R = base_ring(Rx)
-  d = denominator(f)
-  for t = terms(f)
-    e = total_degree(t)
-    @assert length(t) == 1
-    c = coeff(t, 1)
-    setcoeff!(fp, e, R(numerator(c*d)))
-  end
-  return fp * inv(R(d))
-end
 
 function _fac_and_lift(f::fmpq_mpoly, p, degree_limit, lower_limit)
   Zx, x = PolynomialRing(FlintZZ, cached = false)
