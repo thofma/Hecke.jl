e115f6d4cbe443649f16f5765facc49d16121b94
CarloSircana <sircana@mathematik.uni-kl.de>
2019-03-29 14:43:14 +0100

    forgot to include the file

--- src/NfOrd/Ideal.jl
+++ src/NfOrd/Ideal.jl
@@ -33,6 +33,7 @@
 ################################################################################
 
 include("Ideal/Ideal.jl")
+include("Ideal/TwoElement.jl")
 include("Ideal/Arithmetic.jl")
 include("Ideal/Prime.jl")
 include("Ideal/LLL.jl")
