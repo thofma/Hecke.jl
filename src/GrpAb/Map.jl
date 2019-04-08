6e101e5314a1cca3ab21986e32cdb1ac8defb276
Claus Fieker <fieker@mathematik.uni-kl.de>
2019-04-08 20:48:17 +0200

    add proper check for hom's and return value typing
    doc strings

--- src/GrpAb/Map.jl
+++ src/GrpAb/Map.jl
@@ -43,7 +43,6 @@ export haspreimage, hasimage, hom, kernel, cokernel, image, isinjective, issurje
 ################################################################################
 
 @doc Markdown.doc"""
-***
     haspreimage(M::GrpAbFinGenMap, a::GrpAbFinGenElem) -> Bool, GrpAbFinGenElem
 
 > Returns whether $a$ is in the image of $M$. If so, the second return value is
@@ -141,57 +140,49 @@ function hom(G::GrpAbFinGen, B::Array{GrpAbFinGenElem, 1})
   return h
 end
 
-function hom(G::GrpAbFinGen, H::GrpAbFinGen, B::Array{GrpAbFinGenElem, 1})
+function hom(G::GrpAbFinGen, H::GrpAbFinGen, B::Array{GrpAbFinGenElem, 1}; check::Bool = true)
   @assert length(B) == ngens(G)
+  @assert all(i -> parent(i) == H, B)
   M = zero_matrix(FlintZZ, ngens(G), ngens(H))
   for i = 1:ngens(G)
     for j = 1:ngens(H)
       M[i, j] = B[i][j]
     end
   end
-  h = GrpAbFinGenMap(G, H, M)
+  h = hom(G, H, M, check)
   return h
 end
 
-# TODO: Extend the check to non-endomorphisms
+function check_mat(A::GrpAbFinGen, B::GrpAbFinGen, M::fmpz_mat)
+  #we have A = X/Y  and B = U/V (generators and relations)
+  #        M defines a map (hom) from X -> U
+  #        Y -> X is the canonical embedding
+  #        V -> U dito
+  #                          M
+  # need to check if Y -> X --> U lands in V
+  # if Y -> X -> B is zero.
+  R = rels(A) * M
+  return all(x -> iszero(B(R[x, :])), 1:nrows(R))
+end
+
 function hom(A::GrpAbFinGen, B::GrpAbFinGen, M::fmpz_mat, check::Bool = true)
-  if check #totally wrong if A != B
-    if A == B
-      G = A
-      images = [ G([M[i, j] for j in 1:ngens(G)]) for i in 1:ngens(G) ]
-      a = G[0]
-      for i in 1:nrels(G)
-        for j in 1:ngens(G)
-          a = a + rels(G)[i, j] * images[j]
-        end
-        if !iszero(a)
-          error("Matrix does not define a morphism of abelian groups")
-        end
-      end
-    end
+  if check 
+    check_mat(A, B, M) || error("Matrix does not define a morphism of abelian groups")
   end
 
-  return GrpAbFinGenMap(A, B, M)
+  return GrpAbFinGenMap(A, B, M)::GrpAbFinGenMap
 end
 
 function hom(A::GrpAbFinGen, B::GrpAbFinGen, M::fmpz_mat, Minv, check::Bool = true)
   if check
-    if A == B
-      G = A
-      images = [ G([M[i, j] for j in 1:ngens(G)]) for i in 1:ngens(G) ]
-      a = 0 * G[1]
-      for i in 1:nrels(G)
-        for j in 1:ngens(G)
-          a = a + G.rels[i, j] * images[j]
-        end
-        if !iszero(a)
-          error("Matrix does not define a morphism of abelian groups")
-        end
-      end
-    end
+    check_mat(A, B, M) || error("Matrix does not define a morphism of abelian groups")
+    check_mat(A, B, Minv) || error("Matrix does not define a morphism of abelian groups")
+    h = GrpAbFinGenMap(A, B, M, Minv)
+    all(x -> x == inv(h)(h(x)), gens(A)) || error("Matrix does not define a morphism of abelian groups")
+    return h::GrpAbFinGenMap
   end
 
-  return GrpAbFinGenMap(A, B, M, Minv)
+  return GrpAbFinGenMap(A, B, M, Minv)::GrpAbFinGenMap
 end
 
 ################################################################################
@@ -365,13 +356,27 @@ function cyclic_hom(a::fmpz, b::fmpz)
   return (g, divexact(b, g))
 end
 
-function hom(G::GrpAbFinGen, H::GrpAbFinGen)
+
+@doc Markdown.doc"""
+    hom(G::GrpAbFinGen, H::GrpAbFinGen; task::Symbol = :map) -> GrpAbFinGen, Map
+> Computes the group of all homomorpisms from $G$ to $H$ as an abstract group.
+> If {{{task}}} is ":map", then a map $\phi$ is computed that can be used
+> to obtain actual homomorphisms. This map also allows preimages.
+> Set {{{task}}} to ":none" to not compute the map.
+"""
+function hom(G::GrpAbFinGen, H::GrpAbFinGen; task::Symbol = :map)
+  @assert task in [:map, :none]
   sG, mG = snf(G)  # mG : sG -> G
   sH, mH = snf(H)  # mH : sH -> G
   n = ngens(sG)
   m = ngens(sH)
   r = [cyclic_hom(x, y) for x = sG.snf for y = sH.snf]
   R = GrpAbFinGen([x[1] for x = r])
+  set_special(R, :hom=>(G, H), :show => show_hom)
+  if task == :none
+    return R
+  end
+
   c = [x[2] for x = r]
 
   function phi(r::GrpAbFinGenElem)
@@ -380,11 +385,13 @@ function hom(G::GrpAbFinGen, H::GrpAbFinGen)
 
   function ihp(r::GrpAbFinGenMap)
     #transpose is due to linear indexing being wrong order
+    if ngens(sG) == 0
+      return R[0]
+    end
     local m = vcat([preimage(mH, r(mG(sG[i]))).coeff for i = 1:ngens(sG)])'
     return R([divexact(m[i], c[i]) for i = 1:ngens(R)])
   end
   return R, MapFromFunc(phi, ihp, R, MapParent(G, H, "homomorphisms"))
-  return R, phi, ihp
 end
 
 ######################################################################
@@ -455,6 +462,11 @@ end
 (::QmodZ)(a::Rational) = QmodZ(fmpq(a))
 
 #TODO: technically, dual Z could be Q/Z ...
+@doc Markdown.doc"""
+    dual(G::GrpAbFinGen) -> GrpAbFinGen, Map
+> Computes the dual group, ie. $hom(G, Q/Z)$ as an
+> abstract group. The map can be used to obtain actual homomorphisms.
+"""
 function dual(G::GrpAbFinGen)
   T, mT = torsion_subgroup(G)
   u = root_of_one(QmodZ, order(T))
