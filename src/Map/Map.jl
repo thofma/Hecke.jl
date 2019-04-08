1f18d3d5274a51f8648026a1b7c13322e80b116a
Claus Fieker <fieker@mathematik.uni-kl.de>
2019-04-08 13:06:46 +0200

    more compact printing for maps

--- src/Map/Map.jl
+++ src/Map/Map.jl
@@ -9,14 +9,19 @@
 
 function show(io::IO, M::Map)
   @show_name(io, M)
+  if get(io, :compact, false)
+    print(io, domain(M), " --> ", codomain(M), "\n")
+    return
+  end
   io = Base.IOContext(io, :compact => true)
   print(io, "Map with following data\n")
   print(io, "Domain:\n")
   print(io, "=======\n")
-  print(io, "$(domain(M))\n\n")
-  print(io, "Codomain:\n")
+  print(io, domain(M))
+  print(io, "\nCodomain:\n")
   print(io, "=========\n")
-  print(io, "$(codomain(M))\n")
+  print(io, codomain(M))
+  print(io, "\n")
 end
 
 function preimage(M::Map{D, C}, a) where {D, C}
