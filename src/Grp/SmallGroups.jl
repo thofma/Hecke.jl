21b4da8c238fc3f6d864f4feac349dfcf23db26e
Tommy Hofmann <thofma@gmail.com>
2019-05-22 09:01:11 +0200

    Updates to automorphism group computation

--- src/Grp/SmallGroups.jl
+++ src/Grp/SmallGroups.jl
@@ -14,6 +14,8 @@ function number_of_small_groups(i)
   return length(small_groups_1_63[i])
 end
 
+isfrom_db(G::GrpGen) = G.isfromdb
+
 # Data in the DB:
 # <gens, rels, nontrivrels, orderdis, ordersubdis, IsAbelian(G), IsCyclic(G),
 # IsSolvable(G), IsNilpotent(G), #AutomorphismGroup(G);
@@ -26,8 +28,22 @@ function small_group(i, j)
   for p in small_groups_1_63[i][j][1]
     push!(Gens, P(p))
   end
-  G, _, _ = generic_group(closure(Gens, *, P()), *)
+  G, PtoG, GtoP = generic_group(closure(Gens, *, P()), *)
+
+  # The small groups have generators, for which we know
+  # generators for the relations.
+  # This is used in the homomorphism computation.
+  # To make this work, the generators must much exactly
+  # with the generators in the database.
+  G.gens = Vector{Int}(undef, length(small_groups_1_63[i][j][1]))
+  for k in 1:length(small_groups_1_63[i][j][1])
+    p = small_groups_1_63[i][j][1][k]
+    G.gens[k] = PtoG[P(p)][]
+  end
+
   G.issolvable = small_groups_1_63[i][j][8] ? 1 : 2
   G.isnilpotent = small_groups_1_63[i][j][9] ? 1 : 2
+  G.isfromdb = true
+  G.small_group_id = (i, j)
   return G
 end
