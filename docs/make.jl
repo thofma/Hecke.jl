d8c7b403b810e53aef8215984bd77fb292953deb
Tommy Hofmann <thofma@gmail.com>
2019-05-24 14:51:37 +0200

    Play with docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,49 +1,62 @@
 using Documenter, Hecke, Nemo, AbstractAlgebra, Markdown, Pkg
 
 makedocs(
+    doctest= false,
     modules = [Hecke, Nemo, AbstractAlgebra],
-    clean   = true,
-    format = :html,
-    sitename = "Hecke",
-    doctest = !false,
-    pages = [
-      "index.md",
-      "number_fields/intro.md",
-      "Orders" => [ "orders/introduction.md",
-                    "orders/orders.md",
-                    "orders/elements.md",
-                    "orders/ideals.md",
-                    "orders/frac_ideals.md"
-                  ],
-#      "Maximal Orders" => [ "MaximalOrders/Introduction.md",
-#                            "MaximalOrders/Creation.md",
-#                            "MaximalOrders/Elements.md",
-#                            "MaximalOrders/Ideals.md"
-#                          ],
-      "abelian/introduction.md",
-      "class_fields/intro.md",
-      "sparse/intro.md",
-      "FacElem.md"
-      ]
+    format = Markdown(),
 )
-
-# Hack around to get syntax highlighting working
-#cd(joinpath(dirname(pathof(Hecke)), "..", "docs"))
-#
-#cp("application-f78e5cb881.palette.css", "build/application-f78e5cb881.palette.css", force = true)
-#cp("application-e2807e330f.css", "build/application-e2807e330f.css", force = true)
 
 deploydocs(
-     deps = nothing,
-#    deps = Deps.pip()#="pygments",
-#                    "mkdocs==0.16.3",
-#                    "python-markdown-math",
-#                    "mkdocs-cinder")=#,
-    repo = "github.com/thofma/Hecke.jl.git",
-    target = "build",
-    make = nothing,
-    osname = "linux",
-    julia = "1.0",
+  repo = "github.com/shipengcheng1230/GmshTools.jl.git",
+  deps = Deps.pip("pymdown-extensions", "pygments", "mkdocs", "python-markdown-math", "mkdocs-material"),
+  target = "site",
+  make = () -> run(`mkdocs build`),
 )
 
-# Try out the following deps = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4" ,"python-markdown-math", "pygments", "pymdown-extensions")
+#makedocs(
+#    modules = [Hecke, Nemo, AbstractAlgebra],
+#    clean   = true,
+#    format = :html,
+#    sitename = "Hecke",
+#    doctest = !false,
+#    pages = [
+#      "index.md",
+#      "number_fields/intro.md",
+#      "Orders" => [ "orders/introduction.md",
+#                    "orders/orders.md",
+#                    "orders/elements.md",
+#                    "orders/ideals.md",
+#                    "orders/frac_ideals.md"
+#                  ],
+##      "Maximal Orders" => [ "MaximalOrders/Introduction.md",
+##                            "MaximalOrders/Creation.md",
+##                            "MaximalOrders/Elements.md",
+##                            "MaximalOrders/Ideals.md"
+##                          ],
+#      "abelian/introduction.md",
+#      "class_fields/intro.md",
+#      "sparse/intro.md",
+#      "FacElem.md"
+#      ]
+#)
+#
+## Hack around to get syntax highlighting working
+##cd(joinpath(dirname(pathof(Hecke)), "..", "docs"))
+##
+##cp("application-f78e5cb881.palette.css", "build/application-f78e5cb881.palette.css", force = true)
+##cp("application-e2807e330f.css", "build/application-e2807e330f.css", force = true)
+#
+#deploydocs(
+#     deps = nothing,
+##    deps = Deps.pip()#="pygments",
+##                    "mkdocs==0.16.3",
+##                    "python-markdown-math",
+##                    "mkdocs-cinder")=#,
+#    repo = "github.com/thofma/Hecke.jl.git",
+#    target = "build",
+#    make = nothing,
+#    osname = "linux",
+#    julia = "1.0",
+#)
+#
+## Try out the following deps = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4" ,"python-markdown-math", "pygments", "pymdown-extensions")
