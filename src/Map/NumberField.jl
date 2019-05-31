edc724f82e6d336fd36243cf783cbeeed240d4a4
Tommy Hofmann <thofma@gmail.com>
2019-05-31 09:29:01 +0200

    Add prototype for proper automorphism_group

--- src/Map/NumberField.jl
+++ src/Map/NumberField.jl
@@ -1,5 +1,17 @@
 export extend, NfToNfMor, automorphisms
 
+struct NfMorSet{T}
+  field::T
+end
+
+function NfMorSet(K)
+  return NfMorSet{typeof(K)}(K)
+end
+
+function show(io::IO, S::NfMorSet{T}) where {T}
+  print(io, "Set of automorphisms of ", S.field)
+end
+
 mutable struct NfToNfMor <: Map{AnticNumberField, AnticNumberField, HeckeMap, NfToNfMor}
   header::MapHeader{AnticNumberField, AnticNumberField}
   prim_img::nf_elem
@@ -72,6 +84,41 @@ mutable struct NfToNfMor <: Map{AnticNumberField, AnticNumberField, HeckeMap, Nf
   end
 end
 
+parent(f::NfToNfMor) = NfMorSet(domain(f))
+
+mutable struct GrpGenToNfMorSet{T} <: Map{GrpGen, NfMorSet{T}, HeckeMap, GrpGenToNfMorSet{T}}
+  header::MapHeader{GrpGen, NfMorSet{T}}
+
+  function GrpGenToNfMorSet(G::GrpGen, S::NfMorSet{T}) where {T}
+    z = new{T}()
+    z.header = MapHeader(G, S)
+    return z
+  end
+end
+
+function GrpGenToNfMorSet(G::GrpGen, K::AnticNumberField)
+  return GrpGenToNfMorSet(G, NfMorSet(K))
+end
+
+function image(f::GrpGenToNfMorSet, g::GrpGenElem)
+  K = codomain(f).field
+  return automorphisms(K)[g[]]
+end
+
+function (f::GrpGenToNfMorSet)(g::GrpGenElem)
+  return image(f, g)
+end
+
+function preimage(f::GrpGenToNfMorSet, a::NfToNfMor)
+  K = codomain(f).field
+  aut = automorphisms(K, copy = false)
+  for i in 1:length(aut)
+    if a == aut[i]
+      return domain(f)[i]
+    end
+  end
+  error("something wrong")
+end
 
 function inv(f::NfToNfMor)
   if degree(domain(f)) != degree(codomain(f))
@@ -205,20 +252,7 @@ function show(io::IO, h::NfToNfMor)
   println(io, "defined by ", gen(domain(h)), " -> ", h.prim_img)
 end
 
-
-function automorphisms(K::AnticNumberField; copy::Bool = true)
-  try
-    Aut = _get_automorphisms_nf(K)::Vector{NfToNfMor}
-    if copy 
-      return Base.copy(Aut)
-    else 
-      return Aut
-    end
-  catch e
-    if !isa(e, AccessorNotSetError)
-      rethrow(e)
-    end
-  end
+function _automorphisms(K::AnticNumberField)
   if degree(K) == 1
     auts = NfToNfMor[hom(K, K, one(K))]
   else
@@ -235,12 +269,63 @@ function automorphisms(K::AnticNumberField; copy::Bool = true)
     Aut1[end] = id_hom(K)
     auts = closure(Aut1, degree(K))
   end
+end
+
+function automorphisms(K::AnticNumberField; copy::Bool = true)
+  try
+    Aut = _get_automorphisms_nf(K)::Vector{NfToNfMor}
+    if copy 
+      return Base.copy(Aut)
+    else 
+      return Aut
+    end
+  catch e
+    if !isa(e, AccessorNotSetError)
+      rethrow(e)
+    end
+  end
+  auts = _automorphisms(K)
   _set_automorphisms_nf(K, auts)
   if copy
     return Base.copy(auts)
   else 
     return auts
   end
+end
+
+function automorphism_group(K::AnticNumberField)
+  aut = automorphisms(K)
+  n = degree(K)
+  #First, find a good prime
+  p = 11
+  d = numerator(discriminant(K.pol))
+  while mod(d, p) == 0
+    p = next_prime(p)
+  end
+  R = GF(p, cached = false)
+  Rx, x = PolynomialRing(R, "x", cached = false)
+  fmod = Rx(K.pol)
+  pols = gfp_poly[Rx(g.prim_img) for g in aut]
+  #if g in aut
+  #  push!(pols, Rx(g.prim_img))
+  #end
+  Dcreation = Vector{Tuple{gfp_poly, Int}}(undef, length(pols))
+  for i = 1:length(pols)
+    Dcreation[i] = (pols[i], i)
+  end
+  D = Dict{gfp_poly, Int}(Dcreation)
+  @assert length(D) == n
+  mult_table = Array{Int, 2}(undef, n, n)
+  #permutations = Array{Array{Int, 1},1}(undef, n)
+  for s = 1:n
+    #perm = Array{Int, 1}(undef, length(aut))
+    for i = 1:length(aut)
+      mult_table[s, i] = D[Hecke.compose_mod(pols[s], pols[i], fmod)]
+    end
+  end
+  G = GrpGen(mult_table)
+  return G, GrpGenToNfMorSet(G, K)
+  #return _perm_to_gap_grp(permutations)
 end
 
 function hom(K::AnticNumberField, L::AnticNumberField, a::nf_elem; check::Bool = true, compute_inverse::Bool = false)
@@ -274,3 +359,101 @@ isinjective(m::NfToNfMor) = true
 issurjective(m::NfToNfMor) = (degree(domain(m)) == degree(codomain(m)))
 
 isbijective(m::NfToNfMor) = issurjective(m)
+
+###############################################################################
+#
+#  NfToNfMor closure
+#
+###############################################################################
+
+function closure(S::Vector{NfToNfMor}, final_order::Int = -1)
+
+  K = domain(S[1])
+  d = numerator(discriminant(K.pol))
+  p = 11
+  while mod(d, p) == 0
+    p = next_prime(p)
+  end
+  R = GF(p, cached = false)
+  Rx, x = PolynomialRing(R, "x", cached = false)
+  fmod = Rx(K.pol)
+
+  t = length(S)
+  order = 1
+  elements = [id_hom(K)]
+  pols = gfp_poly[x]
+  gpol = Rx(S[1].prim_img)
+  if gpol != x
+    push!(pols, gpol)
+    push!(elements, S[1])
+    order += 1
+
+    gpol = compose_mod(gpol, pols[2], fmod)
+
+    while gpol != x
+      order = order +1
+      push!(elements, S[1]*elements[end])
+      push!(pols, gpol)
+      gpol = compose_mod(gpol, pols[2], fmod)
+    end
+  end
+  if order == final_order
+    return elements
+  end
+
+  for i in 2:t
+    if !(S[i] in elements)
+      pi = Rx(S[i].prim_img)
+      previous_order = order
+      order = order + 1
+      push!(elements, S[i])
+      push!(pols, Rx(S[i].prim_img))
+      for j in 2:previous_order
+        order = order + 1
+        push!(pols, compose_mod(pols[j], pi, fmod))
+        push!(elements, elements[j]*S[i])
+      end
+      if order == final_order
+        return elements
+      end
+      rep_pos = previous_order + 1
+      while rep_pos <= order
+        for k in 1:i
+          s = S[k]
+          po = Rx(s.prim_img)
+          att = compose_mod(pols[rep_pos], po, fmod)
+          if !(att in pols)
+            elt = elements[rep_pos]*s
+            order = order + 1
+            push!(elements, elt)
+            push!(pols, att)
+            for j in 2:previous_order
+              order = order + 1
+              push!(pols, compose_mod(pols[j], att, fmod))
+              push!(elements, elements[j] *elt)
+            end
+            if order == final_order
+              return elements
+            end
+          end
+        end
+        rep_pos = rep_pos + previous_order
+      end
+    end
+  end
+  return elements
+end
+
+function small_generating_set(Aut::Array{NfToNfMor, 1})
+  K = domain(Aut[1])
+  a = gen(K)
+  Identity = Aut[1]
+  for i in 1:length(Aut)
+    Au = Aut[i]
+    if Au.prim_img == a
+      Identity = Aut[i]
+      break
+    end
+  end
+  return small_generating_set(Aut, *, Identity)
+end
