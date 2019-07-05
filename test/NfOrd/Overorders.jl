6a2f39f0fce95fbae2cb75495cc0327fdd0a7087
Tommy Hofmann <thofma@gmail.com>
2019-07-05 15:27:52 +0200

    Clean up overorders and add tests

--- test/NfOrd/Overorders.jl
+++ test/NfOrd/Overorders.jl
@@ -1,21 +1,56 @@
 @testset "Overorders" begin
-   Qx,  x = FlintQQ["x"]
+  Qx,  x = FlintQQ["x"]
 
-  f = x^3 - 1000*x^2 + 1000*x - 1000
+  f = x^2 + 100x + 100
 
-   K,  a = NumberField(f, "a");
+  A = AlgAss(f * (f - 1))
+  O = Order(A, basis(A))
 
-  E = EquationOrder(K)
+  orders = @inferred poverorders(O, 2)
+  @test length(orders) == 6
 
-  #@test length(overorders(E)) == 16
+  orders = @inferred poverorders(O, fmpz(5))
+  @test length(orders) == 2
 
-  f = x^3 + 31 * x^2 + 43* x + 77
+  orders = @inferred poverorders(O, fmpz(7))
+  @test length(orders) == 3
 
-   K,  a = NumberField(f, "a");
+  orders = @inferred overorders(O)
+  @test length(orders) == 36
 
-  E = EquationOrder(K)
+  @test count(isgorenstein, orders) == 36
+  @test count(isbass, orders) == 36
+  @test count(ismaximal, orders) == 1
 
+  orders = @inferred overorders(O, type = :bass)
+  @test length(orders) == 36
 
-  #@test length(overorders(E)) == 15
-  #@test length(Hecke.overorders_naive(E)) == 15
+  orders = @inferred overorders(O, type = :gorenstein)
+  @test length(orders) == 36
+
+  G = small_group(6, 1)
+  A = AlgAss(group_algebra(FlintQQ, G))[1]
+  O = Order(A, basis(A))
+  orders = @inferred overorders(O)
+  @test length(orders) == 12
+
+  orders = @inferred overorders(O, type = :all)
+  @test length(orders) == 12
+
+  @test count(ismaximal, orders) == 2
+
+  @test_throws ErrorException overorders(O, type =  :gorenstein)
+  @test_throws ErrorException overorders(O, type =  :bass)
+  @test_throws ErrorException overorders(O, type =  :blub)
+
+  f = x^4-1680*x^3-25920*x^2-1175040*x+25214976
+  O = EquationOrder(f)
+  orders = @inferred overorders(O)
+  @test length(orders) == 2535
+  @test count(ismaximal, orders) == 1
+  @test count(isgorenstein, orders) == 657
+  @test count(isbass, orders) == 5
+
+  @test length(overorders(O, type = :gorenstein)) == 657
+  @test length(overorders(O, type = :bass)) == 5
 end
