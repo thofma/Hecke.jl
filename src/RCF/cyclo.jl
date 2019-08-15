c83133993c200136b35fbdacdb3ba04ee67aaf18
CarloSircana <sircana@mathematik.uni-kl.de>
2019-08-15 15:02:45 +0200

    new cyclotomic extension

--- src/RCF/cyclo.jl
+++ src/RCF/cyclo.jl
@@ -36,18 +36,21 @@ Computes $k(\zeta_n)$, in particular, a structure containing $k(\zeta_n)$
 both as an absolute extension, as a relative extension (of $k$) and the maps
 between them.
 """
-function cyclotomic_extension(k::AnticNumberField, n::Int)
+function cyclotomic_extension(k::AnticNumberField, n::Int; cached::Bool = true,  compute_maximal_order::Bool = true, compute_LLL_basis::Bool = true)
+
   Ac = CyclotomicExt[]
-  try 
-    Ac = Hecke._get_cyclotomic_ext_nf(k)::Vector{CyclotomicExt}
-    for i = Ac
-      if i.n == n
-        return i
+  if cached
+    try 
+      Ac = Hecke._get_cyclotomic_ext_nf(k)::Vector{CyclotomicExt}
+      for i = Ac
+        if i.n == n
+          return i
+        end
       end
-    end
-  catch e
-    if !(e isa AbstractAlgebra.AccessorNotSetError)
-      rethrow(e)
+    catch e
+      if !(e isa AbstractAlgebra.AccessorNotSetError)
+        rethrow(e)
+      end
     end
   end
   
@@ -60,14 +63,21 @@ function cyclotomic_extension(k::AnticNumberField, n::Int)
   if n == 2
     #Easy, just return the field
     Kr = number_field(t+1, cached = false, check = false)[1]
+    if compute_maximal_order
+      Ok = maximal_order(k)
+      if compute_LLL_basis
+        lll(Ok)
+      end
+    end
     abs2rel = NfToNfRel(k, Kr, gen(k), k(-1), Kr(gen(k)))
     small2abs = id_hom(k)
     c.Kr = Kr
     c.Ka = k
     c.mp = (abs2rel, small2abs)
-
-    push!(Ac, c)
-    Hecke._set_cyclotomic_ext_nf(k, Ac)
+    if cached
+      push!(Ac, c)
+      Hecke._set_cyclotomic_ext_nf(k, Ac)
+    end
     return c
   end
   
@@ -77,7 +87,7 @@ function cyclotomic_extension(k::AnticNumberField, n::Int)
   if n < 5
     #For n = 3, 4 the cyclotomic polynomial has degree 2,
     #so we can just ask for the roots.
-    if !isone(gcd(discriminant(maximal_order(k)), n)) && !istotally_real(k)
+    if !isone(gcd(degree(fk), degree(k))) && !isone(gcd(discriminant(maximal_order(k)), n)) && !istotally_real(k)  
       rt = _roots_hensel(fk, max_roots = 1, isnormal = true)
     else
       rt = nf_elem[]
@@ -87,43 +97,54 @@ function cyclotomic_extension(k::AnticNumberField, n::Int)
       Kr, gKr = number_field(t - rt[1], cached = false, check = false)
       abs2rel = NfToNfRel(k, Kr, gen(k), rt[1], Kr(gen(k)))
       small2abs = id_hom(k)
+      if compute_maximal_order
+        Ok = maximal_order(k)
+        if compute_LLL_basis
+          lll(Ok)
+        end
+      end
       c.Kr = Kr
       c.Ka = k
       c.mp = (abs2rel, small2abs)
     else
       Kr, Kr_gen = number_field(fk, "z_$n", cached = false, check = false)
       Ka, abs2rel, small2abs = Hecke.absolute_field(Kr, false)
-
-      Zk = maximal_order(k)
-      b_k = basis(Zk, k)
-      B_k = Vector{nf_elem}(undef, degree(Ka))
-      for i = 1:length(b_k)
-        B_k[i] = small2abs(b_k[i])
+      if compute_maximal_order
+        Zk = maximal_order(k)
+        b_k = basis(Zk, k)
+        B_k = Vector{nf_elem}(undef, degree(Ka))
+        for i = 1:length(b_k)
+          B_k[i] = small2abs(b_k[i])
+        end
+        g = abs2rel\(Kr_gen)
+        for j = 1:length(b_k)
+          B_k[j+length(b_k)] = B_k[j]*g
+        end
+        ZKa = Hecke.NfOrd(B_k)
+        ZKa.disc = discriminant(f)^degree(k)*discriminant(Zk)^degree(fk)
+        ZKa.index = root(divexact(numerator(discriminant(Ka)), ZKa.disc), 2)
+        ZKa.gen_index = fmpq(ZKa.index)
+        for (p,v) = factor(gcd(discriminant(Zk), fmpz(n))).fac
+          ZKa = pmaximal_overorder(ZKa, p)
+        end
+        ZKa.ismaximal = 1
+        if compute_LLL_basis
+          ZKa = lll(ZKa)
+        end
+        Hecke._set_maximal_order_of_nf(Ka, ZKa)
       end
-      g = abs2rel\(Kr_gen)
-      for j = 1:length(b_k)
-        B_k[j+length(b_k)] = B_k[j]*g
-      end
-      ZKa = Hecke.NfOrd(B_k)
-      ZKa.disc = discriminant(f)^degree(k)*discriminant(Zk)^degree(fk)
-      ZKa.index = root(divexact(numerator(discriminant(Ka)), ZKa.disc), 2)
-      ZKa.gen_index = fmpq(ZKa.index)
-      for (p,v) = factor(gcd(discriminant(Zk), fmpz(n))).fac
-        ZKa = pmaximal_overorder(ZKa, p)
-      end
-      ZKa.ismaximal = 1
-      ZKa = lll(ZKa)
-      Hecke._set_maximal_order_of_nf(Ka, ZKa)
       c.Kr = Kr
       c.Ka = Ka
       c.mp = (abs2rel, small2abs)
     end
-    push!(Ac, c)
-    Hecke._set_cyclotomic_ext_nf(k, Ac)
+    if cached
+      push!(Ac, c)
+      Hecke._set_cyclotomic_ext_nf(k, Ac)
+    end
     return c
   end
   if n == 8 
-    if !isone(gcd(discriminant(maximal_order(k)), n)) 
+    if !isone(gcd(degree(fk), degree(k))) && !isone(gcd(discriminant(maximal_order(k)), 2)) 
       if istotally_real(k)
         f1k = kt(nf_elem[k(-2), zero(k), one(k)])
         rt = _roots_hensel(f1k, max_roots = 1, isnormal = true)
@@ -138,32 +159,38 @@ function cyclotomic_extension(k::AnticNumberField, n::Int)
     
     Kr, Kr_gen = number_field(fk, "z_$n", cached = false, check = false)
     Ka, abs2rel, small2abs = Hecke.absolute_field(Kr, false)
-    Zk = maximal_order(k)
-    b_k = basis(Zk, k)
-    B_k = Vector{nf_elem}(undef, degree(Ka))
-    for i = 1:length(b_k)
-      B_k[i] = small2abs(b_k[i])
+    if compute_maximal_order
+      Zk = maximal_order(k)
+      b_k = basis(Zk, k)
+      B_k = Vector{nf_elem}(undef, degree(Ka))
+      for i = 1:length(b_k)
+        B_k[i] = small2abs(b_k[i])
+      end
+      g = abs2rel\(Kr_gen)
+      for j = 1:length(B_k)-length(b_k)
+        B_k[j+length(b_k)] = B_k[j]*g
+      end
+      ZKa = Hecke.NfOrd(B_k)
+      if divisible(discriminant(Zk), 2)
+        ZKa = pmaximal_overorder(ZKa, 2)
+      end
+      ZKa.ismaximal = 1
+      if compute_LLL_basis
+        ZKa = lll(ZKa)
+      end
+      Hecke._set_maximal_order_of_nf(Ka, ZKa)
     end
-    g = abs2rel\(Kr_gen)
-    for j = 1:length(B_k)-length(b_k)
-      B_k[j+length(b_k)] = B_k[j]*g
-    end
-    ZKa = Hecke.NfOrd(B_k)
-    for (p,v) = factor(gcd(discriminant(Zk), fmpz(n))).fac
-      ZKa = pmaximal_overorder(ZKa, p)
-    end
-    ZKa.ismaximal = 1
-    ZKa = lll(ZKa)
-    Hecke._set_maximal_order_of_nf(Ka, ZKa)
     c.Kr = Kr
     c.Ka = Ka
     c.mp = (abs2rel, small2abs)
-    push!(Ac, c)
-    Hecke._set_cyclotomic_ext_nf(k, Ac)
+    if cached
+      push!(Ac, c)
+      Hecke._set_cyclotomic_ext_nf(k, Ac)
+    end
     return c
   end
   
-  if !isone(gcd(discriminant(maximal_order(k)), n))
+  if gcd(degree(fk), degree(k)) != 1 && !isone(gcd(discriminant(maximal_order(k)), n))
     lf = factor(fk)
     fk = first(keys(lf.fac))
   end
@@ -172,44 +199,54 @@ function cyclotomic_extension(k::AnticNumberField, n::Int)
   if degree(fk) != 1
     Ka, abs2rel, small2abs = Hecke.absolute_field(Kr, false)
     
-    # An equation order defined from a factor of a 
-    # cyclotomic polynomial is always maximal by Dedekind
-    # hence the product basis should be maximal at all primes except
-    # for all p dividing the gcd()
-    Zk = maximal_order(k)
-    b_k = basis(Zk, k)
-    B_k = Vector{nf_elem}(undef, degree(Ka))
-    for i = 1:length(b_k)
-      B_k[i] = small2abs(b_k[i])
-    end
-    g = abs2rel\(Kr_gen)
-    s = length(b_k)+1
-    for i = 1:degree(fk)-1
-      for j = 1:degree(k)
-        B_k[s] = B_k[s-degree(k)]*g
-        s += 1
+    if compute_maximal_order
+      # An equation order defined from a factor of a 
+      # cyclotomic polynomial is always maximal by Dedekind
+      # hence the product basis should be maximal at all primes except
+      # for all p dividing the gcd()
+      Zk = maximal_order(k)
+      b_k = basis(Zk, k)
+      B_k = Vector{nf_elem}(undef, degree(Ka))
+      for i = 1:length(b_k)
+        B_k[i] = small2abs(b_k[i])
       end
+      g = abs2rel\(Kr_gen)
+      s = length(b_k)+1
+      for i = 1:degree(fk)-1
+        for j = 1:degree(k)
+          B_k[s] = B_k[s-degree(k)]*g
+          s += 1
+        end
+      end
+      ZKa = Hecke.NfOrd(B_k)
+      for (p, v) in factor(gcd(discriminant(Zk), fmpz(n)))
+        ZKa = pmaximal_overorder_at(ZKa, [p])
+      end
+      if compute_LLL_basis
+        ZKa = lll(ZKa)
+      end
+      ZKa.ismaximal = 1
+      Hecke._set_maximal_order_of_nf(Ka, ZKa)
     end
-    ZKa = Hecke.NfOrd(B_k)
-    for (p, v) in factor(gcd(discriminant(Zk), fmpz(n)))
-      ZKa = pmaximal_overorder_at(ZKa, [p])
-    end
-    ZKa = lll(ZKa)
-    ZKa.ismaximal = 1
-    Hecke._set_maximal_order_of_nf(Ka, ZKa)
   else
     Ka = k
     abs2rel = NfToNfRel(Ka, Kr, gen(Ka), -coeff(fk, 0), Kr(gen(Ka)))
     small2abs = id_hom(k)
-    ZKa = maximal_order(k)
+    if compute_maximal_order
+      Ok = maximal_order(k)
+      if compute_LLL_basis
+        lll(Ok)
+      end
+    end
   end
 
   c.Kr = Kr
   c.Ka = Ka
   c.mp = (abs2rel, small2abs)
-
-  push!(Ac, c)
-  Hecke._set_cyclotomic_ext_nf(k, Ac)
+  if cached
+    push!(Ac, c)
+    Hecke._set_cyclotomic_ext_nf(k, Ac)
+  end
   return c
   
 end
