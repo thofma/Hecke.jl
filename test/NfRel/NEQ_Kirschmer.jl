a1a84a21f465416346a76dfc2d79e3eb3d93b757
Tommy Hofmann <thofma@gmail.com>
2019-09-06 15:51:01 +0200

    Add another test for relative norm equation

--- test/NfRel/NEQ_Kirschmer.jl
+++ test/NfRel/NEQ_Kirschmer.jl
@@ -4,4 +4,9 @@
   kt,t = K["t"]
   E, b = number_field( t^2 + (a+5)* (-2*a+2) )
   @test Hecke.isnorm( E, -a*(-2*a+2) )[1]
+
+  K, a = NumberField(x^2 - 2, "a")
+  Kt, t = K["t"]
+  L, b = number_field(t^2 - 7)
+  @test isnorm(L, K(324))[1]
 end
