f2f6acae607d0ecbf6074b58b8825c97393fd2f7
Tommy Hofmann <thofma@gmail.com>
2020-03-02 12:59:34 +0100

    post relase woes

--- src/Hecke.jl
+++ src/Hecke.jl
@@ -239,7 +239,7 @@ function __init__()
     include("AlgAssRelOrd/NEQ_polymake.jl")
   end
 
-  #@eval global signature(K::AnticNumberField) = _signature(K)
+  @eval global signature(K::AnticNumberField) = _signature(K)
 end
 
 module Globals
@@ -398,7 +398,7 @@ Base.adjoint(x) = transpose(x)
 #
 ################################################################################
 
-global VERSION_NUMBER = v"0.7.3"
+global VERSION_NUMBER = v"0.7.4-dev"
 
 ######################################################################
 # named printing support
