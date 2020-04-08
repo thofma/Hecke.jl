65bf864e56c100673986d6980171fa6788d492ca
Claus Fieker <fieker@mathematik.uni-kl.de>
2020-04-08 11:59:15 +0200

    improve Build.jl

--- system/Build.jl
+++ system/Build.jl
@@ -1,19 +1,9 @@
 using Pkg
-if VERSION >= v"1.4"
-  d = Pkg.dependencies()
-  f = filter(x->occursin("PackageCompiler", x.name), collect(values(d)))
-else
-  d = Pkg.installed()
-  f = filter(x->occursin("PackageCompiler", x), keys(d))
-end
 
-@show f
+Pkg.add("PackageCompiler")
+Pkg.add("Libdl")
 
-if length(f) == 0
-  Pkg.add("PackageCompiler")
-end
-
-using PackageCompiler
+using PackageCompiler, Libdl
 
 f = open("/tmp/CompileHecke.jl", "w")
 println(f, "using Hecke")
@@ -21,10 +11,10 @@ println(f, "using Pkg")
 println(f, "Hecke.test_module(\"runtests\", false)")
 close(f)
 
-PackageCompiler.create_sysimage([:Hecke, :Nemo, :AbstractAlgebra], sysimage_path="/tmp/Hecke.so", precompile_execution_file="/tmp/CompileHecke.jl")        
+PackageCompiler.create_sysimage([:Hecke], sysimage_path="/tmp/Hecke.$(Libdl.dlext)", precompile_execution_file="/tmp/CompileHecke.jl")        
 
 println("(re)start julia as")
-println("\tjulia -J /tmp/Hecke.so")
+println("\tjulia -J /tmp/Hecke.$(Libdl.dlext)")
 
 
 
