048354f5119d89133faa3943cde9fda788dda2b8
Max Horn <max@quendi.de>
2020-04-17 09:58:57 +0200

    Fix has_preimage documentation

--- src/Grp/Morphisms.jl
+++ src/Grp/Morphisms.jl
@@ -28,7 +28,7 @@ end
 
 @doc Markdown.doc"""
     has_preimage(f::GrpGenToGrpGenMor, g::GrpGenElem) -> (b::Bool, h::GrpGenElem)
-Returns whether $f$ has a preimage. If so, the second return value is an
+Returns whether $g$ has a preimage under $f$. If so, the second return value is an
 element $h$ with $f(h) = g$.
 """
 function has_preimage(f::GrpGenToGrpGenMor, g::GrpGenElem)
