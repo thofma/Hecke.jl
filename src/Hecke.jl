b0777b3541886815d6f73176122fc79f45026d83
Claus Fieker <fieker@mathematik.uni-kl.de>
2020-06-24 07:47:17 +0200

    ..do it properly

--- src/Hecke.jl
+++ src/Hecke.jl
@@ -411,13 +411,7 @@ if VERSION >= v"1.4"
     global VERSION_NUMBER = "building"
   end
 else
-  ver = Pkg.installed()
-  if haskey(ver, "Hecke")
-    ver = ver["Hecke"]
-  else
-    ver = 0
-  end
-    
+  Pkg.API.__installed(PKGMODE_MANIFEST)["Hecke"] 
   dir = dirname(@__DIR__)
   if occursin("/dev/", dir)
     global VERSION_NUMBER = VersionNumber("$(ver)-dev")
