3f1dc20aaa0cc986358b9f3e44df06c81726a4ed
Claus Fieker <fieker@mathematik.uni-kl.de>
2020-07-03 15:48:27 +0200

    a (almost) (automatically) working absolute factorisation

--- examples/MPolyFact.jl
+++ examples/MPolyFact.jl
@@ -3,183 +3,14 @@ module MpolyFact
 using Hecke
 import Hecke: Nemo
 
+Hecke.example("mfactor.jl")
+
 function Hecke.lead(f::fmpq_mpoly)
   return first(coeffs(f))
 end
 
 function Hecke.ismonic(f::fmpq_mpoly)
   return isone(lead(f))
-end
-
-function factor_bi(f::fmpq_mpoly, k::Int)
-  P = parent(f)
-  @assert nvars(P) == 2
-  @assert ismonic(f)
-
-  Qt, t = PolynomialRing(base_ring(f), :t, cached = false)
-  s = -1
-  local ff
-  while true
-    s += 1
-    ff = evaluate(f, [t, Qt(s)])
-    if issquarefree(ff)
-      break
-    end
-  end
-
-  lfp = collect(keys(factor(ff).fac))
-  lf = map(x -> x(gens(P)[1]), lfp)
-  la = elem_type(P)[]
-  i = 1
-  n = length(lf)
-  while i < length(lfp)
-    f1 = lfp[i]
-    f2 = lfp[i+1]
-    g, a, b = gcdx(f1, f2)
-    @assert isone(g)
-    push!(la, a(gens(P)[1]))
-    push!(la, b(gens(P)[1]))
-    push!(lfp, f1*f2)
-    push!(lf, lfp[end](gens(P)[1]))
-    i += 2
-  end
-  lf[end] = f
-
-  N = 1
-  mx = k
-  ch = [mx]
-  while ch[end] > N
-    push!(ch, div(ch[end]+1, 2))
-  end
-
-  p = gens(P)[2] - s
-  for k=length(ch)-1:-1:1
-    p2 = (gens(P)[2]-s)^ch[k]
-    i = length(lf)
-    j = i-1
-    while j > 0
-      h = lf[j]
-      g = lf[j-1]
-      b = la[j]
-      a = la[j-1]
-      f = lf[i]
-
-      fgh = divexact(f-g*h, p)
-      G = divrem(fgh*b, g)[2]*p+g
-      H = divrem(fgh*a, h)[2]*p+h
-      t = divexact(1-a*G-b*H, p)
-      B = divrem(t*b, g)[2]*p+b
-      A = divrem(t*a, h)[2]*p+a
-      if i < length(lf)
-        lf[i] = G*H
-      end
-      _, lf[j-1] = divrem(G, p2)
-      _, lf[j] = divrem(H, p2)
-      _, la[j-1] = divrem(A, p2)
-      _, la[j] = divrem(B, p2)
-      i -= 1
-      j -= 2
-    end
-    p = p2
-  end
-  return lf[1:n], s
-end
-
-function factor_bi2(f::MPolyElem, k::Int)
-  P = parent(f)
-  @assert nvars(P) == 2
-
-  Qt, t = PolynomialRing(base_ring(f), :t, cached = false)
-  Qts, s = PolynomialRing(Qt, :s, cached = false)
-  f = evaluate(f, [s, t])
-  @assert ismonic(f)
-
-  s = -1
-  local ff
-  while true
-    s += 1
-    ff = Qt([x(s) for x = coefficients(f)])
-    if issquarefree(ff)
-      break
-    end
-  end
-
-  lfp = collect(keys(factor(ff).fac))
-  lf = map(x -> x(gen(Qts)), lfp)
-  la = typeof(f)[]
-  i = 1
-  n = length(lf)
-  while i < length(lfp)
-    f1 = lfp[i]
-    f2 = lfp[i+1]
-    g, a, b = gcdx(f1, f2)
-    @assert isone(g)
-    push!(la, a(gen(Qts)))
-    push!(la, b(gen(Qts)))
-    push!(lfp, f1*f2)
-    push!(lf, lfp[end](gen(Qts)))
-    i += 2
-  end
-  lf[end] = f
-
-  N = 1
-  mx = k
-  ch = [mx]
-  while ch[end] > N
-    push!(ch, div(ch[end]+1, 2))
-  end
-
-  function _mod(a, b)
-    c = parent(a)()
-    for i=0:degree(a)
-      setcoeff!(c, i, rem(coeff(a, i), b))
-    end
-    return c
-  end
-  function _div(a, b)
-    c = parent(a)()
-    for i=0:degree(a)
-      setcoeff!(c, i, divexact(coeff(a, i), b))
-    end
-    return c
-  end
-
-
-  p = gen(Qt) - s
-  for k=length(ch)-1:-1:1
-    p2 = (gen(Qt)-s)^ch[k]
-    i = length(lf)
-    j = i-1
-    while j > 0
-      h = lf[j]
-      g = lf[j-1]
-      @assert ismonic(g)
-      @assert ismonic(h)
-      b = la[j]
-      a = la[j-1]
-      f = lf[i]
-      @assert ismonic(f)
-
-      fgh = _div(f-g*h, p)
-      G = pseudorem(fgh*b, g)*p+g
-      H = pseudorem(fgh*a, h)*p+h
-      t = _div(1-a*G-b*H, p)
-      B = pseudorem(t*b, g)*p+b
-      A = pseudorem(t*a, h)*p+a
-      if i < length(lf)
-        lf[i] = _mod(G*H, p2)
-      end
-      lf[j-1] = _mod(G, p2)
-      lf[j] = _mod(H, p2)
-      la[j-1] = _mod(A, p2)
-      la[j] = _mod(B, p2)
-      i -= 1
-      j -= 2
-    end
-    p = p2
-  end
-  #now recombinationis missing - and bounds
-  return lf[1:n], s
 end
 
 function (k::Nemo.GaloisField)(a::fmpq)
@@ -190,7 +21,64 @@ function (R::FmpzMPolyRing)(f::fmpq_mpoly)
   return map_coeffs(ZZ, f, parent = R)
 end
 
-function Hecke.roots(f::fmpq_mpoly, p::Int, k::Int)
+function Hecke.representation_matrix(a::ResElem{<:PolyElem})
+  R = parent(a)
+  S = base_ring(base_ring(R))
+  n = degree(modulus(R))
+  m = zero_matrix(S, n, n)
+  for i=0:n-1
+    m[1, i+1] = coeff(a.data, i)
+  end
+  for j=2:n
+    a *= gen(R)
+    for i=0:n-1
+      m[j, i+1] = coeff(a.data, i)
+    end
+  end
+  return m
+end
+
+function Hecke.preimage(phi::Nemo.FinFieldMorphism, x::FinFieldElem)
+  return preimage_map(phi)(x)
+end
+
+function Nemo.canonical_unit(a::SeriesElem) 
+  iszero(a) && return one(parent(a))
+  v = valuation(a)
+  v == 0 && return a
+  v > 0 && return shift_right(a, v)
+  return shift_left(a, -v)
+end
+
+function Base.gcd(a::T, b::T) where {T <: SeriesElem}
+  iszero(a) && iszero(b) && return a
+  iszero(a) && return gen(parent(a))^valuation(b)
+  iszero(b) && return gen(parent(a))^valuation(a)
+  return gen(parent(a))^min(valuation(a), valuation(b))
+end
+
+function Base.lcm(a::T, b::T) where {T <: SeriesElem}
+  iszero(a) && iszero(b) && return a
+  iszero(a) && return a
+  iszero(b) && return b
+  return gen(parent(a))^max(valuation(a), valuation(b))
+end
+
+
+Hecke.inv(phi :: Nemo.FinFieldMorphism) = preimage_map(phi)
+
+#= AbsFact: strategy
+ f in Q[x,y] = Q[x][y], not necc. monic, irre. over Q
+
+ find evaluation x <- x+ly s.th. g = f(0, y) is square-free
+ find p s.th g mod p is square-free and such that the lcm of the factors
+   is small
+ compute the roots up to a bound B over the F_q splitting field
+   as power series
+ compute the coeffs. of f (as elements in Q[x]) as power series
+=#
+
+function Hecke.roots(f::fmpq_mpoly; p::Int=2^25, pr::Int = 5)
   @assert nvars(parent(f)) == 2
 
   #f in Qxy
@@ -198,54 +86,73 @@ function Hecke.roots(f::fmpq_mpoly, p::Int, k::Int)
   ff = map_coeffs(ZZ, f)
   #TODO: 0 might not be a good evaluation point...
   #f needs to be irreducible over Q and g square-free
-  g = evaluate(ff, [Zx(0), gen(Zx)])
+  @show g = evaluate(ff, [gen(Zx), Zx(0)])
   local d
   while true
     p = next_prime(p)
     gp = factor(g, GF(p))
     d = lcm([degree(x) for x = keys(gp.fac)])
     if d < degree(g)/2
+      @show "using $p of degree $d"
       break
     end
   end
-  @show F = QadicField(p, d, k)
-#  F, _ = ResidueField(F)
+  F = FiniteField(p, d)[1]
+  @assert !iszero(discriminant(map_coeffs(F, g)))
+  @time r = Set(roots(g, F))
+  #use action of Frobenius to lift less roots!!!
 
-  @time r = roots(g, F)
+  #all roots with the same minpoly can be combined...
 
-  Ft, t = F["t"]
+  Ft, t = PowerSeriesRing(F, 2^pr, "t")
 
   R = []
-  for s = r
+  while length(r) > 0
+    s = pop!(r)
     o = Ft(inv(evaluate(derivative(g), s)))
     S = Ft(s)
-    ti = t
-    for i = 1:5
-      ti *= ti
-      _g = evaluate(ff, [t, S]) % ti
-      S = (S - _g*o) % ti
-      o = (o*(2-o*evaluate(derivative(ff, 2), [t, S]))) % ti
+    set_prec!(S, 1)
+    set_prec!(t, 2)
+    set_prec!(o, 1)
+    @assert s == coeff(S, 0)
+    @show "lift"
+    @time for i = 1:pr
+      set_prec!(S, 2*precision(S))
+      set_prec!(t, 1+precision(S))
+      set_prec!(o, precision(S))
+      _g = evaluate(ff, [S, t])
+      S = (S - _g*o)
+      o = (o*(2-o*evaluate(derivative(ff, 1), [S, t])))
     end
+    @assert s == coeff(S, 0)
     push!(R, S)
+    T = deepcopy(S)
+    @time for i = 2:degree(minpoly(s))
+      for j=0:T.length-1
+        setcoeff!(T, j, frobenius(coeff(T, j)))
+      end
+      push!(R, deepcopy(T))
+      @assert coeff(T, 0) in r
+      pop!(r, coeff(T, 0))
+    end
   end
   return R
 end
 
-function combination(R::Array, n::Int, d::Int)
+function combination(R::Array, d::Int)
   #R is a lost of roots, ie. polynomials over a q-Adic field
-  #n is the t-adic precision
   #d a bound on the degree in "x" of the factors
   Ft = parent(R[1])
   t = gen(Ft)
-  tn = t^n
-  td = t^d
+  n = precision(R[1])
+  @assert all(x->precision(x) == n, R)
 
   #ps = [[div(x^i % tn, td^i) for i = 1:n] for x = R] 
   
   F = base_ring(Ft)
   k = degree(F)
 
-  if true
+  if !true
     p = prime(F)
     ll = precision(F)
   else
@@ -258,13 +165,13 @@ function combination(R::Array, n::Int, d::Int)
   j = 0
   while true
     @assert n> d*i
-    nn = matrix([[lift(coeff(coeff(div(x^i % tn, td^i), j), lk)) for lk = 0:k-1] for x = R])'
+    @show nn = matrix([[fmpz(coeff(coeff(shift_right(x^i, d*i), j), lk)) for lk = 0:k-1] for x = R])'
     nn = m[:, 1:length(R)]*nn
     m = [m nn; zero_matrix(FlintZZ, ncols(nn), ncols(m)) p^ll*identity_matrix(FlintZZ, ncols(nn))]
     @time r, m = lll_with_removal(m, fmpz(length(R))^2)
-    m = m[1:r, :]
+    @show m = m[1:r, :]
     if all(i->sum(m[i,j]^2 for j = 1:length(R)) <= length(R)^2, 1:r)
-      if all(ll -> sum([div(R[l]^(i+1) % tn, td^(i+1)) for l=1:length(R) if m[ll, l] != 0]) == 0, 1:r)
+      if all(ll -> sum([shift_right(R[l]^(i+1), d*(i+1)) for l=1:length(R) if m[ll, l] != 0]) == 0, 1:r)
         return m[:, 1:length(R)]
       else
         i += 1
@@ -281,7 +188,7 @@ function combination(R::Array, n::Int, d::Int)
   return m[:, 1:length(R)]
 end
 
-function field(R::Array, m::fmpz_mat, d::Int)
+function field(P::fmpq_mpoly, R::Array, m::fmpz_mat)
   #we have roots, we need to combine roots for each row in m where the entry is pm 1
   #the coeffs then live is a number field, meaning that the elem sym functions or
   #the power sums will be needed
@@ -298,56 +205,86 @@ function field(R::Array, m::fmpz_mat, d::Int)
   Ft = parent(R[1])
   F = base_ring(Ft)
   t = gen(Ft)
-  td = t^d
+  d = precision(R[1])
 
-  el = [[sum(R[i]^j % td for i=1:ncols(m) if m[lj, i] != 0) for j=1:d_f] for lj=1:nrows(m)]
+  Qq = QadicField(characteristic(F), nrows(m), 10)
+  k, mk = ResidueField(Qq)
+  F.overfields = Dict{Int64,Array{Nemo.FinFieldMorphism,1}}()
+  F.subfields = Dict{Int64,Array{Nemo.FinFieldMorphism,1}}()
+  k.overfields = Dict{Int64,Array{Nemo.FinFieldMorphism,1}}()
+  k.subfields = Dict{Int64,Array{Nemo.FinFieldMorphism,1}}()
+  phi = embed(k, F)
+
+
+  #TODO given that all powers are used, build them up properly
+  @time el = [[sum(R[i]^j for i=1:ncols(m) if m[lj, i] != 0) for j=1:d_f] for lj=1:nrows(m)]
+
+  kt, t = PolynomialRing(k)
+  kXY, (X, Y) = PolynomialRing(k, ["X", "Y"])
+
+  el = [Hecke.power_sums_to_polynomial([kt([preimage(phi, coeff(x, i)) for i=0:d_f])(Y) for x = y])(X) for y = el]
+
   #assuming one coeff is primtive...
-  i = 1
-  j = 0
-  K, mK = ResidueField(F)
+  j = 1
   local s
   while true
-    s = Set([mK(coeff(x[i], j)) for x = el])
+    s = Set([(coeff(x, j)) for x = el])
     if length(s) == length(el)
       break
     end
     j += 1
-    if j >= d
-      j = 0
-      i += 1
-    end
   end
 
-  @show prime(F), precision(F), i, j
-  pk = prime(F)^precision(F)
-  p = [coeff(sum(coeff(x[i], j)^l for x = el), 0) for l=1:length(el)]
-  p = map(lift, p)
-  p = map(x->rational_reconstruction(x, pk), p)
-  @assert all(x->x[1], p)
-  @show p = [x[2]//x[3] for x = p]
+  @show "$j is primitive"
 
-  k, a = number_field(Hecke.power_sums_to_polynomial(p))
 
-  @show "using", k
+  QqXY, _ = PolynomialRing(Qq, 2)
 
-  m = matrix([[(coeff(x[i], j)^l) for x = el] for l=0:degree(k)-1])
-  kx, x = k["x"]
-  P = elem_type(kx)[]
+  el = [map_coeffs(x->preimage(mk, x), y, parent = QqXY) for y = el]
 
-  for i = 1:d_f
-    q = nf_elem[]
-    for j=0:degree(el[1][i])
-      n = matrix([[coeff(x[i], j)] for x = el])
+  pr = 0
+  while true
+    @show pr += 5
+    el  = Main.MFactor.lift_prime_power(P, el, [0], [degree(P, 2)], pr)
+
+    F = Qq
+    
+    pk = prime(F)^precision(F)
+    p = [coeff(sum(coeff(x, j)^l for x = el), 0) for l=1:length(el)]
+    p = map(lift, p)
+    @show p = map(x->rational_reconstruction(x, pk), p)
+    if !all(x->x[1], p)
+      continue
+    end
+    @show p = [x[2]//x[3] for x = p]
+
+    k, a = number_field(Hecke.power_sums_to_polynomial(p))
+
+    @show "using", k
+
+    m = matrix([[(coeff(x, j)^l) for x = el] for l=0:degree(k)-1])
+    kx, x = k["x"]
+    P = elem_type(kx)[]
+
+
+    kX, (X, Y) = PolynomialRing(k, ["X", "Y"])
+    B = MPolyBuildCtx(kX)
+    for j=1:length(el[1])
+      n = matrix([[coeff(x, j)] for x = el])
       s = solve(m, n')
       @assert all(x->iszero(coeff(s[x, 1], 1)), 1:degree(k))
       s = [rational_reconstruction(lift(coeff(s[i, 1], 0))% pk, pk) for i=1:degree(k)]
-      @assert all(x->x[1], s)
-      push!(q, k([x[2]//x[3] for x = s]))
+      if !all(x->x[1], s)
+        break
+      end
+      push_term!(B, k([x[2]//x[3] for x = s]), exponent_vector(el[1], j))
     end
-    push!(P, kx(q))
+    q = finish(B)
+    if length(q) < length(el[1])
+      continue
+    end
+    return q
   end
-  @show P
-  return Hecke.power_sums_to_polynomial(P)
 end
 
 function absolute_factorisation(f::fmpq_mpoly)
@@ -362,3 +299,24 @@ function absolute_factorisation(f::fmpq_mpoly)
 end
   
 end
+
+#= revised strategy until I actually understand s.th. better
+ - assumming f is monic in y, irreducible over Q and f(0) is squarefree
+ - find a prime s.th. f(0, y) is square-free with a small degree splitting field
+ - compute roots in F_q[[x]] (finite field!)
+ - use combine to find the combinations giving the proper factor
+ - find the (small) field the factor lives over
+ - lift the factor in the q-adic field
+ - find the number field
+
+example:
+
+Qxy, (y, x) = PolynomialRing(QQ, ["y", "x"])
+include("/home/fieker/Downloads/n60s3.m"); 
+  #from  https://www.math.univ-toulouse.fr/~cheze/n60s3.m
+
+  r = MpolyFact(P, pr = 7)
+  c = MpolyFact.combination(r, 21)
+  q = MpolyFact.field(P, R, c)
+
+=#
