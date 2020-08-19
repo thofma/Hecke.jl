90f50d0a4a488b7016916b16e817a4158658c098
thofma <thofma@gmail.com>
2020-08-19 14:44:49 +0200

    Improvements to come example scripts

--- examples/FieldEnumeration/compute_class_groups.jl
+++ examples/FieldEnumeration/compute_class_groups.jl
@@ -1,33 +1,6 @@
-using Hecke, ArgParse
+using ArgParse
 
-set_verbose_level(:ClassGroup, 1)
-
-include(joinpath(Hecke.pkgdir,"examples/FieldEnumeration/FieldEnumeration.jl"))
-
-function has_obviously_not_class_number_one(K::AnticNumberField)
-  lll(maximal_order(K))
-  subs = Hecke.subfields_normal(K)
-  cmfields = []
-  for (k,_) in subs
-    if degree(k) == degree(K)
-      continue
-    end 
-    fl, tau = Hecke.iscm_field(k)
-    if !fl
-      continue
-    end
-    kplus,_ = fixed_field(k, tau)
-    h = order(class_group(lll(maximal_order(k)), use_aut = true)[1])
-    hplus = order(class_group(lll(maximal_order(kplus)))[1])
-    @assert mod(h, hplus) == 0
-    hminus = divexact(h, hplus)
-    if hminus > 4 && (hminus > 1 && gcd(hminus, fmpz(4)) == 1)
-      @show hminus
-      return true
-    end 
-  end 
-  return false
-end
+using Hecke
 
 function ArgParse.parse_item(::Type{fmpz}, x::AbstractString)
   if in('^', x)
@@ -50,11 +23,20 @@ function parse_commandline()
   s = ArgParseSettings()
 
   @add_arg_table s begin
-    "--plus"
-      help = "Compute plus class number"
+    "--only-relative-one"
+      help = "Keep only fields with relative class number one"
       action = :store_true
-    "--minus"
-      help = "Compute minus class number"
+    "--relative-class-number"
+      help = "Compute relative class number"
+      action = :store_true
+    "--class-number"
+      help = "Compute class number"
+      action = :store_true
+    "--automorphism-group"
+      help = "Compute Galois group"
+      action = :store_true
+    "--discriminant"
+      help = "Discriminant"
       action = :store_true
     "--fields"
       help = "List containing the fields"
@@ -65,19 +47,78 @@ function parse_commandline()
   return parse_args(s)
 end
 
-function main()
-  parsed_args = parse_commandline()
+module T
 
-  local plus::Bool
-  local minus::Bool
+if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
+  @eval Base.Experimental.@optlevel 0
+end
+
+using Hecke
+
+using Base.Threads
+
+set_verbose_level(:ClassGroup, 0)
+
+function _fancy_class_number(OK)
+  K = nf(OK)
+  fl, N = Hecke.NormRel.norm_relation(K)
+  if !fl || degree(K) < 20
+    hK = order(class_group(OK, use_aut = true)[1])
+  else
+    hK = order(Hecke.NormRel.class_group_via_brauer(OK, N, recursive = false, compact = false)[1])
+  end
+  return hK
+end
+
+
+function has_obviously_not_relative_class_number_one(K::AnticNumberField)
+  lll(maximal_order(K))
+  subs = Hecke.subfields_normal(K)
+  cmfields = []
+  for (k,_) in subs
+    if degree(k) == degree(K)
+      continue
+    end 
+    fl, tau = Hecke.iscm_field(k)
+    if !fl
+      continue
+    end
+    kplus,_ = fixed_field(k, tau)
+    h = order(class_group(lll(maximal_order(k)), use_aut = true)[1])
+    hplus = order(class_group(lll(maximal_order(kplus)), use_aut = true)[1])
+    @assert mod(h, hplus) == 0
+    hminus = divexact(h, hplus)
+    if hminus > 4 || hminus == 3
+      return true
+    end 
+  end 
+  return false
+end
+
+include(joinpath(Hecke.pkgdir,"examples/FieldEnumeration/FieldEnumeration.jl"))
+
+function main()
+  parsed_args = Main.parse_commandline()
+
+  local _only_relative_one::Bool
+  local _relative_class_number::Bool
+  local _class_number::Bool
+  local _automorphism_group::Bool
+  local _discriminant::Bool
   local fieldsfile::String
 
   for (arg, val) in parsed_args
     println("$arg => $val")
-    if arg == "plus"
-      plus = val
-    elseif arg == "minus"
-      minus = val
+    if arg == "only-relative-one"
+      _only_relative_one = val
+    elseif arg == "relative-class-number"
+      _relative_class_number = val
+    elseif arg == "class-number"
+      _class_number = val
+    elseif arg == "automorphism-group"
+      _automorphism_group = val
+    elseif arg == "discriminant"
+      _discriminant = val
     elseif arg == "fields"
       fieldsfile = val
     end
@@ -85,19 +126,7 @@ function main()
 
   sprint_formatted(fmt, args...) = @eval @sprintf($fmt, $(args...))
 
-  if plus && !minus
-    outfile = "class_numbers_h_hp_" * fieldsfile
-  elseif !plus && minus
-    outfile = "class_numbers_h_hm_" * fieldsfile
-  elseif plus && minus
-    outfile = "class_numbers_h_hp_hm_" * fieldsfile
-  elseif !plus && !minus
-    outfile = "class_numbers_h_" * fieldsfile
-  end
-
-  @show plus
-  @show minus
-  @show fieldsfile
+  outfile = splitext(fieldsfile)[1] * ".nfdb"
 
   if !isfile(fieldsfile)
     throw(error("File $fields does not exist"))
@@ -117,73 +146,81 @@ function main()
 
   # I change fields inplace
 
-  for i in 1:length(fields)
+  res = Dict{Int, Hecke.NFDBRecord{1}}()
+
+  @threads for i in 1:1#length(fields)
     f = fields[i][1]
     K, = number_field(f, cached = false)
     println("Looking at $i/$(length(fields)) ($f)")
+    flush(stdout)
+    OK = lll(maximal_order(K))
+    @assert discriminant(OK) == fields[i][end]
+    println("Investigating field $i/$(length(fields)) ($f)")
 
-    local res
-    
-    if minus || plus
-      println("Doing the cheap test $i/$(length(fields))")
-      flush(stdout)
-      fl = has_obviously_not_class_number_one(K)
-      if fl
-        println("Not relative class number one")
-        flush(stdout)
-        res = [fmpz(0), fmpz(0), fmpz(0)]
-        result[i] = (fields[i]..., res...)
+    hK = fmpz(0)
+    hrelative = fmpz(0)
+
+    if _only_relative_one
+      if has_obviously_not_relative_class_number_one(K)
+        continue
+      end
+      hK = _fancy_class_number(OK)
+      fl, tau = Hecke.iscm_field(K)
+      k, = fixed_field(K, tau, simplify = true)
+      Ok = lll(maximal_order(k))
+      hk = _fancy_class_number(Ok)
+      @assert mod(hK, hk) == 0
+      hrelative = divexact(hK, hk)
+      if hrelative != 1
         continue
       end
     end
 
-    flush(stdout)
-    OK = lll(maximal_order(K))
-    @assert discriminant(OK) == fields[i][end]
-    println("Computing class group number $i/$(length(fields)) ($f)")
-    flush(stdout)
-    c, _ = class_group(OK, use_aut = true)
-    h = order(c)
-    res = [h]
-    if minus || plus
-      fl, tau = Hecke.iscm_field(K)
-      println("Computing fixed field")
-      k, = fixed_field(K, tau, simplify = true)
-      println("Computing class group number $i/$(length(fields)) ($(defining_polynomial(k)))")
-      flush(stdout)
-      cc, = class_group(lll(maximal_order(k)))
-      hplus = order(cc)
-      @assert mod(h, hplus) == 0
-      hminus = divexact(h, hplus)
-      if plus
-        push!(res, hplus)
+    r = Hecke._create_record(K)
+
+    if _relative_class_number
+      if hrelative == 0 
+        hK = _fancy_class_number(OK)
+        fl, tau = Hecke.iscm_field(K)
+        k, = fixed_field(K, tau, simplify = true)
+        Ok = lll(maximal_order(k))
+        hk = _fancy_class_number(Ok)
+        @assert mod(hK, hk) == 0
+        hrelative = divexact(hK, hk)
       end
-      if minus
-        push!(res, hminus)
-      end
+
+      r[:relative_class_number] = hK
     end
-    result[i] = (fields[i]..., res...)
+
+    if _class_number
+      if hK == 0
+        hK = _fancy_class_number(OK)
+      end
+
+      r[:class_number] = hK
+    end
+
+    if _automorphism_group
+      r[:automorphism_group] = find_small_group(automorphism_group(K)[1])[1]
+    end
+
+    if _discriminant
+      r[:discriminant] = discriminant(OK)
+    end
+
+    res[i] = r
   end
 
-  println("Writing to $outfile...")
+  nfdb = Hecke.NFDB([res[i] for i in 1:length(res)])
+
+  Hecke.add_meta!(nfdb, :title => "Database of $fieldsfile (only relative class number one $_only_relative_one")
 
   f = open(outfile, "a")
 
-  for L in result
-    z = (fmpq[ coeff(L[1], i) for i in 0:degree(L[1]) ],)
-    i = -1
-    if nonsimpleincluded
-      y = Vector{fmpq}[fmpq[coeff(g, i) for i=0:degree(g)] for g in L[2]]
-      zz = (z, y, L[3])
-      i = 4
-    else
-      zz = (z, L[2])
-      i = 3
-    end
-    zzz = (zz..., L[i:end]...)
-    Base.write(f, "$zzz\n")
-  end
+  write(f, nfdb)
+
   close(f)
 end
+end
 
-main()
+T.main()
