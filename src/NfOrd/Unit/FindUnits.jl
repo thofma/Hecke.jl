eb6d481bf2ebdb87753caba86192885a7837b42c
Carlo Sircana <sircana@mathematik.uni-kl.de>
2020-09-01 15:21:34 +0200

    new strategy for orbits

--- src/NfOrd/Unit/FindUnits.jl
+++ src/NfOrd/Unit/FindUnits.jl
@@ -10,174 +10,6 @@ function _unit_group(O::NfOrd, c::ClassGrpCtx)
   return u
 end
 
-# TH:
-# Current strategy
-# ================
-# Compute some "random" kernel elements using the transformation matrix to first
-# find r independent units, where r is the unit rank.
-# In the second round, try to enlarge the unit group with some random kernel
-# elements.
-function _unit_group_find_units_with_transform(u::UnitGrpCtx, x::ClassGrpCtx; add_orbit::Bool = false)
-  @assert false
-
-  @vprint :UnitGroup 1 "Processing ClassGrpCtx to find units ... \n"
-
-  @vprint :UnitGroup 1 "Relation module  $(x.M)\n"
-
-  O = order(u)
-
-  #ker = transpose(ker)
-
-  K = nf(order(x.FB.ideals[1]))
-  r = unit_rank(O)
-  r1, r2 = signature(O)
-
-  A = u.units
-
-  j = 0
-
-  dim_ker = length(x.M.rel_gens.rows) # dimension of the kernel
-
-  total_dim = length(x.M.bas_gens.rows) + dim_ker
-
-  kelem = fmpz[ 0 for i in 1:total_dim ]
-
-  trafos = x.M.trafo
-
-  MAX_RND_RD_1 = 2*r
-
-  time_indep = 0.0
-  time_add_dep_unit = 0.0
-  time_kernel = 0.0
-  time_torsion = 0.0
-
-  while(length(A) < r)
-
-    @assert length(kelem) == length(x.R_gen) + length(x.R_rel)
-
-    for i in 1:length(kelem)
-      kelem[i] = 0
-    end
-
-    if j > MAX_RND_RD_1
-      return 0
-    end
-    @vprint :UnitGroup 1 "Found $(length(A)) independent units so far ($(r - length(A)) left to find)\n"
-    j = j + 1
-
-    for i in 1:dim_ker
-      kelem[end - i + 1] = rand(0:1)
-    end
-
-    time_kernel += @elapsed for i in length(trafos):-1:1
-      apply_right!(kelem, trafos[i])
-    end
-
-    _make_row_primitive!(kelem)
-
-    @assert length(kelem) == length(x.R_gen) + length(x.R_rel)
-
-    y = FacElem(vcat(x.R_gen, x.R_rel), kelem)
-
-    time_torsion += @elapsed is_tors, p = istorsion_unit(y, false, u.tors_prec)
-    u.tors_prec = max(p, u.tors_prec)
-    if is_tors
-      continue
-    end
-
-    @vprint :UnitGroup 1 "Exponents are of bit size $(maximum([ nbits(o) for o in kelem]))\n"
-
-    if add_orbit
-      for phi in aut
-        time_indep += @elapsed _add_unit(u, phi(y))
-      end
-    else
-      time_indep += @elapsed _add_unit(u, y)
-    end
-  end
-
-  @vprint :UnitGroup 1 "Found $r linear independent units \n"
-
-  @vprint :UnitGroup 1 "Independent unit time: $time_indep\n"
-  @vprint :UnitGroup 1 "Adding dependent unit time: $time_add_dep_unit\n"
-  @vprint :UnitGroup 1 "Torsion test time: $time_torsion\n"
-  @vprint :UnitGroup 1 "Kernel time: $time_kernel\n"
-
-  u.full_rank = true
-
-  u.units = reduce(u.units, u.tors_prec)
-
-  not_larger = 0
-
-  @vprint :UnitGroup 1 "Enlarging unit group by adding more kernel basis elements ...\n"
-  while not_larger < 3 
-
-    @assert length(kelem) == length(x.R_gen) + length(x.R_rel)
-
-    for i in 1:length(kelem)
-      kelem[i] = 0
-    end
-
-    for i in 1:dim_ker
-      kelem[end - i + 1] = rand(-2:2)
-    end
-
-    time_kernel += @elapsed for i in length(trafos):-1:1
-      apply_right!(kelem, trafos[i])
-    end
-
-    @assert length(kelem) == length(x.R_gen) + length(x.R_rel)
-
-    y = FacElem(vcat(x.R_gen, x.R_rel), kelem)
-
-    @hassert :UnitGroup 2 _isunit(y)
-    @vprint :UnitGroup 2 "Reduce 1st...\n"
-    y = reduce_mod_units([y], u)[1]
-
-    @vprint :UnitGroup 2 "Test if kernel element yields torsion unit ... \n"
-    @v_do :UnitGroup 2 pushindent()
-    time_torsion += @elapsed is_tors, p = istorsion_unit(y, false, u.tors_prec)
-    @v_do :UnitGroup 2 popindent()
-    u.tors_prec = max(p, u.tors_prec)
-    if is_tors
-      @vprint :UnitGroup 2 "Element is torsion unit\n"
-      not_larger = not_larger + 1
-      continue
-    end
-
-    @v_do :UnitGroup 2 pushindent()
-    if add_orbit
-      for phi in aut
-        time_add_dep_unit += @elapsed m = _add_dependent_unit(u, phi(y))::Bool
-      end
-    else
-      time_add_dep_unit += @elapsed m = _add_dependent_unit(u, y)::Bool
-    end
-    @v_do :UnitGroup 2 popindent()
-
-    if !m
-      not_larger = not_larger + 1
-    else
-      not_larger = 0
-    end
-  end
-  #final reduction ...
-  u.units = reduce(u.units, u.tors_prec)
-
-  u.tentative_regulator = regulator(u.units, 64)
-
-  @vprint :UnitGroup 1 "Finished processing\n"
-  @vprint :UnitGroup 1 "Regulator of current unit group is $(u.tentative_regulator)\n"
-  @vprint :UnitGroup 1 "-"^80 * "\n"
-  @vprint :UnitGroup 1 "Independent unit time: $time_indep\n"
-  @vprint :UnitGroup 1 "Adding dependent unit time: $time_add_dep_unit\n"
-  @vprint :UnitGroup 1 "Torsion test time: $time_torsion\n"
-  @vprint :UnitGroup 1 "Kernel time: $time_kernel\n"
-  @vtime_add :UnitGroup 1 x :unit_hnf_time time_kernel
-  return 1
-end
-
-
 function find_candidates(x::ClassGrpCtx, u::UnitGrpCtx, add::Int = 0)
   time_kernel = 0.0
   add_units = Int[]
@@ -254,24 +86,9 @@ function _unit_group_find_units(u::UnitGrpCtx, x::ClassGrpCtx; add_orbit::Bool =
     return 0
   end
 
-
-
-  r1, r2 = signature(O)
-
-  A = u.units
-
-  j = 0
-
-  MAX_RND_RD_1 = 2*r
-
   time_indep = 0.0
   time_add_dep_unit = 0.0
-  
   time_torsion = 0.0
-
-
-  j = 0
-
   not_larger = 0
 
   @vprint :UnitGroup 1 "Enlarging unit group by adding kernel elements ...\n"
@@ -282,12 +99,26 @@ function _unit_group_find_units(u::UnitGrpCtx, x::ClassGrpCtx; add_orbit::Bool =
   add = 0
   if has_full_rank(u)
     first = false
-    add += 2
+    add += 2 #+ div(nrows(x.M.rel_gens), r)
   end
   if !isdefined(u, :relations_used)
     u.relations_used = Vector{Int}()
   end
   finished = false
+
+  if add_orbit
+    aut = automorphisms(u)
+    gens_aut = small_generating_set(aut)
+    indices_aut = Int[]
+    for s = 1:length(gens_aut)
+      for j = 1:length(aut)
+        if aut[j] == gens_aut[s]
+          push!(indices_aut, j)
+          break
+        end
+      end
+    end
+  end
 
   
   while not_larger < not_larger_bound 
@@ -365,6 +196,15 @@ function _unit_group_find_units(u::UnitGrpCtx, x::ClassGrpCtx; add_orbit::Bool =
             finished = true
             break
           end
+          if add_orbit
+            for j  in indices_aut
+              phiu = apply_automorphism(u, j, y)
+              if add_unit!(u, phiu)
+                @vprint :UnitGroup 1 "New regulator: $(tentative_regulator(u))\n"
+                not_larger = 0
+              end
+            end
+          end
         else
           @vprint :UnitGroup 1 "Increased rank by 1 (now $(rank(u)))\n"
         end
@@ -381,40 +221,7 @@ function _unit_group_find_units(u::UnitGrpCtx, x::ClassGrpCtx; add_orbit::Bool =
 
       @v_do :UnitGroup 2 popindent()
     end
-    if add_orbit && rank(u) > r-div(r, 4)
-      @vprint :UnitGroup 1 "Adding orbits\n"
-      # I close the units via Galois action
-      aut = automorphisms(u)
-      for i = 1:length(u.units)
-        threshold = 3
-        for j = 1:length(aut)
-          if aut[j] == id_hom(K)
-            continue
-          end
-          phiu = apply_automorphism(u, j, u.units[i])
-          if !add_unit!(u, phiu)
-            not_larger += 1
-            threshold -= 1
-          else
-            if has_full_rank(u)
-	            @vprint :UnitGroup 1 "New regulator: $(tentative_regulator(u))\n"
-            end
-	          not_larger = 0
-          end
-          if iszero(threshold)
-            break
-          end
-        end
-        if not_larger > not_larger_bound
-          break
-        end
-        if nrows(x.M.rel_gens) == not_larger_bound
-          finished = true
-          break
-        end
-      end
-    end
-    
+       
     if has_full_rank(u)
       add_done = false
       for i = 1:length(elements)
