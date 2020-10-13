48928f075c0e230ddb7e78875b136e351c56cb06
Hpetri <54960839+Pi-tree@users.noreply.github.com>
2020-10-13 11:48:29 +0200

    Update HNF.jl

--- src/Sparse/HNF.jl
+++ src/Sparse/HNF.jl
@@ -373,6 +373,7 @@ function reduce_right(A::SMat{fmpz}, b::SRow{fmpz},
                       start::Int = 1, trafo::Type{Val{N}} = Val{false}) where N
   with_transform = (trafo == Val{true})
   with_transform ? trafos = [] : nothing
+  new = true
   if length(b.pos) == 0
     with_transform ? (return b, trafos) : return b
   end
@@ -400,7 +401,13 @@ function reduce_right(A::SMat{fmpz}, b::SRow{fmpz},
         @hassert :HNF 1  r >= 0
       end
       if q != 0
-        b = Hecke.add_scaled_row(A[p], b, -q)
+        if new
+          b = Hecke.add_scaled_row(A[p], b, -q)
+          new = false
+        else
+          Hecke.add_scaled_row!(A[p], b, -q)
+        end
+
         with_transform ? push!(trafos, sparse_trafo_add_scaled(p, nrows(A) + 1, -q)) : nothing
         if r == 0
           j -= 1
@@ -478,7 +485,7 @@ function hnf_extend!(A::SMat{fmpz}, b::SMat{fmpz}, trafo::Type{Val{N}} = Val{fal
       if nc % 10 == 0
         println("Now at $nc rows of $(nrows(b)), HNF so far $(nrows(A)) rows")
         println("Current density: $(density(A))")
-        println("and size of largest entry: $(nbits(maximum(abs, A))) bits")
+        println("and size of largest entry: $(nbits(maximum(abs, A))) bits $(sum(nbits, A))")
       end
     end
     nc += 1
@@ -496,6 +503,9 @@ function hnf_extend!(A::SMat{fmpz}, b::SMat{fmpz}, trafo::Type{Val{N}} = Val{fal
   with_transform ? (return A, trafos) : (return A)
 end
 
+function nbits(s::SRow{fmpz})
+  return sum(nbits, s.values)
+end
 
 @doc Markdown.doc"""
     hnf_kannan_bachem(A::SMat{fmpz}) -> SMat{fmpz}
@@ -586,10 +596,46 @@ end
 
 Inplace transform of $A$ into upper right Hermite normal form.
 """
-function hnf!(A::SMat{fmpz})
-  B = hnf(A)
+function hnf!(A::SMat{fmpz}; truncate::Bool = false)
+  B = hnf(A, truncate = truncate)
   A.rows = B.rows
   A.nnz = B.nnz
   A.r = B.r
   A.c = B.c
+end
+
+
+
+function reduce_right!(A::SMat{fmpz}, b::SRow{fmpz})
+  if length(b.pos) == 0
+    return b
+  end
+  j = 1
+  p = find_row_starting_with(A, b.pos[j])
+  if p > nrows(A)
+    return b
+  end
+  while j <= length(b.pos)
+    while p < nrows(A) && A[p].pos[1] < b.pos[j]
+      p += 1
+    end
+    if A[p].pos[1] == b.pos[j]
+      q, r = divrem(b.values[j], A[p].values[1])
+      if r < 0
+        q -= 1
+        r += A[p].values[1]
+        @hassert :HNF 1 r >= 0
+      end
+      if q != 0
+        Hecke.add_scaled_row!(A[p], b, -q)
+        if r == 0
+          j -= 1
+        else
+          @hassert :HNF 1 b.values[j] == r
+        end
+      end
+    end
+    j += 1
+  end
+  return b
 end
