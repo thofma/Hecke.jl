e74cd9d0328ad7814a676631df1f1360e3fa0571
Carlo Sircana <sircana@mathematik.uni-kl.de>
2020-10-19 13:47:47 +0200

    some new files

---
+++ src/NumField/NfAbs/ConjugatesNS.jl
@@ -0,0 +1,141 @@
+function conjugate_data_arb_roots(K::NfAbsNS, p::Int)
+
+  pols = fmpq_poly[isunivariate(x)[2] for x in K.pol]
+  ctxs = acb_root_ctx[acb_root_ctx(x) for x in pols]
+  acb_roots_vec = Vector{acb_roots}(undef, length(ctxs))
+  for i = 1:length(ctxs)
+    c = ctxs[i]
+    while c.prec < p
+      refine(c)
+    end
+    all_roots = deepcopy(c.roots)
+    real_roots = deepcopy(c.real_roots)
+    complex_roots = deepcopy(c.complex_roots)
+    for z in all_roots
+      expand!(z, -p)
+    end
+    for z in real_roots
+      expand!(z, -p)
+    end
+    for z in complex_roots
+      expand!(z, -p)
+    end
+    acb_roots_vec[i] = acb_roots(p, all_roots, real_roots, complex_roots)
+  end
+  ind_real, ind_complex = enumerate_conj_prim(acb_roots_vec)
+  return acb_roots_vec, ind_real, ind_complex
+
+end
+
+function enumerate_conj_prim(v::Vector{acb_roots})
+  indices = Tuple[]
+  for i in CartesianIndices(Tuple(1:length(v[i].roots) for i in 1:length(v)))
+    push!(indices, i)
+  end
+  #I have the indices, now I need to order them.
+  complex_indices = Int[]
+  for i = 1:length(v)
+    indc = length(v[i].real_roots)+1
+    push!(complex_indices, indc)
+  end
+  real_combinations = Int[]
+  for i = 1:length(indices)
+    isreal = true
+    for j = 1:length(indices[i])
+      if indices[i][j] >= complex_indices[j]
+        isreal = false
+        break
+      end
+    end
+    if isreal
+      push!(real_combinations, i)
+    end
+  end
+  res_real = indices[real_combinations]
+  res_complex = typeof(indices)()
+  for i = 1:length(indices)
+    if i in real_combinations
+      continue
+    end
+    s = indices[i]
+    ind_complex = Int[]
+    for t = 1:length(s)
+      if s[t] >= complex_indices[t]
+        push!(ind_complex, t)
+      end
+    end
+    found = false
+    for t = 1:length(res_complex)
+      found = _is_complex_conj(res_complex[t], s, ind_complex, v)
+      if found
+        break
+      end
+    end
+    if found
+      continue
+    end
+
+    push!(res_complex, indices[i])
+  end
+  return res_real, res_complex
+end
+
+function _is_complex_conj(v::Tuple, w::Tuple, pos::Vector, roots::Vector)
+  i = 1
+  for x in v
+    if i in pos
+      if v[i] <= length(roots[i].real_roots)
+        return false
+      end
+      lc = length(roots[i].complex_roots)
+      if v[i] != w[i] + lc && v[i] != w[i] - lc
+        return false
+      end
+    elseif v[i] != w[i]
+      return false
+    end
+    i += 1    
+  end
+  return true
+end
+
+
+function conjugates_arb(a::NfAbsNSElem, p::Int, work_tol::Int = p)
+
+  K = parent(a)
+  conjs, ind_real, ind_complex = conjugate_data_arb_roots(K, work_tol)
+  res = Vector{acb}(undef, degree(K))
+  pol_a = data(a)
+  r = length(ind_real)
+  s = div(degree(K) - r, 2)
+  for i = 1:r
+    res[i] = evaluate(pol_a, acb[conjs[j].roots[ind_real[i][j]] for j = 1:ngens(K)])
+    if !isfinite(res[i]) || !radiuslttwopower(res[i], -p)
+      return conjugates_arb(a, p, 2*p)
+    end
+  end
+  ind = r+1
+  for i = 1:length(ind_complex)
+    ev = acb[conjs[j].roots[ind_complex[i][j]] for j = 1:ngens(K)]
+    res[ind] = evaluate(pol_a, ev)
+    if !isfinite(res[ind]) || !radiuslttwopower(res[ind], -p)
+      return conjugates_arb(a, p, 2*p)
+    end
+    res[ind+s] = conj(res[ind])
+    ind += 1
+  end
+  return res
+end
+
+
+function signature(K::NfAbsNS)
+  if K.signature[1] != -1
+    return K.signature
+  end
+  signatures = Tuple{Int, Int}[signature(isunivariate(f)[2]) for f in K.pol]
+  r = prod(x[1] for x in signatures)
+  s = div(degree(K) - r, 2)
+  K.signature = (r, s)
+  return (r, s)
+end
+
---
+++ src/NumField/NfRel/Simplify.jl
@@ -0,0 +1,176 @@
+################################################################################
+#
+#  Simplify
+#
+################################################################################
+
+function simplify(K::NfRel{nf_elem}; cached::Bool = true, prec::Int = 100)
+  Kabs, mK, mk = absolute_field(K, cached = false)
+  OK = maximal_order(K)
+  new_basis = Vector{nf_elem}(undef, degree(Kabs))
+  B = pseudo_basis(OK)
+  ideals = Dict{NfOrdIdl, Vector{nf_elem}}()
+  for i = 1:length(B)
+    I = B[i][2].num
+    if !haskey(ideals, I)
+      bas = lll_basis(I)
+      ideals[I] = nf_elem[mK\(K(x)) for x in bas]
+    end
+  end
+  ind = 1
+  for i = 1:degree(OK)
+    I = B[i][2]
+    bI = ideals[I.num]
+    el = mK\(B[i][1])
+    for j = 1:length(bI)
+      new_basis[ind] = divexact(el*bI[j], I.den)
+      ind += 1
+    end
+  end
+  O = NfOrd(new_basis)
+  O.ismaximal = 1
+  O.disc = absolute_discriminant(OK)
+  if prec == 100
+    OLLL = lll(O)
+  else
+    OLLL = lll(O, prec = prec)
+  end
+  el = _simplify(OLLL, mK)
+  pel = mK(el)
+  f = minpoly(pel)
+  Ks = number_field(f, cached = cached, check = false)[1]
+  mKs = hom(Ks, K, pel)
+  return Ks, mKs
+end
+
+#Finds a small elements given by small combinations of the basis of O 
+#that generates the extension given by mK
+function _simplify(O::NfOrd, mK::NfToNfRel)
+
+  L = nf(O)
+  #First, we choose the candidates
+  B = basis(O, L)
+  a = gen(L)
+  Bnew = nf_elem[]
+  nrep = min(3, degree(L))
+  for i = 1:length(B)
+    push!(Bnew, B[i])
+    for j = 1:nrep
+      push!(Bnew, B[i]+B[j])
+      push!(Bnew, B[i]-B[j])
+    end
+  end
+
+  #Now, we test for primitiveness.
+  Lrel = codomain(mK)
+  OLrel = maximal_order(Lrel)
+  K = base_field(Lrel)
+  OK = maximal_order(K)
+
+  n = degree(Lrel)
+
+  P, d = _find_prime(Lrel)
+  p = minimum(P, copy = false)
+  abs_deg = degree(P)*d
+  FP, mFP = ResidueField(OK, P)
+  mFP1 = extend_easy(mFP, K)
+  #First, we search for elements that are primitive using block systems
+  F = FlintFiniteField(p, abs_deg, "w", cached = false)[1]
+  emb = find_embedding(FP, F)
+  rt = roots(map_coeffs(emb, map_coeffs(mFP1, Lrel.pol)))
+``
+  Fp = GF(p, cached =  false)
+  x = PolynomialRing(Fp, "x", cached = false)[2]
+  indices = Int[]
+  for i = 1:length(Bnew)
+    if isone(denominator(Bnew[i]))
+      continue
+    end
+    b = _block(Bnew[i], rt, x)
+    if length(b) == n
+      push!(indices, i)
+    end
+  end
+  #Now, we select the one of smallest T2 norm
+  I = t2(a)
+  for i = 1:length(indices)
+    t2n = t2(Bnew[indices[i]])
+    if t2n < I
+      a = Bnew[indices[i]]
+      I = t2n
+    end
+  end
+  return a
+end
+
+function find_embedding(F::FqFiniteField, K::FqFiniteField)
+  f = defining_polynomial(F)
+  rt = roots(f, K)
+  img = rt[1]
+  return (x -> sum([coeff(x, i)*img^i for i = 0:degree(F)-1]))
+end
+
+
+function _block(a::nf_elem, rt::Vector{fq}, tmp::gfp_fmpz_poly)
+  nf_elem_to_gfp_fmpz_poly!(tmp, a, false) # ignore denominator
+  evs = fq[evaluate(tmp, x) for x in rt]
+  b = Vector{Int}[]
+  a = BitSet()
+  i = 0
+  n = length(rt)
+  while i < n
+    i += 1
+    if i in a
+      continue
+    end
+    z = evs[i]
+    push!(b, findall(x-> evs[x] == z, 1:n))
+    for j in b[end]
+      push!(a, j)
+    end
+  end
+  return b
+end
+
+function _find_prime(L::NfRel{nf_elem})
+  p = 2^10
+  K = base_field(L)
+  OK = maximal_order(K)
+  OL = maximal_order(L)
+
+  n_attempts = min(degree(L), 10)
+  candidates = Vector{Tuple{NfOrdIdl, Int}}(undef, n_attempts)
+  i = 1
+  f = L.pol
+  threshold = degree(f)^2
+  while i < n_attempts+1
+    p = next_prime(p)
+    if isindex_divisor(OK, p)
+      continue
+    end
+    lp = prime_decomposition(OK, p)
+    P = lp[1][1]
+    if isindex_divisor(OL, P)
+      continue
+    end
+    F, mF = ResidueField(OK, P)
+    mF1 = extend_easy(mF, K)
+    fF = map_coeffs(mF1, f)
+    if degree(fF) != degree(f) || !issquarefree(fF)
+      continue
+    end
+    FS = factor_shape(fF)
+    d = lcm(Int[x for (x, v) in FS])
+    if d < threshold
+      candidates[i] = (P, d)
+      i += 1
+    end
+  end
+  res =  candidates[1]
+  for j = 2:n_attempts
+    if candidates[j][2] < res[2]
+      res = candidates[j]
+    end
+  end
+  return res[1], res[2]
+end
