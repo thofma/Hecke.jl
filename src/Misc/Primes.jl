90e6b65aaa8e945109d9b8a614ecc1d36171b638
Claus Fieker <fieker@mathematik.uni-kl.de>
2020-12-11 11:38:01 +0100

    reduce the val in PriemsSet

--- src/Misc/Primes.jl
+++ src/Misc/Primes.jl
@@ -117,7 +117,7 @@ struct PrimesSet{T}
       p = next_prime(p, false)
     end
     mod = abs(mod)
-    val = abs(val)
+    val = abs(val % mod)
     if mod == 1 && val == 0
       return new{T}(f, t, false, false, mod, val, sv)
     end
