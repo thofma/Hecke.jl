0722815255c25892c83219922222252f9d79d732
Claus Fieker <fieker@mathematik.uni-kl.de>
2021-01-13 09:15:56 +0100

    reorganize, add fmpz_poly

--- src/Misc/toPari.jl
+++ src/Misc/toPari.jl
@@ -17,9 +17,9 @@ function toPari(io::IO, f::Generic.Poly{nf_elem})
   nothing
 end
 
-function toPari(s::String, f::Generic.Poly{nf_elem})
+function toPari(s::String, a)
   g = open(s, "w")
-  toPari(g, f)
+  toPari(g, a)
   close(g)
 end
 
@@ -36,12 +36,10 @@ function toPari(f::IO, m::fmpz_mat)
   print(f, "];")
 end
 
-function toPari(s::String, m::fmpz_mat)
-  g = open(s, "w")
-  toPari(g, m)
-  close(g)
+function toPari(io::IO, f::fmpz_poly)
+  print(io, "f = $f;\n")
+  nothing
 end
-
 
 # gp> #
 # gp> read("bla");
@@ -51,27 +49,31 @@ end
 from 
   https://www.math.u-bordeaux.fr/~kbelabas/research/vanhoeij.pdf
   p 26, polys h_1, ..., h_10 (so far)
-x-1
-x^2-19
-x^3-x^2+6
-x^4 - 2*x^3 - 7*x^2 - 7*x + 3
-x^5 - 3*x^3 - 2*x^2+8*x-10
-x^6 - 5*x^4 - x^3 + 10*x^2 - 11*x + 5
-x^7-x^6-32*x^5-100*x^4-130*x^3-70*x^2+ 4*x+ 23
-x^8-8*x^6-x^5+x^3+ 3*x^2-7*x+ 2
-x^9-x^8-42*x^7-180*x^6-375*x^5-452*x^4-327*x^3-133*x^2-31*x-11
-x^10-2*x^9+ 5*x^8+ 8*x^7+ 5*x^6+ 8*x^5-9*x^4-6*x^3+ 7*x^2+ 4*x+ 8
-
 
 Zx, x = PolynomialRing(FlintZZ)
+l = [x-1,
+x^2-19,
+x^3-x^2+6,
+x^4 - 2*x^3 - 7*x^2 - 7*x + 3,
+x^5 - 3*x^3 - 2*x^2+8*x-10,
+x^6 - 5*x^4 - x^3 + 10*x^2 - 11*x + 5,
+x^7-x^6-32*x^5-100*x^4-130*x^3-70*x^2+ 4*x+ 23,
+x^8-8*x^6-x^5+x^3+ 3*x^2-7*x+ 2,
+x^9-x^8-42*x^7-180*x^6-375*x^5-452*x^4-327*x^3-133*x^2-31*x-11,
+x^10-2*x^9+ 5*x^8+ 8*x^7+ 5*x^6+ 8*x^5-9*x^4-6*x^3+ 7*x^2+ 4*x+ 8,
+]
+
+
 g1 = swinnerton_dyer(7, x);
 g2 = swinnerton_dyer(8, x);
 
-k, a = number_field()
-kt, t = k["t"]
+for f = l
+  k, a = number_field(f)
+  kt, t = k["t"]
 
-g = (g1*g2)(t-a)
-@time length(factor(g))
+  g = (g1*g2)(t-a)
+  @time length(factor(g))
+end
 
 needs prec_scale of 40 rather than 20. Why I don't know (yet)
 
