b4dce5918441341d0191e7129ada9290ff9900c8
thofma <thofma@gmail.com>
2021-01-26 21:15:05 +0100

    More lattice tests (#139)

---
+++ test/QuadForm/Herm/Spaces.jl
@@ -0,0 +1,18 @@
+@testset "Spaces" begin
+  Qx, x = PolynomialRing(FlintQQ, "x")
+  K, a = NumberField(x^2 - 2, "a1")
+  Kt, t = K["t"]
+
+  E, b = NumberField(t^2 + 3)
+
+  F = GF(3)
+
+  Hecke.change_base_ring(::Hecke.NfRel, ::Hecke.gfp_mat) = error("asd")
+  @test_throws ErrorException hermitian_space(E, F[1 2; 2 1])
+
+  Hecke.change_base_ring(::Hecke.NfRel, x::Hecke.gfp_mat) = x
+  @test_throws ErrorException hermitian_space(E, F[1 2; 2 1])
+
+  V = @inferred hermitian_space(E, FlintQQ[1 2; 2 1])
+  @test V isa Hecke.HermSpace
+end
