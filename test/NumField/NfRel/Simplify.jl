716cd8666f2a82354ca778b82a1e24c126213dbd
thofma <thofma@gmail.com>
2021-02-01 10:03:44 +0100

    Add test for simplified_absolute_field

---
+++ test/NumField/NfRel/Simplify.jl
@@ -0,0 +1,8 @@
+@testset "Simplify" begin
+  Qx, x = QQ["x"]
+  K, a = number_field(x^2 - x + 1289)
+  Kt, t = K["t"]
+  L, = number_field(t^2 - 5, cached = false)
+  k, = @inferred Hecke.simplified_absolute_field(L, cached = false)
+  @test isisomorphic(k, number_field(x^4 + 513*x^2 + 67081)[1])[1]
+end
