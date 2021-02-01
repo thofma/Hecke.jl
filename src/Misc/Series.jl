b45cdbc0293e8183ae4d90bbb88fd996eeee7d9e
Claus Fieker <fieker@mathematik.uni-kl.de>
2021-02-01 16:05:07 +0100

    add IJulia back in and trivia Oscar -> Hecke

--- src/Misc/Series.jl
+++ src/Misc/Series.jl
@@ -104,4 +104,21 @@ function Nemo.integral(f::RelSeriesElem{T}) where T
   return g
 end
 
+#TODO: check where this should be done properly
+# not needed for Galois stuff.
+function (f::RelSeriesElem)(a::RingElem)
+  y = a
+  v = valuation(f)
+  p = precision(f)
+  z = zero(y)
+  for i = Int(p):-1:Int(v)
+      z *= y
+      c = coeff(f, i)
+      if !iszero(c)
+          z += c
+      end
+  end
+  z *= y^Int(v)
+  return z
+end
 
