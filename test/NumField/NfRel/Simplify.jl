fe6728870cb5918901e1b77a15a1e85e49168970
thofma <thofma@gmail.com>
2021-02-01 17:17:30 +0100

    Another test

--- test/NumField/NfRel/Simplify.jl
+++ test/NumField/NfRel/Simplify.jl
@@ -5,4 +5,9 @@
   L, = number_field(t^2 - 5, cached = false)
   k, = @inferred Hecke.simplified_absolute_field(L, cached = false)
   @test isisomorphic(k, number_field(x^4 + 513*x^2 + 67081)[1])[1]
+
+  K, a = number_field(x^3 + 42*x - 192, "a", cached = false);
+  Kt, t = K["t"]
+  L, = number_field(t^2 + 3, cached = false)
+  k, = @inferred  Hecke.simplified_absolute_field(L)
 end
