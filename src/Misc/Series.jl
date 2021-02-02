36f4a11871a0bacdf5d5847aa93e1c45b57ee0a5
Claus Fieker <fieker@mathematik.uni-kl.de>
2021-02-02 09:13:26 +0100

    finally remove completely, needed for Galois

--- src/Misc/Series.jl
+++ src/Misc/Series.jl
@@ -104,24 +104,3 @@ function Nemo.integral(f::RelSeriesElem{T}) where T
   return g
 end
 
-#=
-needs Julia > 1.1
-#TODO: check where this should be done properly
-# not needed for Galois stuff.
-function (f::RelSeriesElem)(a::RingElem)
-  y = a
-  v = valuation(f)
-  p = precision(f)
-  z = zero(y)
-  for i = Int(p):-1:Int(v)
-      z *= y
-      c = coeff(f, i)
-      if !iszero(c)
-          z += c
-      end
-  end
-  z *= y^Int(v)
-  return z
-end
-
-=#
