2f2c9ae97705b61d5f52a614f5bcdd343639c0b5
simonbrandhorst <51749255+simonbrandhorst@users.noreply.github.com>
2021-03-03 17:08:02 +0100

    fix normal_form(TorQuadMod) (#192)

--- test/QuadForm/Torsion.jl
+++ test/QuadForm/Torsion.jl
@@ -1,6 +1,6 @@
 @testset "Torsion" begin
   # Trivial torsion module
-  
+
   A = diagonal_matrix(fmpq[1, 1])
   T = Hecke.TorQuadMod(A)
   @test order(T) == 1
@@ -41,24 +41,40 @@
   L = Zlattice(matrix(ZZ, [[2,0,0],[0,2,0],[0,0,2]]))
   T = Hecke.discriminant_group(L)
   @test basis_matrix(Hecke.cover(Hecke.primary_part(T,fmpz(2))[1])) == matrix(QQ, 3, 3, [1//2, 0, 0, 0, 1//2, 0, 0, 0, 1//2])
-  
+
   #orthogonal submodule to a TorQuadMod
   L = Zlattice(matrix(ZZ, [[2,0,0],[0,2,0],[0,0,2]]))
   T = Hecke.discriminant_group(L)
   S = sub(T, gens(T))[1]
   @test basis_matrix(Hecke.cover(Hecke.orthogonal_submodule_to(T, S)[1])) == matrix(QQ, 3, 3, [1//2,0,0,0,1//2,0,0,0,1//2])
-  
+
   #checks if a TorQuadMod is degenerate
-  @test Hecke.isdegenerate(T) == false  
-  
+  @test Hecke.isdegenerate(T) == false
+
   #test for rescaled torsion quadratic module
   @test Hecke.gram_matrix_quadratic(Hecke.rescale(T, -1)) == matrix(QQ, 3, 3, [7//4,0,0,0,7//4,0,0,0,7//4])
   t = Hecke.TorQuadMod(QQ[1//3 0; 0 1//9])
   @test Hecke.gram_matrix_quadratic(Hecke.rescale(t, 2)) == matrix(QQ, 2, 2, [2//3,0,0,2//9])
 
   #test for normal form
-  L1 = Zlattice(matrix(ZZ, [[-2,0,0],[0,1,0],[0,0,4]]))
+  L1 = Zlattice(gram=matrix(ZZ, [[-2,0,0],[0,1,0],[0,0,4]]))
   T1 = Hecke.discriminant_group(L1)
-  @test Hecke.gram_matrix_quadratic(Hecke.normal_form(T1)[1]) == matrix(QQ, 2, 2, [1//4,0,0,1//16])
+  @test Hecke.gram_matrix_quadratic(Hecke.normal_form(T1)[1]) == matrix(QQ, 2, 2, [1//2,0,0,1//4])
+
+
+  L1 = Zlattice(gram=ZZ[-4 0 0; 0 4 0; 0 0 -2])
+  AL1 = discriminant_group(L1)
+  L2 = Zlattice(gram=ZZ[-4 0 0; 0 -4 0; 0 0 2])
+  AL2 = discriminant_group(L2)
+  n1 = normal_form(AL1)[1]
+  g1 = QQ[1//2   0   0;
+            0 1//4   0;
+            0   0 5//4]
+  @test Hecke.gram_matrix_quadratic(n1) == g1
+  n2 = normal_form(AL2)[1]
+  g2 = QQ[1//2   0   0;
+            0 1//4   0;
+            0   0 5//4]
+  @test Hecke.gram_matrix_quadratic(n2) == g2
 end
 
