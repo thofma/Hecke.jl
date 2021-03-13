e495b526d70e4b93c2355b3d6fa05de0df3ee326
thofma <thofma@gmail.com>
2021-03-13 18:05:27 +0100

    Implement short/long test functionality (#208)

--- system/precompile.jl
+++ system/precompile.jl
@@ -1,13 +1,18 @@
-quadratic_field(10)
+k, a = quadratic_field(10)
+@test degree(k) == 2
 k, a = cyclotomic_field(11)
-automorphism_group(k)
+G, mG = automorphism_group(k)
+@test order(G) == 10
 kt, t = k["t"]
 factor(t^2-a)
 factor(t^5-a)
 k, a = wildanger_field(3, 13)
 h = hilbert_class_field(k)
+@test degree(h) == 9
 K = number_field(h)
+@test degree(h) == 9
 L = simple_extension(K)[1]
+@test degree(L) == 9
 absolute_simple_field(L)
 discriminant(maximal_order(K))
 norm_equation(k, 27)
