0ea7ebe4c19e1cbd900e971c0b7fa1a28e0bdeb1
William Hart <goodwillhart@googlemail.com>
2021-03-17 17:31:45 +0100

    Replace trail -> trailing_coefficient.

--- src/NumField/NfAbs/NfAbs.jl
+++ src/NumField/NfAbs/NfAbs.jl
@@ -135,7 +135,7 @@ function quadratic_field(d::fmpz; cached::Bool = true, check::Bool = true)
 end
 
 function show_quad(io::IO, q::AnticNumberField)
-  d = trail(q.pol)
+  d = trailing_coefficient(q.pol)
   if d < 0
     print(io, "Real quadratic field by ", q.pol)
   else
