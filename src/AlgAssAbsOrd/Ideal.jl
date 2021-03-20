56de8507b1063ecbe46310fd1c2f1c60b3192116
Tommy Hofmann <thofma@gmail.com>
2021-03-20 12:11:26 +0100

    Make my code run again

--- src/AlgAssAbsOrd/Ideal.jl
+++ src/AlgAssAbsOrd/Ideal.jl
@@ -164,8 +164,8 @@ ideal(O::AlgAssAbsOrd{S, T}, x::AlgAssAbsOrdElem{S, T}, side::Symbol) where { S,
 
 Returns the ideal $O \cdot x$ or $x \cdot O$ respectively.
 """
-*(O::AlgAssAbsOrd{S, T}, x::T) where {S, T <: AlgAssElem} = ideal(O, x, :left)
-*(x::T, O::AlgAssAbsOrd{S, T}) where {S, T <: AlgAssElem} = ideal(O, x, :right)
+*(O::AlgAssAbsOrd{S, T}, x::T) where {S, T <: Union{AlgAssElem, AlgMatElem}} = ideal(O, x, :left)
+*(x::T, O::AlgAssAbsOrd{S, T}) where {S, T <: Union{AlgAssElem, AlgMatElem}} = ideal(O, x, :right)
 *(O::AlgAssAbsOrd{S, T}, x::AlgAssAbsOrdElem{S, T}) where {S, T} = ideal(O, x, :left)
 *(x::AlgAssAbsOrdElem{S, T}, O::AlgAssAbsOrd{S, T}) where {S, T} = ideal(O, x, :right)
 *(O::AlgAssAbsOrd, x::Union{ Int, fmpz }) = ideal(O, O(x), :left)
@@ -555,7 +555,7 @@ end
 
 *(x::Union{ Int, fmpz, fmpq }, a::AlgAssAbsOrdIdl) = a*x
 
-function *(a::AlgAssAbsOrdIdl{S, T}, x::T) where { S, T <: Union{AlgAssElem, AlgGrpElem} }
+function *(a::AlgAssAbsOrdIdl{S, T}, x::T) where { S, T <: Union{AlgAssElem, AlgGrpElem, AlgMatElem} }
   if iszero(x)
     return _zero_ideal(algebra(a))
   end
@@ -567,7 +567,7 @@ function *(a::AlgAssAbsOrdIdl{S, T}, x::T) where { S, T <: Union{AlgAssElem, Alg
   return b
 end
 
-function *(x::T, a::AlgAssAbsOrdIdl{S, T}) where { S, T <: Union{AlgAssElem, AlgGrpElem}}
+function *(x::T, a::AlgAssAbsOrdIdl{S, T}) where { S, T <: Union{AlgAssElem, AlgGrpElem, AlgMatElem}}
   if iszero(x)
     return _zero_ideal(algebra(a))
   end
