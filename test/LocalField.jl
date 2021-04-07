3b9cdedae1d949b849b1c4e2dfea3f3bc71a9959
CarloSircana <sircana@mathematik.uni-kl.de>
2021-04-07 10:01:37 +0200

    tests

--- test/LocalField.jl
+++ test/LocalField.jl
@@ -1,6 +1,6 @@
 @testset "LocalField ..." begin
   println("Conjugates.jl")
   @time include("LocalField/Conjugates.jl")
-  #println("Poly.jl")
-  #@time include("LocalField/Poly.jl")
+  println("Poly.jl")
+  @time include("LocalField/Poly.jl")
 end
