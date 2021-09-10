3068de68a317800b6f75c9cd8a4d404d5be8c901
Claus Fieker <fieker@mathematik.uni-kl.de>
2021-09-10 18:04:29 +0200

    before I delete it (again)

--- examples/Round2.jl
+++ examples/Round2.jl
@@ -37,7 +37,7 @@ mutable struct Order <: AbstractAlgebra.Ring
   trans::MatElem #both matrices are over base_ring(F)
   itrans::MatElem
 
-  function Order(R::AbstractAlgebra.Ring, F::AbstractAlgebra.Field, empty::Bool = false)
+  function Order(R::AbstractAlgebra.Ring, F::AbstractAlgebra.Field, empty::Bool = false; check::Bool = true)
     #empty allows to create an Order that is none:
     # Z[x]/3x+1 is no order. This will be "fixed" by using any_order, but
     #the intial shel needs to be empty (illegal)
@@ -57,13 +57,13 @@ mutable struct Order <: AbstractAlgebra.Ring
           M[i, j] = coeff(f, d - (i - j))
         end
       end
-      O = Order(r, M, one(Qt))
+      O = Order(r, M, one(Qt), check = check)
       return O
     end
     return r
   end
 
-  function Order(O::Order, T::MatElem, d::RingElem)
+  function Order(O::Order, T::MatElem, d::RingElem; check::Bool = true)
     F = base_ring(O.F)
     T = map_entries(F, T)
     T = divexact(T, base_ring(T)(d))
@@ -76,7 +76,7 @@ mutable struct Order <: AbstractAlgebra.Ring
       r.trans = T
       r.itrans = Ti
     end
-    map(representation_matrix, basis(r))
+    check && map(representation_matrix, basis(r))
     return r
   end
 end
@@ -196,12 +196,12 @@ Base.:^(a::OrderElem, n::Integer) = parent(a)(a.data^n)
 function Hecke.representation_matrix(a::OrderElem)
   O = parent(a)
   b = basis(O)
-  m = zero_matrix(O.R, degree(O), degree(O))
+  m = zero_matrix(base_ring(O.F), degree(O), degree(O))
   for i=1:degree(O)
     c = coordinates(b[i]*a)
     for j=1:degree(O)
-      m[i,j] = numerator(c[j], base_ring(O))
-      @assert isone(denominator(c[j], base_ring(O)))
+      m[i,j] = c[j] #numerator(c[j], base_ring(O))
+      #@assert isone(denominator(c[j], base_ring(O)))
     end
   end
   return m
@@ -360,7 +360,7 @@ function radical_basis_power_non_perfect(O::Order, p::RingElem)
       mm[i,j] = d
     end
   end
-  @show dd
+  
   for i=1:degree(O)
     for j=1:degree(O)
       d = numerator(mm[i,j])
@@ -403,14 +403,10 @@ function ring_of_multipliers(O::Order, I::MatElem)
   m = hcat([divexact(representation_matrix(O(vec(collect(I[i, :]))))*II, d) for i=1:nrows(I)]...)
   m = m'
   n = degree(O)
-  @show n, d
   mm = hnf(m[1:n, 1:n])
   for i=2:n
-    @show i
     mm = vcat(mm, hnf(m[(i-1)*n+1:i*n, 1:n]))
-    global last_hnf = (mm, d)
-    @time mm = hnf(mm)[1:n, 1:n]
-    @show nrows(mm), ncols(mm)
+    mm = hnf(mm)[1:n, 1:n]
   end
   H = mm
 
@@ -1033,6 +1029,10 @@ function Hecke.factor(a::Generic.Rat, R::HessQR)
   return f1
 end
 
+function Hecke.isconstant(a::HessQRElem)
+  return iszero(a) || (isconstant(a.f) && isconstant(a.g))
+end
+
 end
 
 using .HessQRModule
@@ -1048,12 +1048,72 @@ function GenericRound2.integral_closure(S::HessQR, F::Generic.FunctionField{T}) 
   return GenericRound2._integral_closure(S, F)
 end
 
+function _gcdx(a::fmpq, b::fmpq)
+  l = lcm(denominator(a), denominator(b))
+  g, e, f = gcdx(numerator(a*l), numerator(b*l))
+  return g//l, e, f
+end
+
+function two_by_two(Q::MatElem{<:Generic.Rat{_T}}, R::PolyRing{_T}, S::HessQR) where {_T}
+  @assert size(Q) == (2,2)
+
+  Qt = base_ring(Q)
+  T1 = identity_matrix(Qt, 2)
+  T2 = identity_matrix(Qt, 2)
+  @show :START, Q
+  while !iszero(Q[2,1])
+    @assert all(x->isone(denominator(x, R)), Q)
+    @assert iszero(Q[1,2])
+    @assert degree(numerator(Q[2,1])) < degree(numerator(Q[2,2]))
+
+    n, d = integral_split(Q[1,1], S)
+    @assert isconstant(d)
+    a = n.c//d.c
+    Q[1,1] = a
+    c = Qt(n)//Qt(d)*inv(Q[1,1])
+    @assert isunit(c)
+    T1[1,:] *= inv(c)
+    n, d = integral_split(Q[2,1], S)
+    b = n.c//d.c
+    beta = Q[2,1]//b
+
+    g, e, f = _gcdx(a, b)
+    T = matrix(Qt, 2, 2, [e*beta, Qt(f), -Q[2,1]//g, Q[1,1]//g])
+    Q = T*Q
+    @assert iszero(Q[2,1])
+    T1 = T*T1
+
+    if iszero(Q[1,2])
+      return Q, T1, T2
+    end
+
+    n, d = integral_split(Q[1,1], R)
+    @assert isone(d)
+    nn, d = integral_split(Q[1,2], R)
+    @assert isone(d)
+    @assert degree(nn) > degree(n)
+    q, r = divrem(nn, n)
+    
+    T = matrix(Qt, 2, 2, [Qt(1), -q, Qt(0), Qt(1)])
+    Q = Q*T
+    T2 = T2 * T
+
+    swap_cols!(Q, 1, 2)
+    swap_cols!(T2, 1, 2)
+    swap_rows!(Q, 1, 2)
+    swap_rows!(T1, 1, 2)
+  end
+
+  return Q, T1, T2
+end
+
 function GenericRound2.integral_closure(Zx::FmpzPolyRing, F::Generic.FunctionField)
   Qt = base_ring(F)
   t = gen(Qt)
   S = HessQR(Zx, Qt)
+  R = parent(numerator(t))
   o1 = integral_closure(S, F)
-  o2 = integral_closure(parent(denominator(t)), F)
+  o2 = integral_closure(R, F)
   if isdefined(o1, :trans)
     T = o1.trans
   else
@@ -1062,35 +1122,52 @@ function GenericRound2.integral_closure(Zx::FmpzPolyRing, F::Generic.FunctionFie
   if isdefined(o2, :itrans)
     T = T * o2.itrans
   end
-  q, w = integral_split(T, S)
-  TT1 = identity_matrix(S, degree(F))
-  TT2 = identity_matrix(base_ring(o2), degree(F))
+  q, w = integral_split(T, R)
+  h, T2 = Hecke._hnf_with_transform(q', :upperright)
+  T = map_entries(Qt, h')
+
+  TT2 = map_entries(Qt, T2')
+  TT1 = identity_matrix(Qt, degree(F))
   cnt = 0
-  local H
-  #TODO: Algo is wrong!!!! needs special ref, not iterated HNF.
-  #      the denominators explode
-  while !isdiagonal(q)
-    h, T1 = Hecke._hnf_with_transform(q, :lowerleft)
-  
-    TT1 = T1*TT1
-    T = map_entries(x->Qt(x)//Qt(w), h)
-    qq, ww = integral_split(T', base_ring(o2))
-    H, T2 = Hecke._hnf_with_transform(qq, :lowerleft)
- 
-    TT2 = T2*TT2
-    T = map_entries(x->Qt(x)//Qt(ww), H)
-    q, w = integral_split(T', S)
-    cnt += 1
-    cnt > 5 && error("asdas")
+  @assert TT1*o1.trans*o2.itrans*TT2 == divexact(T, Qt(w))
+  for i=1:degree(F)
+    for j=i+1:degree(F)
+      q, t1, t2 = two_by_two(T[ [i,j], [i,j]], R, S)
+      T[[i,j], [i,j]] = q
+      TT = identity_matrix(Qt, degree(F))
+      TT[[i,j], [i,j]] = t1
+      TT1 = TT*TT1
+      TT[[i,j], [i,j]] = t2
+      TT2 = TT2 * TT
+  @assert TT1*o1.trans*o2.itrans*TT2 == divexact(T, Qt(w))
+    end
   end
-#  @assert isone(q)
+
+
+  @assert isdiagonal(T)
+  T = divexact(T, Qt(w))
+  @assert TT1*o1.trans*o2.itrans*TT2 == T
+  for i=1:degree(F)
+    n, d = integral_split(T[i,i], S)
+    @assert isconstant(d)
+    u = Qt(n.f)//Qt(n.g)
+    @assert n.c//d.c*u == T[i,i]
+    TT2[:, i] *= inv(Qt(n.c))
+    TT1[i, :] *= Qt(d.c)*inv(u)
+    T[i,i] = 1
+  @assert TT1*o1.trans*o2.itrans*TT2 == T
+  end
+
+  TT1 = TT1
+  n, d = integral_split(TT1, Zx)
+  @assert map_entries(Qt, n) == TT1 * Qt(d)
   o3 = GenericRound2.Order(Zx, F)
   if isdefined(o1, :trans)
-    return GenericRound2.Order(o3, integral_split(map_entries(Qt, TT1)*o1.trans, Zx)...)
+    return GenericRound2.Order(o3, integral_split(map_entries(Qt, TT1)*o1.trans, Zx)..., check = false)
   else
-    return GenericRound2.Order(o3, integral_split(map_entries(Qt, TT1), Zx)...)
+    return GenericRound2.Order(o3, integral_split(map_entries(Qt, TT1), Zx)..., check = false)
   end
-  return H, GenericRound2.Order(o1, TT1, one(S)), GenericRound2.Order(o2, inv(TT2'), one(base_ring(TT2)))
+  return GenericRound2.Order(o1, TT1, one(S)), GenericRound2.Order(o2, inv(TT2'), one(base_ring(TT2)))
 end
 
 function Base.denominator(a::Generic.Rat{fmpq}, S::FmpzPolyRing)
