d3bef30cbc7cacad0654d3e3a0876fc8cb70de90
Aslam Ali <59648192+aslam7861@users.noreply.github.com>
2021-09-20 14:07:41 +0200

    added_an_example
    added_an_example

--- test/LocalField/LocalField.jl
+++ test/LocalField/LocalField.jl
@@ -224,3 +224,15 @@
   end
 
 end
+
+
+Qx,x = FlintQQ["x"]
+f = Qx([ 1, 8, -40, -46, 110, 71, -113, -43, 54, 11, -12, -1, 1 ])
+L = number_field(f)[1];
+P = prime_decomposition(maximal_order(L),7)[1][1];
+lp, mp = Hecke.generic_completion(L,P);
+Qy,y = PolynomialRing(lp,"y")
+f, mf = ResidueField(lp)
+N = Hecke.unramified_extension(y^3+preimage(mf,(gen(f))) +4 )[1]
+ResidueField(N) # "this was not working before"
+automorphism_group(N) #"This was also not working"
