b57d7bb35be68a13f24f9e9bc38930818458268e
Aslam Ali <59648192+aslam7861@users.noreply.github.com>
2021-10-05 10:04:08 +0200

    Update neq.jl
    added a function which computes inverse of arbitrary element of local field

--- src/LocalField/neq.jl
+++ src/LocalField/neq.jl
@@ -1,12 +1,12 @@
 #export: degree_relative, random_elem, one_root, norm_equation  
 
 
-degree_relative(L::Hecke.LocalField,K::Union{FlintQadicField, Hecke.LocalField}) = Int(absolute_degree(L)//absolute_degree(K)
+degree_relative(L::Hecke.LocalField, K::Union{FlintQadicField, Hecke.LocalField}) = Int(absolute_degree(L)//absolute_degree(K)
 )
 
- degree_relative(L::Hecke.RelFinField, K:: Union{FqNmodFiniteField, Hecke.RelFinField})  = Int(absolute_degree(L)//absolute_degree(K))
+ degree_relative(L::Hecke.RelFinField, K:: Union{FqNmodFiniteField, Hecke.RelFinField}) = Int(absolute_degree(L)//absolute_degree(K))
 
-degree_relative(L::FqNmodFiniteField, K::Hecke.Nemo.GaloisField)  = Int(absolute_degree(L)//absolute_degree(K))
+degree_relative(L::FqNmodFiniteField, K::Hecke.Nemo.GaloisField) = Int(absolute_degree(L)//absolute_degree(K))
 
 
 ##############################################
@@ -17,7 +17,20 @@ function random_elem(L::Union{FlintQadicField, Hecke.LocalField})
    b = basis(L)
    n = degree(L)
    r = [rand(1:5*n) for i in 1:n]   # Choose small coefficients
-return sum( [ r[i]*b[i] for i in 1:n])
+   return sum( [ r[i]*b[i] for i in 1:n])
+end
+########################################
+# inverse of an element in local field i.e inv(b)*b=1
+########################################
+function inv(b::Hecke.LocalFieldElem)
+   L = parent(b)
+   m = matrix([[L(coeff(b*x, i-1)) for i in 1:degree(L)] for x in basis(L)])
+   h= charpoly(m)
+   c = coeff(h,1)*b^(0)
+   for i = 2:degree(h)
+      c += coeff(h,i)*b^(i-1)
+   end
+   return c//(-coeff(h,0))
 end
 
 ################ one_root computes a single root in the finite field extensions#############################
@@ -27,9 +40,9 @@ function one_root(f::Union{gfp_poly, fq_nmod_poly}, F::Union{FqNmodFiniteField, 
    if length(fac)  == 1 
       error("no roots") 
    end
-  r = first(fac)[1] 
-  @assert degree(r) == 1
-return -coeff(r,0)
+   r = first(fac)[1] 
+   @assert degree(r) == 1
+   return -coeff(r,0)
 end
  
 
@@ -41,7 +54,7 @@ function one_root(f::Hecke.AbstractAlgebra.Generic.Poly, F:: Hecke.RelFinField)
    end
    r = first(fac)[1] 
    @assert degree(r) == 1
-return -coeff(r,0)
+   return -coeff(r,0)
 end
 
 #########################################################################
@@ -52,7 +65,7 @@ end
 function norm_det(a)
    L = parent(a)
    m = matrix([[coeff(a*x, i-1) for i in 1:degree(L)] for x in basis(L)])
-return det(m)
+   return det(m)
 end
 
 
@@ -65,7 +78,7 @@ function norm_equation(F::Union{FqNmodFiniteField, Hecke.RelFinField}, b::Union{
    while !isirreducible(f)
       f = polynomial(k,vcat([b],[rand(k) for i = 1:n-1],[1] ) )
    end
-return (-1)^(n)*one_root(f,F)
+   return (-1)^(n)*one_root(f,F)
 end
 
 ################ norm equation over local field extensions###########################
@@ -94,10 +107,10 @@ function norm_equation(R:: Hecke.LocalField, b::Union{qadic,Hecke.LocalFieldElem
    end
    r = random_elem(R);
    while valuation(trace(r)) != 0 ||  valuation(r // R(trace(r))) != 0
-       r = random_elem(R);
+      r = random_elem(R);
    end
-   s = r *R(trace(r)^-1) * R(log(b))
-return exp(s)*f_nm
+   s = r * R(trace(r)^-1) * R(log(b))   
+   return exp(s)*f_nm
 end
  
 ############### ############################################################################################
@@ -126,24 +139,24 @@ function norm_equation_unramified(L::Hecke.LocalField, b::Hecke.LocalFieldElem)
    n = ee*valuation((b //  norm_det(C[1]))-1)
    r = random_elem(L);
    while valuation(trace(r)) != 0 ||  valuation(r // L(trace(r))) != 0
-       r = random_elem(L);
+      r = random_elem(L);
    end
    z = ((b // norm_det(c))-1) // piK^ZZ(n);
    push!(C, 1+ piL^ZZ(n)* (L(z)*r // L(trace(r))));
    c = prod(C)
    n = ee*valuation( (b// norm_det(c))-1);
    while prec_b >= n+2 #   == n 
-       z = ((b // norm_det(c))-1) * piK^-ZZ(n);
-       push!(C, 1+ piL^ZZ(n)* (L(z)*r // L(trace(r))));
-       c = prod(C);
-       chk = (norm_det(c) * b^-1)-1
-       if iszero(chk) == true
-          n = prec_b           
-       else
-          n = ee*valuation((b// norm_det(c))-1);
-       end
-    end
-return c*f_nm;
+      z = ((b // norm_det(c))-1) * piK^-ZZ(n);
+      push!(C, 1+ piL^ZZ(n)* (L(z)*r // L(trace(r))));
+      c = prod(C);
+      chk = (norm_det(c) * b^-1)-1
+      if iszero(chk) == true
+         n = prec_b           
+      else
+         n = ee*valuation((b// norm_det(c))-1);
+      end
+   end
+   return c*f_nm;
 end
 
     
