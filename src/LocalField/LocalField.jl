8b4ddf51333260cc46f591620e8b96faae831c1e
Aslam Ali <59648192+aslam7861@users.noreply.github.com>
2021-10-14 14:34:57 +0200

    updated unramified extension with a given degree
    unramified extension over local field L of a given degree n

--- src/LocalField/LocalField.jl
+++ src/LocalField/LocalField.jl
@@ -462,3 +462,17 @@ function ResidueField(K::LocalField{ S, UnramifiedLocalField}) where {S <: Field
    K.residue_field_map = mp
   return kk, mp
 end
+                        
+ ################### unramified extension over local field L of a given degree n ####################
+
+function unramified_extension(L::Union{FlintPadicField, FlintQadicField, LocalField}, n::Int)
+    R, mR = ResidueField(L)
+    f = polynomial(R, [rand(R) for i = 0:n])
+    while !isirreducible(f)
+        f = polynomial(R, [rand(R) for i = 0:n])
+    end
+    f_L = polynomial(L, [mR\(coeff(f, i)) for i = 0:degree(f)])
+    return Hecke.unramified_extension(f_L)
+end
+
+                              
