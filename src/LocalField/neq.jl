6aca3bb40b6db4d0d5d54c6ec1d49b0c6c45ced2
aslam7861 <aliaslam9439@gmail.com>
2021-10-15 11:23:52 +0200

    refined version of norm eqaution and fixed spacing

--- src/LocalField/neq.jl
+++ src/LocalField/neq.jl
@@ -1,11 +1,11 @@
-#export: degree_relative, random_elem, one_root, norm_equation  
+#export: degree_relative, random_elem, one_root, norm_equation
 
 
-degree_relative(L::Hecke.LocalField, K::Union{FlintQadicField, Hecke.LocalField}) = Int(absolute_degree(L)//absolute_degree(K))
+degree_relative(L::Hecke.LocalField, K::Union{FlintQadicField, Hecke.LocalField}) = divexact(absolute_degree(L), absolute_degree(K))
 
-degree_relative(L::Hecke.RelFinField, K::Union{FqNmodFiniteField, Hecke.RelFinField}) = Int(absolute_degree(L)//absolute_degree(K))
+degree_relative(L::Hecke.RelFinField, K::Union{FqNmodFiniteField, Hecke.RelFinField}) = divexact(absolute_degree(L), absolute_degree(K))
 
-degree_relative(L::FqNmodFiniteField, K::Hecke.Nemo.GaloisField) = Int(absolute_degree(L)//absolute_degree(K))
+degree_relative(L::FqNmodFiniteField, K::Hecke.Nemo.GaloisField) = divexact(absolute_degree(L), absolute_degree(K))
 
 
 ##############################################
@@ -16,7 +16,7 @@ function random_elem(L::Union{FlintQadicField, Hecke.LocalField})
    b = basis(L)
    n = degree(L)
    r = [rand(1:5*n) for i in 1:n]   # Choose small coefficients
-   return sum( [ r[i]*b[i] for i in 1:n])
+   return sum( [r[i]*b[i] for i in 1:n])
 end
 
 
@@ -24,22 +24,22 @@ end
 function one_root(f::Union{gfp_poly, fq_nmod_poly}, F::Union{FqNmodFiniteField, Hecke.RelFinField})
    g = polynomial(F, [coeff(f,i) for i = 0:degree(f) ] )
    fac = factor(g)
-   if length(fac)  == 1 
-      error("no roots") 
+   if length(fac) == 1
+      error("no roots")
    end
-   r = first(fac)[1] 
+   r = first(fac)[1]
    @assert degree(r) == 1
    return -coeff(r,0)
 end
- 
 
-function one_root(f::Hecke.AbstractAlgebra.Generic.Poly, F:: Hecke.RelFinField)
-   g = polynomial(F, [coeff(f,i) for i = 0:degree(f) ] )
+
+function one_root(f::Hecke.AbstractAlgebra.Generic.Poly, F::Hecke.RelFinField)
+   g = polynomial(F, [coeff(f,i) for i = 0:degree(f)])
    fac = factor(g)
-   if length(fac)  == 1     
-      error("no roots") 
+   if length(fac) == 1
+      error("no roots")
    end
-   r = first(fac)[1] 
+   r = first(fac)[1]
    @assert degree(r) == 1
    return -coeff(r,0)
 end
@@ -50,9 +50,9 @@ end
 function norm_equation(F::Union{FqNmodFiniteField, Hecke.RelFinField}, b::Union{gfp_elem, fq_nmod})
    k = parent(b)
    n = degree_relative(F,k)
-   f = polynomial(k,vcat([b],[rand(k) for i = 1:n-1],[1] ) )
+   f = polynomial(k,vcat([b],[rand(k) for i = 1:n-1],[1]))
    while !isirreducible(f)
-      f = polynomial(k,vcat([b],[rand(k) for i = 1:n-1],[1] ) )
+      f = polynomial(k,vcat([b],[rand(k) for i = 1:n-1],[1]))
    end
    return (-1)^(n)*one_root(f,F)
 end
@@ -66,7 +66,7 @@ function norm_equation(R:: Hecke.LocalField, b::Union{qadic,Hecke.LocalFieldElem
    f,mf = ResidueField(K)
    F,mF = ResidueField(R)
    ee = absolute_ramification_index(K)
-   if degree(R) == ramification_index(R) && mf(b) !=f(1)     
+   if degree(R) == ramification_index(R) && mf(b) !=f(1)
       error("To be implemented")
    end
    if mf(b) == f(1)
@@ -75,20 +75,20 @@ function norm_equation(R:: Hecke.LocalField, b::Union{qadic,Hecke.LocalFieldElem
       f_nm = norm_equation(F,mf(b))
       f_nm = mF\(f_nm)
    end
-   b = b //norm(f_nm)
+   b = b//norm(f_nm)
    b = setprecision(b,prec_b)
-   p = prime(R)  
-   if valuation(b-1) < ee//(p-1)+1//ee     
+   p = prime(R)
+   if valuation(b-1) < ee//(p-1)+1//ee
       error("To be implemented or try norm_equation_unramified")
    end
    r = random_elem(R)
-   while valuation(trace(r)) != 0 ||  valuation(r // R(trace(r))) != 0
+   while valuation(trace(r)) != 0 || valuation(r//R(trace(r))) != 0
       r = random_elem(R)
    end
-   s = r * R(trace(r)^-1) * R(log(b))   
+   s = r*R(trace(r)^-1)*R(log(b))
    return exp(s)*f_nm
 end
- 
+
 ###########################################################################################################
 #   The following "norm_equation_unramified" solves the norm equations only in unramified extensions
 ###########################################################################################################
@@ -108,36 +108,34 @@ function norm_equation_unramified(L::Hecke.LocalField, b::Hecke.LocalFieldElem)
       f_nm = norm_equation(F,mf(b))
       f_nm = mF\(f_nm)
    end
-   b = b //norm(f_nm)
+   b = b//norm(f_nm)
    b = setprecision(b,prec_b)
    c = L(1)
    C = [L(1)]
-   n = ee*valuation((b //  norm(C[1]))-1)
+   n = ee*valuation((b//norm(C[1]))-1)
    r = random_elem(L)
-   while valuation(trace(r)) != 0 ||  valuation(r // L(trace(r))) != 0
+   while valuation(trace(r)) != 0 || valuation(r//L(trace(r))) != 0
       r = random_elem(L)
    end
-   z = ((b // norm(c))-1) // piK^ZZ(n)
-   push!(C, 1+ piL^ZZ(n)* (L(z)*r // L(trace(r))))
+   z = ((b//norm(c))-1)//piK^ZZ(n)
+   setprecision!(z, prec_b)
+   push!(C, 1+ piL^ZZ(n)* (L(z)*r//L(trace(r))))
    c = prod(C)
-   n = ee*valuation( (b// norm(c))-1)
-   while prec_b >= n+2 #   == n 
-      z = ((b // norm(c))-1) * piK^-ZZ(n)
-      push!(C, 1+ piL^ZZ(n)* (L(z)*r // L(trace(r))))
+   nc = norm(c)
+   n = ee*valuation((b//nc)-1)
+   while prec_b >= n+1 #  "solve till precision n-1"
+      z = ((b//nc)-1)*piK^-ZZ(n)
+      setprecision!(z, prec_b)
+      push!(C, 1+ piL^ZZ(n)*(L(z)*r//L(trace(r))))
       c = prod(C)
-      chk = (norm(c) * b^-1)-1
+      setprecision!(c, precision(L))
+      nc = norm(c)
+      chk = (nc*b^-1)-1
       if iszero(chk) == true
-         n = prec_b           
+         n = prec_b
       else
-         n = ee*valuation((b// norm(c))-1)
+         n = ee*valuation((b//nc)-1)
       end
    end
    return c*f_nm
 end
-
-    
-
-
-
-
-
