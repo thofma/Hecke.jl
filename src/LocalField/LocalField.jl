343d168d8cd2ed7ad8874de6a53fc06856b3b5ae
aslam7861 <aliaslam9439@gmail.com>
2021-10-18 14:47:11 +0200

    corrected inertia degree

--- src/LocalField/LocalField.jl
+++ src/LocalField/LocalField.jl
@@ -243,6 +243,10 @@ function inertia_degree(K::FlintPadicField)
   return 1
 end
 
+function inertia_degree(K::FlintQadicField)
+  return degree(K)
+end
+
 function inertia_degree(K::LocalField{S, EisensteinLocalField}) where S
   return 1
 end
@@ -260,7 +264,7 @@ function inertia_degree(L::LocalField, K::LocalField)
 end
 
 function absolute_inertia_degree(L::LocalField)
-  return inertia_degree(L)*inertia_degree(base_field(L))
+  return inertia_degree(L)*absolute_inertia_degree(base_field(L))
 end
 
 absolute_inertia_degree(::PadicField) = 1
