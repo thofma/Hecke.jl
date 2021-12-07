7b66309666c37659503f148349de96d2455bcf3c
Claus Fieker <fieker@mathematik.uni-kl.de>
2021-12-07 17:06:35 +0100

    find the T2-shortest elem iwht higher precision (#476)

--- test/NumField/NfAbs/NfAbs.jl
+++ test/NumField/NfAbs/NfAbs.jl
@@ -47,3 +47,14 @@ end
   L = simplify(K, canonical = true)[1]
   @test L.pol == x^5 - x^4 + x^3 - 19*x^2 + 35*x + 77
 end
+
+@testset "simplify-Gunter" begin
+
+  Qx, x = PolynomialRing(FlintQQ, "x")
+  f = x^18 - x^16 - 6*x^14 - 4*x^12 - 4*x^10 + 2*x^8 + 6*x^6 - 4*x^4 + 3*x^2 - 1
+  g = x^18 - 3*x^16 + 4*x^14 - 6*x^12 - 2*x^10 + 4*x^8 + 4*x^6 + 6*x^4 + x^2 - 1
+  h = x^18 + x^16 - x^14 - 8*x^12 - 3*x^8 + 27*x^6 - 25*x^4 + 8*x^2 - 1
+  @test simplify(number_field(f)[1], canonical = true)[1].pol == g
+  @test simplify(number_field(g)[1], canonical = true)[1].pol == g
+  @test simplify(number_field(h)[1], canonical = true)[1].pol == g
+end
