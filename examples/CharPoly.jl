93fe79b60ed9940c394b1abd24113224e0aa78d1
Claus Fieker <fieker@mathematik.uni-kl.de>
2021-12-09 10:55:00 +0100

    clarifyL use fac_elem and eval

--- examples/CharPoly.jl
+++ examples/CharPoly.jl
@@ -43,7 +43,7 @@ function append_rref!(a::Vector{Vector{T}}, piv::Vector{Int}, e::Vector{T}) wher
   return rel
 end
 
-function Hecke.charpoly(A::SMat{<:FieldElem})
+function charpoly_fac_elem(A::SMat{<:FieldElem})
   n = ncols(A)
   @assert n == nrows(A)
   dims_free = Set{Int}(1:n)
@@ -91,9 +91,100 @@ function Hecke.charpoly(A::SMat{<:FieldElem})
       end
     end
   end
-  return cp
+  m = MSet(cp)
+  return FacElem(kt, m.dict)
 end
 
+Hecke.charpoly(A::SMat{nf_elem}) = evaluate(charpoly_fac_elem(A))
+#=
+#works as the charpoly_fac_elem actually returns the minpolys
+#for the sigma cyclic spaces
+Hecke.minpoly(A::SMat{nf_elem}) = reduce(lcm, keys(charpoly_fac_elem(A).fac))
+
+does not work:
+a 1
+0 a
+could return t-a => 2 as there is a cyclic subspace of dim 1
+and, once we factor this out, the second is also dim 1.
+For this to work, we cannot factor out the older subspaces
+=#
+
+Hecke.iszero_entry(M::MatElem, i::Int, j::Int) = iszero(M[i,j])
+
+#uses Berlekamp-Massey instead of lin-alg to get the minpoly for
+#the sigma cyclic spaces. Seems to not be too good on the
+#huge example, but according to lit. is fine.
+function minpoly_bm(A::SMat{nf_elem})
+  k = base_ring(A)
+  zk = maximal_order(k)
+  local mF
+  done = false
+  for p = PrimesSet(2^40, -1)
+    lp = prime_decomposition(zk, p)
+    for P = lp
+      if degree(P[1]) == 1 && P[2] == 1
+        F, mF = ResidueField(zk, P[1])
+        mF = Hecke.extend_easy(mF, k)
+        done = true
+        break
+      end
+    end
+    if done
+      break
+    end
+  end
+
+  F = codomain(mF)
+  bas_p = matrix(F, 0, ncols(A), elem_type(F)[])
+
+  used_bas = []
+  kt, t = PolynomialRing(k, cached = false)
+  
+  e = zeros(k, nrows(A))
+  f = zeros(k, nrows(A))
+  fp = []
+
+  for i=1:nrows(A)
+    if i in used_bas
+      continue
+    end
+    @show :doing, i
+    for j = 1:length(e)
+      e[j] = zero(k)
+    end
+    e[i] = one(k)
+    bm = [one(k)]
+    used = [bm]
+    start = nrows(bas_p)
+    bas_p = vcat(bas_p, matrix(F, 1, ncols(A), map(mF, e)))
+    while true
+      Hecke.mul!(f, A, e)
+      e, f = f, e
+      push!(bm, sum(e))
+      bas_p = vcat(bas_p, matrix(F, 1, ncols(A), map(mF, e)))
+      fl, g = berlekamp_massey(bm, parent = kt)
+      if fl && degree(g)*2 +2 < length(bm)
+        @show g
+        push!(fp, g)
+        bas_p = bas_p[1:start+degree(g), :]
+        rk = rref!(bas_p)
+        @show rk, nrows(bas_p)
+        @assert rk == nrows(bas_p)
+        bas_p = bas_p[1:rk, :]
+        v = [findfirst(x->!Hecke.iszero_entry(bas_p, i, x), 1:ncols(bas_p)) for i=1:nrows(bas_p)]
+        @show used_bas, v
+        @assert issubset(used_bas, v)
+        used_bas = v
+        if length(used_bas) > 500
+          return fp, bas_p
+        end
+        break
+      end
+    end
+  end
+  return fp
+end
+        
 #function Hecke.charpoly(A::SMat{nf_elem})
 #end
 
