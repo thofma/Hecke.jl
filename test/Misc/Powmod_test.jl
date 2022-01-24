4a94069ec794c31860469f0074ee534b00ce8994
Tommy Hofmann <thofma@gmail.com>
2022-01-24 21:29:49 +0100

    Try to make the (parallel) tests faster

--- test/Misc/Powmod_test.jl
+++ test/Misc/Powmod_test.jl
@@ -1,7 +1,7 @@
 @testset "Power modulo" begin
     R = ResidueRing(ZZ,7)
     Rx,x = R["x"]
-    f = Base.rand(Rx,10)
+    f = rand(Rx, 1:10)
     g = x^4+x+1
     e = fmpz(10)^20
     @test powermod(f,e,g) == powermod(f,e%(7^degree(g)-1),g)
