4bd50fd28843fb27cbd6dc51590eaebe1727f26c
Tommy Hofmann <thofma@gmail.com>
2022-02-01 10:17:38 +0100

    Update src/NumFieldOrd/NfRelOrd/NfRelOrd.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- src/NumFieldOrd/NfRelOrd/NfRelOrd.jl
+++ src/NumFieldOrd/NfRelOrd/NfRelOrd.jl
@@ -556,7 +556,7 @@ function maximal_order_via_relative(K::AnticNumberField, m::NfToNfRel)
     OK = Order(K, [ m\b for b in B ], check = false, isbasis = true)
     OK.ismaximal = 1
     return OK
-  end::order_type(OK)
+  end::order_type(K)
 end
 
 function absolute_basis(O::NfRelOrd{T, S, U}, K::NumField{T}) where {T, S, U}
