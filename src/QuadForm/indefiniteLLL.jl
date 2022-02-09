28dc0ae5f456545189810f04e87dff0e7b576c34
Yvonne Neuy <YvonneNeuy@gmail.com>
2022-02-09 10:45:50 +0100

    Fixes in complete_to basis, quad_form_ll_gram_indef, ...

--- src/QuadForm/indefiniteLLL.jl
+++ src/QuadForm/indefiniteLLL.jl
@@ -5,7 +5,60 @@
 #
 ################################################################################
 
+################################################################################
+#                  Helpful
+################################################################################
 
+function round_matrix(M::MatElem)
+  n = nrows(M)
+  m = ncols(M)
+  L = zero(parent(M))
+    
+  for i = 1:n
+    for j = 1:m 
+        L[i,j] = round(M[i,j])
+    end
+  end
+  
+  return L
+end
+
+function abs_matrix(M::MatElem)
+    n = nrows(M)
+    m = ncols(M)
+    L = zero(parent(M))
+    
+    for i = 1:n
+      for j = 1:m 
+        L[i,j] = abs(M[i,j])
+      end
+    end
+  
+    return L
+end
+
+@doc Markdown.doc"""
+  vecextract(M::MatElem,x::Int64,y::Int64) -> MatElem 
+  
+"""
+function vecextract(M::MatElem,x::Int64,y::Int64)
+  x_bin = digits(x,base = 2)
+  y_bin = digits(y, base = 2)
+
+  list_x = [i for i = 1:length(x_bin) if x_bin[i] == 1]
+  list_y = [i for i = 1:length(y_bin) if y_bin[i] == 1]
+
+  return M[list_x , list_y]
+
+end
+
+function vecextract(M::MatElem, x,y::Int64)
+  y_bin = digits(y, base = 2)
+  list_y = [i for i = 1:length(y_bin) if y_bin[i] == 1]
+
+  return M[x , list_y]
+
+end
 ################################################################################
 #                           linear algebra
 ################################################################################
@@ -13,153 +66,127 @@
 @doc Markdown.doc"""
     mathnf(A::MatElem) -> MatElem, MatElem
     
-    Given a rectangular matrix $A$ of dimension nxm with n != m. 
-    The function computes the Hessian matrix $H$ of dimension 
-    nxm and the unimodular transformation matrix $U$ such that A U = H.
+Given a rectangular matrix $A$ of dimension nxm with n != m. 
+The function computes the Hessian matrix $H$ of dimension 
+nxm and the unimodular transformation matrix $U$ such that A U = H.
 """
 function mathnf(A::MatElem)
-    
-    H, U = hnf_with_transform(reverse_cols(A'))
-    H = reverse_rows(reverse_cols(H'))
-    U = reverse_cols(U')
+  H, U = hnf_with_transform(reverse_cols(A'))
+  H = reverse_rows(reverse_cols(H'))
+  U = reverse_cols(U')
 
-    return H, U
+  return H, U
 end
-
 
 @doc Markdown.doc"""
     complete_to_basis(v::MatElem, redflag = 0) -> MatElem
 
-    Given a rectangular matrix nxm with n != m and redflag = 0.
-    Computes a unimodular matrix with the last column equal to v. 
-    If redflag = 1, it LLL-reduce the n-m first columns if n > m.
+Given a rectangular matrix nxm with n != m and redflag = 0.
+Computes a unimodular matrix with the last column equal to v. 
+If redflag = 1, it LLL-reduce the n-m first columns if n > m.
 """
 function complete_to_basis(v::MatElem, redflag = 0)
-    
-    if(redflag != 1 && redflag != 0)
-        error("Wrong second input.")
-    end
-    n = nrows(v) #Number of rows
-    m = ncols(v) #Number of cols
+  if(redflag != 1 && redflag != 0)
+    error("Wrong second input.")
+  end
+  
+  n = nrows(v) #Number of rows
+  m = ncols(v) #Number of cols
         
-    if(n == m) #return nxn matrices
-        return v
-    end
+  if(n == m) #return nxn matrices
+    return v
+  end
     
-    U = inv(mathnf(v')[2]')
+  U = inv(mathnf(v')[2]')
        
-    if(n == 1 || n-m < 0 || redflag == 0)
-        return U
-    end
+  if(n == 1 || n-m < 0 || redflag == 0)
+    return U
+  end
 
-    re = lll(U[:,1:n-m])
+  re = vecextract(U,1<<n-1 , 1<<(n-m)-1)
+  re = lll_with_transform(re')[2]'
+  
+  l = diagonal_matrix(re,one(parent(v[1:m,1:m])))
+  re = U*l
 
-    l = hcat(re,vcat(zero(parent(U2[1:n-m,1:m])),one(U2[1:m,1:m])))
-    re = U*l
-
-    return re
+  return re
 end
 
 @doc Markdown.doc"""
     ker_mod_p(M::MatElem,p) -> Int, MatElem
 
-    Computes the kernel of the given matrix $M$ mod $p$. 
-    It returns [rank,U], where rank = dim (ker M mod p) and $U$ in GLn(Z),
-    The first $rank$ columns of $U$ span the kernel.
+Computes the kernel of the given matrix $M$ mod $p$. 
+It returns [rank,U], where rank = dim (ker M mod p) and $U$ in GLn(Z),
+The first $rank$ columns of $U$ span the kernel.
 """
 function ker_mod_p(M::MatElem,p)
-    ring = parent(M[1,1])
-    rank, ker = kernel(change_base_ring(ResidueRing(ring,p),M))
-    U = complete_to_basis(lift(ker[:,1:rank]))
-    reverse_cols!(U)
+  ring = parent(M[1,1])
+  rank, ker = kernel(change_base_ring(ResidueRing(ring,p),M))
+  U = complete_to_basis(lift(ker[:,1:rank]))
+  reverse_cols!(U)
 
-    return rank, U    
+  return rank, U    
 end
-
-
-#Invariants computations check: myhilbertsymbol == hilbert_symbol & qflocalinvariant == hasse_invariant
-# qfbreduce == Hecke.reduction_with_transformation
 
 ################################################################################
 #                           Quadratic Forms Reduction
 ################################################################################
 
-function abs_matrix(M::MatElem)
-    n = nrows(M)
-    m = ncols(M)
-
-    L = zero(parent(M))
-    for i = 1:n
-        for j = 1:m 
-            L[i,j] = abs(M[i,j])
-        end
-    end
-
-    return L
-end
-
-
-
 @doc Markdown.doc"""
     quadratic_form_lll_gram_indef(G::MatElem,base=0) -> Dict{Int64, MatElem}
-    Performs a LLL-reduction on a positive definite quadratic form QD bounding the indefinite G
-    Then finishes the reduction with 
+    
+Performs a LLL-reduction on a positive definite quadratic form QD bounding the indefinite G
+Then finishes the reduction with 
 """
 function quad_form_lll_gram_indef(G::MatElem,base=0)
-    n = ncols(G)
-    M = one(parent(G))
-    QD = G
+  n = ncols(G)
+  M = one(parent(G))
+  QD = G
+  MS = MatrixSpace(FractionField(base_ring(G)),n,n)
 
-    MS = MatrixSpace(FractionField(base_ring(G)),n,n)
-
-    for i = 1:n-1
-        
-        if(QD[i,i] == 0)
-            return quad_form_solve_triv(G, base)
-        end
-        
-        M1 = one(MS)
-        for j = 1:n
-            M1[i,j] = - QD[i,j]//QD[i,i]
-        end
-        M1[i,i] = 1
-        
-        M = M*M1
-        QD = M1'*QD*M1
-    
+  for i = 1:n-1
+    if(QD[i,i] == 0)
+      return quad_form_solve_triv(G, base)
     end
-
-    M = inv(M)
-
-    QD = M'*abs_matrix(QD)*M 
-    QD = QD*denominator(QD)
-    QD_cont = divexact(QD,content(QD))
-    QD_cont = change_base_ring(base_ring(G),QD_cont)
+        
+    M1 = one(MS)
+    for j = 1:n
+      M1[i,j] = - QD[i,j]//QD[i,i]
+    end
     
-    rank_QD = rank(QD_cont)
-    
-    S = lll_gram_with_transform(QD_cont)[2]'
-    S = S[:,ncols(S)-rank_QD+1:ncols(S)]
+    M1[i,i] = 1
+    M = M*M1
+    QD = M1'*QD*M1
+  end
   
-    if(ncols(S) < n)
-        S = complete_to_basis(S)
-    end
-
-    red = quad_form_solve_triv(S'*G*S,base)
+  M = inv(M)
+  QD = M'*abs_matrix(QD)*M 
+  QD = QD*denominator(QD)
+  QD_cont = divexact(QD,content(QD))
+  QD_cont = change_base_ring(base_ring(G),QD_cont)
+  rank_QD = rank(QD_cont)
     
-    if(length(red) == 1)
-        red[1] = S*red[1]
-        return  red
-    end
+  S = lll_gram_with_transform(QD_cont)[2]'
+  S = S[:,ncols(S)-rank_QD+1:ncols(S)]
+  
+  if(ncols(S) < n)
+    S = complete_to_basis(S)
+  end
 
-    red[2] = S*red[2]
+  red = quad_form_solve_triv(S'*G*S,base)
+  
+  if(length(red) == 1)
+    red[1] = S*red[1]
+    return  red
+  end
 
-    if(length(red) == 3)
-        red[3] = S*red[3]
-    end
+  red[2] = S*red[2]
 
-    return red
+  if(length(red) == 3)
+    red[3] = S*red[3]
+  end
 
+  return red
 end
 
 ################################################################################
@@ -169,84 +196,128 @@ end
 @doc Markdown.doc"""
     quad_form_solve_triv(G, base = 0) -> Dictionary{Int64, MatElem} 
 
-    Trying to solve G = 0 with small coefficients. Works if det(G) = 1, dim <= 6 and G is LLL-reduced.
-    Return G,Identity if no solution is found. Exit with a norm 0 vector if one such is found.
-    If base = 1 and norm 0 is obtained returns H'*G*H, H, sol 
-    where sol is a norm 0 vector and is the 1st column of H.
+Trying to solve G = 0 with small coefficients. Works if det(G) = 1, dim <= 6 and G is LLL-reduced.
+Return G,Identity if no solution is found. Exit with a norm 0 vector if one such is found.
+If base = 1 and norm 0 is obtained returns H'*G*H, H, sol 
+where sol is a norm 0 vector and is the 1st column of H.
 """
 function quad_form_solve_triv(G, base = 0)
-    n = ncols(G)
-    H = one(parent(G))
-   
-
-    #Case 1: A basis vector is isotropic
-    for i = 1:n
-        if(G[i,i] == 0)
-            
-            sol = H[:,i]
-            if(base == 0)
-                d = Dict([1 => sol])
-                return d
-            end
-            H[:,i] = H[:,1]
-            H[:,1] = sol
-
-            d = Dict([ 1 => H'*G*H, 2 => H , 3 => sol])
-            return d
-        end
-    end
-
-    #Case 2: G has a block +- [1 0 ; 0 -1] on the diagonal
-    for i = 2:n
-        if(G[i-1,i] == 0 && G[i-1,i-1]*G[i,i] == -1)
-           
-            H[i-1,i] = -1
-            sol = H[:,1]
-            if (base == 0)
-                d = Dict([1 => sol])
-                return d
-            end 
-            H[:,i] = H[:,1]
-            H[:,1] = sol
-
-            d = Dict([ 1 => H'*G*H, 2 => H , 3 => sol])
-            return d
-        end
-    end
-    
-    
-    #Case 3: a principal minor is 0
-    for i = 1:n
-        
-        GG = G[1:i,1:i]
-        if(det(GG) != 0)
-            continue
-        end
-    
-        sol = kernel(GG)[2][:,1]
-        sol = divexact(sol,content(sol))
-        sol = vcat(sol,zero(GG,n-i,1))
-
-        if (base == 0)
-            d = Dict([1 => sol])
-            return d
-        end
-        
-        H = complete_to_basis(sol)
-        H[:,n] = - H[:,1]
-        H[:,1] = sol
-        
-        d = Dict([ 1 => H'*G*H, 2 => H , 3 => sol])
+  n = ncols(G)
+  H = one(parent(G))
+  
+  #Case 1: A basis vector is isotropic
+  for i = 1:n
+    if(G[i,i] == 0)  
+      sol = H[:,i]
+      if(base == 0)
+        d = Dict([1 => sol])
         return d
+      end
+      H[:,i] = H[:,1]
+      H[:,1] = sol
+      d = Dict([ 1 => H'*G*H, 2 => H , 3 => sol])
+      return d
     end
+  end
 
-    d = Dict([1 => G, 2 => H])
+  #Case 2: G has a block +- [1 0 ; 0 -1] on the diagonal
+  for i = 2:n
+    if(G[i-1,i] == 0 && G[i-1,i-1]*G[i,i] == -1)
+      println()
+      H[i-1,i] = -1
+      sol = H[:,i]
+      if (base == 0)
+        d = Dict([1 => sol])
+        return d
+      end 
+      H[:,i] = H[:,1]
+      H[:,1] = sol
+      d = Dict([ 1 => H'*G*H, 2 => H , 3 => sol])
+      return d
+    end
+  end
+
+  #Case 3: a principal minor is 0
+  for i = 1:n
+    GG = G[1:i,1:i]
+    if(det(GG) != 0)
+      continue
+    end
+    sol = kernel(GG)[2][:,1]
+    sol = divexact(sol,content(sol))
+    sol = vcat(sol,zero(GG,n-i,1))
+    if (base == 0)
+      d = Dict([1 => sol])
+      return d
+    end
+    H = complete_to_basis(sol)
+    H[:,n] = - H[:,1]
+    H[:,1] = sol    
+    d = Dict([ 1 => H'*G*H, 2 => H , 3 => sol])
     return d
+  end
+
+  d = Dict([1 => G, 2 => H])
+  return d
 end
 
+@doc Markdown.doc"""
+    quad_form_lll_gram_indefgoon(G::MatElem) -> Dictionary{Int64, MatElem}  
 
+LLL reduction of the quadratic form G (Gram matrix)
+"""
+function quad_form_lll_gram_indefgoon(G::MatElem)
+  red = quad_form_lll_gram_indef(G,1)
 
-M = MatrixSpace(ZZ,2,2)
-G = M([1 2; 2 5])
-a = quad_form_lll_gram_indef(G)
+  #If no isotropic vector is found
+  if (length(red) == 2)
+    return red 
+  end
+
+  U1 = red[2]
+  G2 = red[1] 
+  U2 = mathnf(G2[1,:])[2]
+  G3 = U2'*G2*U2
+
+  #The first line of the matrix G3 only contains 0, except some 'g' on the right, where g² | det G.
+  n = ncols(G)
+  U3 = one(parent(G))
+  U3[1,n] = round(- G3[n,n]//2*1//G3[1,n])
+  G4 = U3'*G3*U3
+  
+  #The coeff G4[n,n] is reduced modulo 2g
+  U = G4[[1,n],[1,n]]
+
+  if (n == 2)
+    V = zero(parent(G[:,1]))
+  else
+    V = vecextract(G4, [1,n] , 1<<(n-1)-2)
+  end
+  
+  B = round_matrix(-inv(change_base_ring(FractionField(base_ring(U)),U))*V)
+  B = change_base_ring(base_ring(G),B)
+  U4 = one(parent(G))
+
+  for j = 2:n-1
+    U4[1,j] = B[1,j-1]
+    U4[n,j] = B[2,j-1]  
+  end
+
+  G5 = U4'*G4*U4
+  
+  # The last column of G5 is reduced
+  if (n  < 4)
+    d = Dict(1 => G5 , 2 => U1*U2*U3*U4)
+    return d
+  end
+
+  red = quad_form_lll_gram_indefgoon(G5[2:n-1,2:n-1])
+  One = one(MatrixSpace(base_ring(G)))
+  U5 = diagonal_matrix(One,red[2],One)
+  G6 = U5'*G5*U5
+  
+  d = Dict(1 => G6, 2 => U1*U2*U3*U4*U5)
+  return d
+end 
+
 
--- test/QuadForm/indefiniteLLL.jl
+++ test/QuadForm/indefiniteLLL.jl
@@ -1,68 +1,69 @@
 @testset "indefiniteLLL" begin
+
+######################################################
+#                   complete_to_basis
+######################################################
+
+L = MatrixSpace(ZZ,5,4)
+S = MatrixSpace(ZZ,3,4)
+w = L([ 0 2  3  0 ; -5 3 -5 -5; 4 3 -5  4; 1 2 3 4; 0 1 0 0])
+v = S([ 0 2  3  0; -5 3 -5 -5; 4 3 -5  4])
     
-    ######################################################
-    #                   complete_to_basis
-    ######################################################
-    n = 5 
-    m = 4
+x = Hecke.complete_to_basis(w)
+y = Hecke.complete_to_basis(v)
 
-    L = MatrixSpace(ZZ,n,m)
-    S = MatrixSpace(ZZ,3,4)
-    w = L([ 0 2  3  0 ; -5 3 -5 -5; 4 3 -5  4; 1 2 3 4; 0 1 0 0])
-    v = S([ 0 2  3  0; -5 3 -5 -5; 4 3 -5  4])
-    
-    x = Hecke.complete_to_basis(w)
-    y = Hecke.complete_to_basis(v)
+@test x[:,ncols(x)] == w[:,ncols(w)]
+@test det(x) == 1 || det(x) == -1
+@test y[:,ncols(y)] == v[:,ncols(v)]
+@test det(y) == 1 || det(y) == -1
 
-    @test x[:,ncols(x)] == w[:,ncols(w)]
-    @test det(x) == 1 || det(x) == -1
+######################################################
+#                   ker_mod_p
+######################################################
 
-    @test y[:,ncols(y)] == v[:,ncols(v)]
-    @test det(y) == 1 || det(y) == -1
+p1 = 3
+p2 = 4
 
-    ######################################################
-    #                   ker_mod_p
-    ######################################################
+rank1, U1 = Hecke.ker_mod_p(v,p1)
+rank2, U2 = Hecke.ker_mod_p(w,p2)
+v_mod_p = change_base_ring(ResidueRing(ZZ,p1),v)
+w_mod_p = change_base_ring(ResidueRing(ZZ,p2),w)
 
-    p1 = 3
-    p2 = 4
+if (rank1 != 0)
+  u1 = change_base_ring(ResidueRing(ZZ,p1),U1[:,1:rank1])
+  for i = 1:rank1
+    @test v_mod_p*u1[:,i] == 0
+  end
+end
 
-    rank1, U1 = Hecke.ker_mod_p(v,p1)
-    rank2, U2 = Hecke.ker_mod_p(w,p2)
+if (rank2 != 0)
+  u2 = change_base_ring(ResidueRing(ZZ,p2),U2[:,1:rank2])
+  for i = 1:rank2
+    @test w_mod_p*u2[:,i] == 0
+  end
+end
 
-    v_mod_p = change_base_ring(ResidueRing(ZZ,p1),v)
-    w_mod_p = change_base_ring(ResidueRing(ZZ,p2),w)
+######################################################
+#              quad_form_solve_triv
+######################################################
 
-    if (rank1 != 0)
-        u1 = change_base_ring(ResidueRing(ZZ,p1),U1[:,1:rank1])
-        for i = 1:rank1
-            @test v_mod_p*u1[:,i] == 0
-        end
-    end
+M = MatrixSpace(ZZ,2,2)
+G = M([1 2; 2 3])
+v = Hecke.quad_form_solve_triv(G)
+d = Dict([1 => G, 2 => one(parent(G))])
 
-    if (rank2 != 0)
-        u2 = change_base_ring(ResidueRing(ZZ,p2),U2[:,1:rank2])
-        for i = 1:rank2
-            @test w_mod_p*u2[:,i] == 0
-        end
-    end
+if (length(v) == 2)
+  @test v == d
+elseif (length(v) == 3)
+  @test v[2][:,1] == v[3]
+  @test v[3]' * G * v[3] == 0
+else
+  @test v'*G*v == 0
+end
 
-    
-    ######################################################
-    #              quadratic_form_solve_triv
-    ######################################################
-
-    M = MatrixSpace(ZZ,2,2)
-    G = M([1 2; 2 3])
-    v = Hecke.quad_form_solve_triv(G)
-    d = Dict([1 => G, 2 => one(parent(G))])
-    if (length(v) == 2)
-        @test v == d
-    elseif (length(v) == 3)
-        @test v[2][:,1] == v[3]
-        @test v[3]' * G * v[3] == 0
-    else
-        @test v'*G*v == 0
-    end
+######################################################
+#             quad_form_lll_gram_indef
+#######################################################
+d = Hecke.quadratic_form_lll_gram_indef(G)
     
 end
