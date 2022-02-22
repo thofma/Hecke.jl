1f47ea46bbcc28a7687c7089d48610b1b319f747
YvonneNeuy <YvonneNeuy@gmail.com>
2022-02-22 13:33:01 +0100

    Fixes in quad_form_lll_indef

--- src/QuadForm/indefiniteLLL.jl
+++ src/QuadForm/indefiniteLLL.jl
@@ -39,6 +39,7 @@ function vecextract(M::MatElem, x,y::Int64)
   return M[x , list_y]
 
 end
+
 ################################################################################
 #                           linear algebra
 ################################################################################
@@ -51,9 +52,9 @@ The function computes the Hessian matrix $H$ of dimension
 nxm and the unimodular transformation matrix $U$ such that A U = H.
 """
 function mathnf(A::MatElem)
-  H, U = hnf_with_transform(reverse_cols(A'))
-  H = reverse_rows(reverse_cols(H'))
-  U = reverse_cols(U')
+  H, U = hnf_with_transform(reverse_cols(transpose(A)))
+  H = reverse_rows(reverse_cols(transpose(H)))
+  U = reverse_cols(transpose(U))
 
   return H, U
 end
@@ -77,14 +78,14 @@ function complete_to_basis(v::MatElem, redflag = 0)
     return v
   end
 
-  U = inv(mathnf(v')[2]')
+  U = inv(transpose(mathnf(transpose(v))[2]))
 
   if(n == 1 || redflag == 0)
     return U
   end
 
-  re = vecextract(U,1<<n-1 , 1<<(n-m)-1)
-  re = lll_with_transform(re')[2]'
+  re = U[:,1:n-m]
+  re = transpose(lll_with_transform(transpose(re))[2])
 
   l = diagonal_matrix(re,one(parent(v[1:m,1:m])))
   re = U*l
@@ -106,67 +107,6 @@ function ker_mod_p(M::MatElem,p)
   reverse_cols!(U)
 
   return rank, U
-end
-
-################################################################################
-#                           Quadratic Forms Reduction
-################################################################################
-
-@doc Markdown.doc"""
-    quadratic_form_lll_gram_indef(G::MatElem,base=0) -> Dict{Int64, MatElem}
-
-Performs a LLL-reduction on a positive definite quadratic form QD bounding the indefinite G
-Then finishes the reduction with
-"""
-function quad_form_lll_gram_indef(G::MatElem,base=0)
-  n = ncols(G)
-  M = one(parent(G))
-  QD = G
-  MS = MatrixSpace(FractionField(base_ring(G)),n,n)
-
-  for i = 1:n-1
-    if(QD[i,i] == 0)
-      return quad_form_solve_triv(G, base)
-    end
-
-    M1 = one(MS)
-    for j = 1:n
-      M1[i,j] = - QD[i,j]//QD[i,i]
-    end
-
-    M1[i,i] = 1
-    M = M*M1
-    QD = M1'*QD*M1
-  end
-
-  M = inv(M)
-  QD = M'*abs_matrix(QD)*M
-  QD = QD*denominator(QD)
-  QD_cont = divexact(QD,content(QD))
-  QD_cont = change_base_ring(base_ring(G),QD_cont)
-  rank_QD = rank(QD_cont)
-
-  S = lll_gram_with_transform(QD_cont)[2]'
-  S = S[:,ncols(S)-rank_QD+1:ncols(S)]
-
-  if(ncols(S) < n)
-    S = complete_to_basis(S)
-  end
-
-  red = quad_form_solve_triv(S'*G*S,base)
-
-  if(length(red) == 1)
-    red[1] = S*red[1]
-    return  red
-  end
-
-  red[2] = S*red[2]
-
-  if(length(red) == 3)
-    red[3] = S*red[3]
-  end
-
-  return red
 end
 
 ################################################################################
@@ -195,7 +135,7 @@ function quad_form_solve_triv(G, base = 0)
       end
       H[:,i] = H[:,1]
       H[:,1] = sol
-      d = Dict([ 1 => H'*G*H, 2 => H , 3 => sol])
+      d = Dict([ 1 => transpose(H)*G*H, 2 => H , 3 => sol])
       return d
     end
   end
@@ -212,7 +152,7 @@ function quad_form_solve_triv(G, base = 0)
       end
       H[:,i] = H[:,1]
       H[:,1] = sol
-      d = Dict([ 1 => H'*G*H, 2 => H , 3 => sol])
+      d = Dict([ 1 => transpose(H)*G*H, 2 => H , 3 => sol])
       return d
     end
   end
@@ -233,13 +173,77 @@ function quad_form_solve_triv(G, base = 0)
     H = complete_to_basis(sol)
     H[:,n] = - H[:,1]
     H[:,1] = sol
-    d = Dict([ 1 => H'*G*H, 2 => H , 3 => sol])
+    d = Dict([ 1 => transpose(H)*G*H, 2 => H , 3 => sol])
     return d
   end
 
   d = Dict([1 => G, 2 => H])
   return d
 end
+
+################################################################################
+#                           Quadratic Forms Reduction
+################################################################################
+
+@doc Markdown.doc"""
+    quadratic_form_lll_gram_indef(G::MatElem,base=0) -> Dict{Int64, MatElem}
+
+Performs a LLL-reduction on a positive definite quadratic form QD bounding the indefinite G
+Then finishes the reduction with quad_form_solve_triv.
+"""
+function quad_form_lll_gram_indef(G::MatElem,base=0)
+  n = ncols(G)
+  M = one(parent(G))
+  QD = G
+  MS = identity_matrix(FractionField(base_ring(G)),n)
+
+  # GSO breaks off if one of the basis vectors is isotropic
+  for i = 1:n-1
+    if(QD[i,i] == 0)
+      return quad_form_solve_triv(G, base)
+    end
+
+    M1 = one(MS)
+    for j = 1:n
+      M1[i,j] = - QD[i,j]//QD[i,i]
+    end
+
+    M1[i,i] = 1
+    M = M*M1
+    QD = transpose(M1)*QD*M1
+  end
+
+  M = inv(M)
+  QD = transpose(M)*abs_matrix(QD)*M
+  QD = QD*denominator(QD)
+  QD_cont = divexact(QD,content(QD))
+  QD_cont = change_base_ring(base_ring(G),QD_cont)
+  rank_QD = rank(QD_cont)
+
+  S = transpose(lll_gram_with_transform(QD_cont)[2])
+  S = S[:,ncols(S)-rank_QD+1:ncols(S)]
+
+  if(ncols(S) < n)
+    S = complete_to_basis(S)
+  end
+
+  red = quad_form_solve_triv(transpose(S)*G*S,base)
+
+  if(length(red) == 1)
+    red[1] = S*red[1]
+    return  red
+  end
+
+  red[2] = S*red[2]
+
+  if(length(red) == 3)
+    red[3] = S*red[3]
+  end
+
+  return red
+end
+
+
 
 @doc Markdown.doc"""
     quad_form_lll_gram_indefgoon(G::MatElem) -> Dictionary{Int64, MatElem}
@@ -257,13 +261,13 @@ function quad_form_lll_gram_indefgoon(G::MatElem)
   U1 = red[2]
   G2 = red[1]
   U2 = mathnf(G2[1,:])[2]
-  G3 = U2'*G2*U2
+  G3 = transpose(U2)*G2*U2
 
   #The first line of the matrix G3 only contains 0, except some 'g' on the right, where g² | det G.
   n = ncols(G)
   U3 = one(parent(G))
   U3[1,n] = round(- G3[n,n]//2*1//G3[1,n])
-  G4 = U3'*G3*U3
+  G4 = transpose(U3)*G3*U3
 
   #The coeff G4[n,n] is reduced modulo 2g
   U = G4[[1,n],[1,n]]
@@ -274,9 +278,7 @@ function quad_form_lll_gram_indefgoon(G::MatElem)
     V = vecextract(G4, [1,n] , 1<<(n-1)-2)
   end
 
-  B = -inv(change_base_ring(FractionField(base_ring(U)),U))*V
   B = round_matrix(-inv(change_base_ring(FractionField(base_ring(U)),U))*V)
-  #B = change_base_ring(base_ring(G),B)
   U4 = one(parent(G))
 
   for j = 2:n-1
@@ -284,7 +286,7 @@ function quad_form_lll_gram_indefgoon(G::MatElem)
     U4[n,j] = B[2,j-1]
   end
 
-  G5 = U4'*G4*U4
+  G5 = transpose(U4)*G4*U4
 
   # The last column of G5 is reduced
   if (n  < 4)
@@ -295,7 +297,7 @@ function quad_form_lll_gram_indefgoon(G::MatElem)
   red = quad_form_lll_gram_indefgoon(G5[2:n-1,2:n-1])
   One = one(MatrixSpace(base_ring(G)))
   U5 = diagonal_matrix(One,red[2],One)
-  G6 = U5'*G5*U5
+  G6 = transpose(U5)*G5*U5
 
   d = Dict(1 => G6, 2 => U1*U2*U3*U4*U5)
   return d
