2a7f2738e18cb7ea61a303611123fe6f7da8c43b
Johannes Schmitt <jschmitt@posteo.eu>
2022-03-03 19:04:14 +0100

    Sparse linear system solving (#611)
    * Sparse linear system solving
    
    * Remove push_row! (already exists) and insert_row! -> insert!
    
    * Remove echelon!

--- src/Sparse/Rref.jl
+++ src/Sparse/Rref.jl
@@ -52,22 +52,6 @@ function rref!(A::SMat{T}; truncate::Bool = false) where {T <: FieldElement}
   return rankA
 end
 
-function insert_row!(A::SMat{T}, i::Int, r::SRow{T}) where T
-  insert!(A.rows, i, r)
-  A.r += 1
-  A.nnz += length(r)
-  A.c = max(A.c, r.pos[end])
-  return A
-end
-
-function push_row!(A::SMat{T}, r::SRow{T}) where T
-  push!(A.rows, r)
-  A.r += 1
-  A.nnz += length(r)
-  A.c = max(A.c, r.pos[end])
-  return A
-end
-
 # Reduce v by M and if the result is not zero add it as a row (and then reduce
 # M to maintain the rref).
 # Return true iff v is not in the span of the rows of M.
@@ -109,12 +93,14 @@ function _add_row_to_rref!(M::SMat{T}, v::SRow{T}) where { T <: FieldElem }
   end
 
   # Multiply v by inv(v.values[1])
-  t = inv(v.values[1])
-  for j = 2:length(v)
-    v.values[j] = mul!(v.values[j], v.values[j], t)
+  if !isone(v.values[1])
+    t = inv(v.values[1])
+    for j = 2:length(v)
+      v.values[j] = mul!(v.values[j], v.values[j], t)
+    end
+    v.values[1] = one(base_ring(M))
   end
-  v.values[1] = one(base_ring(M))
-  insert_row!(M, new_row, v)
+  insert!(M, new_row, v)
 
   # Reduce the rows above the newly inserted one
   for i = 1:new_row - 1
