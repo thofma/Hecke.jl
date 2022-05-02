cec84eda7258bf9fca970ff3cbb811c6d899dd1a
Yvonne Neuy <YvonneNeuy@gmail.com>
2022-05-02 13:54:50 +0200

    tests adjusted

--- test/QuadForm/indefiniteLLL.jl
+++ test/QuadForm/indefiniteLLL.jl
@@ -57,12 +57,12 @@
 #  lll_gram_indefgoon
 #######################################################
 
-  function find_delta(H::MatElem{fmpq})
+  function _find_gamma(H::MatElem{fmpq})
     O,M = Hecke._gram_schmidt(H,QQ)
     d = diagonal(O)
-    delta =[abs(d[i])//abs(d[i+1]) for i=1:ncols(H)-1]
-    delta_max = maximum(delta)
-    return delta_max
+    gamma =[abs(d[i])//abs(d[i+1]) for i=1:ncols(H)-1]
+    gamma_max = maximum(gamma)
+    return gamma_max
   end
   
   G0 = ZZ[0 1 2; 1 -1 3; 2 3 0]
@@ -72,24 +72,35 @@
   G1 = ZZ[1 2 3; 2 -1 0 ; 3 0 0]
   H1,U1 = Hecke.lll_gram_indefgoon(G1)
   @test transpose(U1)*G1*U1 == H1
-  @test find_delta(change_base_ring(QQ,H1)) <= find_delta(change_base_ring(QQ,G1))
+  @test find_gamma(change_base_ring(QQ,H1)) <= find_gamma(change_base_ring(QQ,G1))
   
   A = ZZ[1 -1 2 3; 2 4 0 1; 0 0 -1 3; 1 1 2 0]
   G2 = A+transpose(A)
   H2,U2 = Hecke.lll_gram_indefgoon(G2)
   @test transpose(U2)*G2*U2 == H2
-  @test find_delta(change_base_ring(QQ,H2)) <= find_delta(change_base_ring(QQ,G2))
+  @test find_gamma(change_base_ring(QQ,H2)) <= find_gamma(change_base_ring(QQ,G2))
 
   G3 = ZZ[1 0 -2 3;0 -1 1 1;-2 1 0 4; 3 1 4 0]
   H3,U3 = Hecke.lll_gram_indefgoon(G3)
   @test transpose(U3)*G3*U3 == H3
-  @test find_delta(change_base_ring(QQ,H3[2:3,2:3])) <= find_delta(change_base_ring(QQ,G3[2:3,2:3]))
+  @test find_gamma(change_base_ring(QQ,H3[2:3,2:3])) <= find_gamma(change_base_ring(QQ,G3[2:3,2:3]))
 
   G4 = ZZ[2 2 2 0 3; 2 0 3 1 0;2 3 -6 -4 -3; 0 1 -4 2 6; 3 0 -3 6 0]
   H4,U4 = Hecke.lll_gram_indefgoon(G4)
   @test transpose(U4)*G4*U4 == H4
-  @test find_delta(change_base_ring(QQ,H4)) <= find_delta(change_base_ring(QQ,G4))
+  @test find_gamma(change_base_ring(QQ,H4)) <= find_gamma(change_base_ring(QQ,G4))
   
+  G5 = ZZ[2 2 1 12 0 0; 2 -4 -1 1 2 6;1 -1 8 1 2 5;12 1 1 2 0 2 ; 0 2 2 0 0 0; 0 6 5 2 0 -2]
+  H5,U5 = Hecke.lll_gram_indefgoon(G5)
+  @test transpose(U5)*G5*U5 == H5
+  @test find_gamma(change_base_ring(QQ,H5)) <= find_gamma(change_base_ring(QQ,G5))
+
+  G6 = ZZ[2 0 3 2 0 6; 0 6 0 2 1 0; 3 0 -4 0 1 1;2 2 0 -4 2 4;0 1 1 2 4 -2;6 0 1 4 -2 4]
+  H6,U6 = Hecke.lll_gram_indefgoon(G6)
+  @test transpose(U6)*G6*U6 == H6
+  @test find_gamma(change_base_ring(QQ,H6)) <= find_gamma(change_base_ring(QQ,G6))
+  
+
   #=
   G2 = ZZ[1 2 3; 2 -1 -1; 3 -1 0] 
   H2,U2 = Hecke.lll_gram_indefgoon(G2)
@@ -114,19 +125,14 @@
   G6 = ZZ[1 2 ;2 -1]
   H6,U6 = Hecke.lll_gram_indefgoon(G6)
   @test transpose(U6)*G6*U6 == H6
-<<<<<<< HEAD
-  @test abs(det(U6)) == 1  #G7 = ZZ[1 2 3 4 5 6; 2 1 0 0 0 0; 3 0 1 0 0 0; 4 0 0 1 0 0 ; 5 0 0 0 5 2; 6 0 0 0 2 -3]
-=======
-  @test abs(det(U6)) == 1 
->>>>>>> f64fffd564349fc7ddce9b110a4449688dfa5ab4
-  @test find_delta(change_base_ring(QQ,H6)) <= find_delta(change_base_ring(QQ,G6))
+  @test find_gamma(change_base_ring(QQ,H6)) <= find_gamma(change_base_ring(QQ,G6))
   
   G7 = ZZ[1 2 3 4 5 6; 2 1 0 0 0 0; 3 0 1 0 0 0; 4 0 0 1 0 0 ; 5 0 0 0 5 2; 6 0 0 0 2 -3]
   L7_G = Zlattice(gram = G7)
   H7,U7 = Hecke.lll_gram_indefgoon(G7)
   L7_H = Zlattice(gram = H7)
   @test isrationally_isometric(L7_G,L7_H)
-  #@test find_delta(change_base_ring(QQ,H7)) <= find_delta(change_base_ring(QQ,G7))
+  #@test find_gamma(change_base_ring(QQ,H7)) <= find_gamma(change_base_ring(QQ,G7))
   
   gen = genera((2,2),1)
   L = representative(gen[1]) #Takes pretty long to compute
