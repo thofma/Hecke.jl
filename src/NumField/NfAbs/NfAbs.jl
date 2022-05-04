f96399555c3f8b1450dc8df6b594b879dd3ad378
Thomas Breuer <sam@math.rwth-aachen.de>
2022-05-04 11:33:39 +0200

    fixed comparison of `nf_elem` with different parents (#677)

--- src/NumField/NfAbs/NfAbs.jl
+++ src/NumField/NfAbs/NfAbs.jl
@@ -1207,7 +1207,7 @@ end
 function common_super(A::NumField, B::NumField)
   A === B && return A
   if Nemo.iscyclo_type(A) && Nemo.iscyclo_type(B)
-    return cyclotomic_field(lcm(get_attribute(A, :cyclotomic_field), get_attribute(B, :cyclotomic_field)))[1]
+    return cyclotomic_field(lcm(get_attribute(A, :cyclo), get_attribute(B, :cyclo)))[1]
   end
 
   c = intersect(find_all_super(A), find_all_super(B))
