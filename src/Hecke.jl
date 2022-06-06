046ceef0eb6d2a306030f0c264fecdd1f588348e
Tommy Hofmann <thofma@gmail.com>
2022-06-06 20:11:32 +0200

    Fix export (#727)

--- src/Hecke.jl
+++ src/Hecke.jl
@@ -31,6 +31,7 @@
 #
 ################################################################################
 
+
 @doc Markdown.doc"""
 Hecke is a Julia package for algorithmic algebraic number theory.
 For more information please visit
