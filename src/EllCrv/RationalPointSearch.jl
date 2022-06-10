7271e9edbc3b5d9c58cc652a9493e5182ccab3ba
Tommy Hofmann <thofma@gmail.com>
2022-06-10 10:23:30 +0200

    Some tweaks

--- src/EllCrv/RationalPointSearch.jl
+++ src/EllCrv/RationalPointSearch.jl
@@ -9,20 +9,8 @@
 
 export find_points
 
-
-
-function find_points(coefficients::Array{fmpz}, bound::Union{Integer, fmpz}, N = 2^14, P = 40, Pfirst = 30)
-
-  #N is size of chunks in which we subdivide
-  #P is the number of primes we consider for sieving
-  #Pfirst is the number of optimal primes we consider 
-
-  #Number of parts
-  H_parts = Int(div(2*bound + 1, N))
-  
-  rest = rem(2*bound + 1, N)
-
-  primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,
+const _primes_for_sieve =
+ [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,
  107,109,113,127,131, 137,139,149,151,157,163,167,173,179,181,191,193,197,199,
  211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,
  317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,
@@ -31,59 +19,94 @@ function find_points(coefficients::Array{fmpz}, bound::Union{Integer, fmpz}, N =
  677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,
  821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,
  947,953,967,971,977,983,991,997,1009,1013,1019,1021]
- 
-  primes = primes[1:P]
- 
+
+function find_points(coefficients::Vector{fmpz}, bound::IntegerUnion, N = 2^14, P = 40, Pfirst = 30)
+  # This function is table unstable, but it does not matter
+  # We just assemble the values with a minimal type,
+  # and it will specialize in the inner function _find_points
+  if bound isa fmpz && fits(Int, bound)
+    _bound = Int(bound)
+  else
+    _bound = bound
+  end
+
+  if all(x -> fits(Int, x), coefficients)
+    _coefficients = Int.(coefficients)
+  else
+    _coefficients = coefficients
+  end
+
+  return _find_points(_coefficients, _bound, N, P, Pfirst)
+end
+
+function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^14, P = 40, Pfirst = 30)
+
+  #N is size of chunks in which we subdivide
+  #P is the number of primes we consider for sieving
+  #Pfirst is the number of optimal primes we consider
+
+  #Number of parts
+  H_parts = Int(div(2*bound + 1, N))
+
+  rest = rem(2*bound + 1, N)
+
+  primes = _primes_for_sieve[1:P]
+
+  # Define the polynomial because we like to evaluate it
+  f = Hecke.Globals.Qx(coefficients)
+
  #Take the Pfirst primes that are most optimal for sieving
-  best_primes = []
+  best_primes = Tuple{Int, fmpq}[]
   for p in primes
-    F = GF(p)
+    F = GF(p, cached = false)
     order = Hecke.order_via_exhaustive_search(map(F, coefficients))
     push!(best_primes, (p, order//p^2))
   end
- 
+
   sort!(best_primes, by = last)
- 
-  primes = [p for (p,q) in best_primes[1:Pfirst]]
- 
- 
-  #H[m][n] contains sieving info for the residue class k-1 mod m 
-  H = Vector{BitVector}[]      
-  p_starts = []
+
+  primes = Int[p for (p,q) in best_primes[1:Pfirst]]
+
+  #H[m][n] contains sieving info for the residue class k-1 mod m
+  H = Vector{Vector{Bool}}[]
+  p_starts = Int[]
   for p in primes
     p_sieve = prime_check_arrays(coefficients, p, N)
     push!(H, p_sieve)
     push!(p_starts, mod(-bound, p))
   end
   #candidates = fill(trues(N), H_parts)
-  candidates = [trues(N) for i in 1:H_parts]
-  ce = BitArray(x <= rest for x = 1:N)
+  candidates = Vector{Bool}[ones(Bool, N) for i in 1:H_parts]
+  ce = Bool[x <= rest for x = 1:N]
   push!(candidates, copy(ce))
-  
+
 
   #Currently only doesn't have infinity.
-  res = Vector{fmpq}[]
+  res = Tuple{fmpq, fmpq}[]
 
   n = length(coefficients)
   #Determine the set of denumerators b
-  
-  B = fmpz[]
-  
-  #if the polynomial is odd we can restrict the possible denominators to the ones of the form a*b^2 
+
+  #if the polynomial is odd we can restrict the possible denominators to the ones of the form a*b^2
   #where a is a non-square-free divisor of the leading coefficent
   if isodd(n - 1)
+    BB = Int[]
     leadingcoeff = coefficients[n]
     q = Hecke.squarefree_part(leadingcoeff)
     d = divisors(q)
-    sqrt_bound = fmpz(floor(sqrt(bound)))
+    sqrt_bound = isqrt(bound)
     for a in d
-      append!(B, [a*b^2 for b in (1:sqrt_bound)])
+      append!(BB, (a*b^2 for b in 1:sqrt_bound))
     end
-    filter!(t -> t <= bound, B) 
+    B = collect(filter!(t -> t <= bound, BB))
   else
-    B = (1:bound)
+    B = collect(1:bound)
   end
-  
+
+  shifter = ones(Bool, N)
+
+  shif = view(shifter, 1:N)
+
   for b in B
     #Initiate candidate list as chunks of BitArrays of size N with true everywhere
     #candidatesn = fill(trues(N), H_parts)
@@ -92,47 +115,57 @@ function find_points(coefficients::Array{fmpz}, bound::Union{Integer, fmpz}, N =
     #@show typeof(candidates)
 
     # Reset the candidates
-    for i in 1:H_parts
+    @inbounds for i in 1:H_parts
       fill!(candidates[i], true)
     end
-    candidates[end] = copy(ce)
-    
+    candidates[end] .= ce
+
     for i in 1:length(primes)
-      p = primes[i]
+      p = @inbounds primes[i]
       if p < N
         shift = -rem(N, p)
       else
         shift = -N
       end
-      
-      offset = p_starts[i]
-      k = Int(mod(b, p))
+
+      offset = @inbounds p_starts[i]
+      k = mod(b, p)
 
       #Need to shift by 1 as H[i][k] corresponds to k-1 mod p
-      p_sieve = H[i][k + 1]
+      p_sieve = @inbounds H[i][k + 1]
       #@show p_sieve
-      shifter = trues(length(p_sieve))
+      resize!(shifter, length(p_sieve))
       fill!(shifter, true)
+      #shifter = trues(length(p_sieve))
+      #@show length(p_sieve)
+      #fill!(shifter, true)
       #@assert length(p_sieve) == N
       #@assert length(shifter) == length(p_sieve)
 
-      for j in 1:(H_parts +1)
+
+      for j in 1:(H_parts + 1)
         #Storing the shifted p_sieve into shifter is apparently faster
 
         circshift!(shifter, p_sieve, (j-1)*shift - offset)
         #Do a broadcasted & on the candidate list with the shifted p_sieve
-        candidates[j] .= candidates[j] .& shifter[1:N]
+        # shif is view(shifter, 1:N) aka shifter[1:N]
+        @inbounds candidates[j] .&= shif
       end
     end
-    
+
+    _b = fmpz(b)
+
     #Print potential rational points
     for i in 1:length(candidates)
       #if candidates[i]!= falses(N)
       S = findall(candidates[i])
-      for s in S
-        a = fmpz((i - 1)*N + s - bound - 1)
-        if gcd(a, b) == 1
-          append!(res, points_with_x(coefficients, a//b)) 
+      if length(S) > 0
+        _a = (i - 1) * N - bound - 1
+        for s in S
+          a = _a + s
+          if gcd(a, b) == 1
+            points_with_x!(res, coefficients, a//_b, f)
+          end
         end
       end
     end
@@ -140,33 +173,54 @@ function find_points(coefficients::Array{fmpz}, bound::Union{Integer, fmpz}, N =
   return res
 end
 
+Hecke.squarefree_part(x::Int) = Int(squarefree_part(fmpz(x)))
+
 
 #Equation y^2 = an*x^n + a_{n-1}*x^(n-1)*z + ... + a1*x*z^(n - 1) + a0*z^n
-function prime_check_arrays(coeff::Array{fmpz}, p::Int, N)
+function prime_check_arrays(coeff::Vector{<: IntegerUnion}, p::Int, N)
 
-  F = GF(p)
+  F = GF(p, cached = false)
   # a contains n+1 elemts : a0, ...., an
   n = length(coeff) - 1
-  
+
   a = map(F, coeff)
-  
-  p_part = Array{BitArray}(undef, p)
+
+  p_part = Vector{Vector{Bool}}(undef, p)
+  az = Vector{elem_type(F)}(undef, n + 1)
+  _chunk = Vector{Bool}(undef, length(F))
   for t in (0:p - 1)
     z = F(t)
+    zpowers = powers(z, n)
     #a[i+1] correponds to a_i above
-    chunk = BitArray(issquare(sum([a[i + 1]*x^i*z^(n - i) for i in (0:n)])) for x in F)
-    
+    for i in 0:n
+      az[i + 1] = a[i + 1] * zpowers[n - i + 1]
+    end
+
+    chunk = _chunk
+
+    for (i, x) in enumerate(F)
+      @inbounds chunk[i] = issquare(sum([az[i + 1]*x^i for i in (0:n)]))
+    end
+    #chunk = Bool[issquare(sum([az[i + 1]*x^i for i in (0:n)])) for x in F]
+
     #Pad the BitArray to have chunks that are at least big enough to do a broadcasted & with
     if p<N
       p_chunks = div(N, p)
-      temp = chunk
-      for t in (1:p_chunks)
-        chunk = vcat(chunk, temp)
+      if p_chunks == 1
+        chunk = append!(chunk, chunk)
+      else
+        chunk = reduce(vcat, [chunk for tt in 1:p_chunks + 1])
       end
+      #temp = chunk
+      #l = length(temp)
+      #for tt in 1:p_chunks
+      #  #chunk = vcat(chunk, temp)
+      #  append!(chunk, @view chunk[1:l])
+      #end
     end
     p_part[t+1] = chunk
   end
-  
+
   return p_part
 end
 
@@ -176,9 +230,9 @@ function mod16_check_arrays(coefficients::Array{fmpz})
   R = ResidueRing(ZZ, 16)
   # a contains n+1 elemts : a0, ...., an
   n = length(coefficients) - 1
-  
+
   a = map(R, coefficients)
-  
+
   part_16 = Array{Int}(undef, 16)
   for t in (0:15)
     z = R(t)
@@ -198,9 +252,9 @@ function mod16_check_arrays(coefficients::Array{fmpz})
         part_16[t+1] = 4
       end
     end
-      
+
   end
-  
+
   return part_16
 end
 
@@ -214,16 +268,12 @@ function Hecke.order_via_exhaustive_search(coeff::Array{T}) where T<:FinFieldEle
   return order
 end
 
-function points_with_x(coeff::Array{fmpz}, x::fmpq)
-  n = length(coeff) - 1
-  test, y = is_square_with_sqrt(sum([coeff[i + 1]*x^i for i in (0:n)]))
-  pts = []
+function points_with_x!(res, coeff::Vector{<: IntegerUnion}, x::fmpq, f)
+  test, y = is_square_with_sqrt(evaluate(f, x))
   if test
-    pts = [[x, y], [x, -y]]
+    push!(res, (x, y), (x, -y))
   end
-  return pts
 end
-
 
 function points_with_x(coeff::Array{T}, x::T) where T
   n = length(coeff) - 1
@@ -278,7 +328,7 @@ mutable struct NegativityCertificate
         @assert !contains(r[i + 1], b)
       end
       if i > 1
-        @assert !contains(r[i - 1], a) 
+        @assert !contains(r[i - 1], a)
       end
       push!(root_intervals, (a, b))
     end
@@ -288,7 +338,7 @@ mutable struct NegativityCertificate
 
     ints = Tuple{fmpq, fmpq}[]
 
-    if signs[1] == -1 # 
+    if signs[1] == -1 #
       z.is_negative_at_negative = true
       z.leftmost_negative = root_intervals[1][1]
     else
@@ -310,7 +360,7 @@ mutable struct NegativityCertificate
 
     z.intervals = ints
 
-    return z 
+    return z
   end
 end
 
@@ -341,4 +391,5 @@ end
 
 # Return a collection I = [(a1, a2), ..., ]
 # such that whenever x in (a1, a2) for some element of I, then f(x) < 0
+
 
