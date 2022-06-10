be9619e146dbf7f358516cc37dc45a57a9a7a627
jeroen <jeroen@debian.jeroenthuis.edu>
2022-06-10 20:54:46 +0200

    intervals

--- src/EllCrv/RationalPointSearch.jl
+++ src/EllCrv/RationalPointSearch.jl
@@ -7,7 +7,7 @@
 #
 ###############################################################################
 
-export find_points
+export find_points, negative_intervals, _find_points_greater_than, NegativityCertificate
 
 const _primes_for_sieve =
  [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,
@@ -44,6 +44,8 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
   #N is size of chunks in which we subdivide
   #P is the number of primes we consider for sieving
   #Pfirst is the number of optimal primes we consider
+  
+  
 
   #Number of parts
   H_parts = Int(div(2*bound + 1, N))
@@ -60,12 +62,14 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
   for p in primes
     F = GF(p, cached = false)
     order = Hecke.order_via_exhaustive_search(map(F, coefficients))
-    push!(best_primes, (p, order//p^2))
+    push!(best_primes, (p, order//p))
   end
 
-  sort!(best_primes, by = last)
+  #sort!(best_primes, by = last)
 
-  primes = Int[p for (p,q) in best_primes[1:Pfirst]]
+  #primes = Int[p for (p,q) in best_primes[1:Pfirst]]
+
+  #primes = _primes_for_sieve[1:5]
 
   #H[m][n] contains sieving info for the residue class k-1 mod m
   H = Vector{Vector{Bool}}[]
@@ -74,6 +78,7 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
     p_sieve = prime_check_arrays(coefficients, p, N)
     push!(H, p_sieve)
     push!(p_starts, mod(-bound, p))
+    
   end
   #candidates = fill(trues(N), H_parts)
   candidates = Vector{Bool}[ones(Bool, N) for i in 1:H_parts]
@@ -107,6 +112,25 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
 
   shif = view(shifter, 1:N)
 
+  neg_intervals = negative_intervals(f)
+
+  left_bound = neg_ints[1][1]
+  right_bound = neg_ints[3][1]
+  
+  
+  if left_bound != []
+    append!(res, _find_points_in_interval(coefficients, primes, H, B, left_bound, bound, N))
+  end
+  
+  if right_bound != []
+    append!(res, _find_points_greater_than(coefficients, primes, H, B, -bound, right_bound, N))
+  end
+  
+  for I in neg_ints[2]
+    append!(res, _find_points_greater_than(coefficients, primes, H, B, I[1], I[2], N))
+  end
+  
+  #=
   for b in B
     #Initiate candidate list as chunks of BitArrays of size N with true everywhere
     #candidatesn = fill(trues(N), H_parts)
@@ -169,12 +193,82 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
         end
       end
     end
-  end
+  end=#
   return res
 end
 
 Hecke.squarefree_part(x::Int) = Int(squarefree_part(fmpz(x)))
 
+function _find_points_in_interval(coefficients::Vector, primes, H, B, left_bound::fmpq, right_bound::fmpq, N = 2^14)
+
+  res = Tuple{fmpq, fmpq}[]
+  shifter = ones(Bool, N)
+  shif = view(shifter, 1:N)
+  
+  f = Hecke.Globals.Qx(coefficients)
+  
+  for b in B
+    start_interval = max(ceil(fmpz, b*right_bound), -bound)
+    end_interval = min(floor(fmpz, b*right_bound), bound)
+    
+    numerator_range = 1 + bound - start_interval
+    
+    H_parts = Int(div(numerator_range, N))
+    
+    rest = rem(numerator_range, N)
+    
+    candidates = Vector{Bool}[ones(Bool, N) for i in 1:H_parts]
+    ce = Bool[x <= rest for x = 1:N]
+    push!(candidates, copy(ce))
+    
+    for i in 1:length(primes)
+      p = @inbounds primes[i]
+      
+      if p < N
+        shift = -rem(N, p)
+      else
+        shift = -N
+      end
+
+      offset = @inbounds Int(mod(start_interval, p))
+      
+      k = mod(b, p)
+
+      #Need to shift by 1 as H[i][k] corresponds to k-1 mod p
+      p_sieve = @inbounds H[i][k + 1]
+
+      resize!(shifter, length(p_sieve))
+      fill!(shifter, true)
+
+      for j in 1:(H_parts + 1)
+        #Storing the shifted p_sieve into shifter is apparently faster
+
+        circshift!(shifter, p_sieve, (j-1)*shift - offset)
+        #Do a broadcasted & on the candidate list with the shifted p_sieve
+        # shif is view(shifter, 1:N) aka shifter[1:N]
+        @inbounds candidates[j] .&= shif
+      end
+    end
+
+    _b = fmpz(b)
+
+    #Print potential rational points
+    for i in 1:length(candidates)
+      #if candidates[i]!= falses(N)
+      S = findall(candidates[i])
+      if length(S) > 0
+        _a = (i - 1) * N + start - 1
+        for s in S
+          a = _a + s
+          if gcd(a, b) == 1
+            points_with_x!(res, coefficients, a//_b, f)
+          end
+        end
+      end
+    end
+  end
+  return res
+end
 
 #Equation y^2 = an*x^n + a_{n-1}*x^(n-1)*z + ... + a1*x*z^(n - 1) + a0*z^n
 function prime_check_arrays(coeff::Vector{<: IntegerUnion}, p::Int, N)
@@ -195,19 +289,18 @@ function prime_check_arrays(coeff::Vector{<: IntegerUnion}, p::Int, N)
     for i in 0:n
       az[i + 1] = a[i + 1] * zpowers[n - i + 1]
     end
-
+        
     chunk = _chunk
-
-    for (i, x) in enumerate(F)
-      @inbounds chunk[i] = issquare(sum([az[i + 1]*x^i for i in (0:n)]))
-    end
-    #chunk = Bool[issquare(sum([az[i + 1]*x^i for i in (0:n)])) for x in F]
+    #for (j, x) in enumerate(F)
+    #  @inbounds chunk[j] = issquare(sum([az[i + 1]*x^i for i in (0:n)]))
+    #end
+    chunk = Bool[issquare(sum([az[i + 1]*x^i for i in (0:n)])) for x in F]
 
     #Pad the BitArray to have chunks that are at least big enough to do a broadcasted & with
     if p<N
       p_chunks = div(N, p)
       if p_chunks == 1
-        chunk = append!(chunk, chunk)
+        chunk = append!(copy(chunk), chunk)
       else
         chunk = reduce(vcat, [chunk for tt in 1:p_chunks + 1])
       end
@@ -244,6 +337,41 @@ function mod16_check_arrays(coefficients::Array{fmpz})
     else
       evens = [chunk[i] for i in (1:2:16)]
       odds = [chunk[i] for i in (2:2:16)]
+      if odds == falses(8)
+        part_16[t+1] = 2
+      elseif evens == falses(8)
+        part_16[t+1] = 1
+      else
+        part_16[t+1] = 4
+      end
+    end
+
+  end
+
+  return part_16
+end
+
+
+#Equation y^2 = an*x^n + a_{n-1}*x^(n-1)*z + ... + a1*x*z^(n - 1) + a0*z^n
+function modn_check_arrays(coefficients::Array{fmpz}, m)
+
+  R = ResidueRing(ZZ, m)
+  # a contains n+1 elemts : a0, ...., an
+  n = length(coefficients) - 1
+
+  a = map(R, coefficients)
+
+  part_16 = Array{Int}(undef, m)
+  for t in (0:m-1)
+    z = R(t)
+    #a[i+1] correponds to a_i above
+    chunk = BitArray(sum([a[i + 1]*x^i*z^(n - i) for i in (0:n)]) in map(R, [0,1,4,9]) for x in R)
+    @show chunk
+    if chunk == falses(m)
+      part_16[t+1] = 0
+    else
+      evens = [chunk[i] for i in (1:2:m)]
+      odds = [chunk[i] for i in (2:2:m)]
       if odds == falses(8)
         part_16[t+1] = 2
       elseif evens == falses(8)
@@ -359,7 +487,7 @@ mutable struct NegativityCertificate
         push!(res, (root_intervals[i][2], root_intervals[i + 1][1]))
       end
     end
-
+    z.is_negative_at_negative
     z.intervals = ints
 
     return z
