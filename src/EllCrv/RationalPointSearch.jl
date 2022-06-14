9dd1a9c111b2167b0817083c7473d09e0a183890
Tommy Hofmann <thofma@gmail.com>
2022-06-14 10:14:34 +0200

    Faster brr

--- src/EllCrv/RationalPointSearch.jl
+++ src/EllCrv/RationalPointSearch.jl
@@ -47,7 +47,13 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
   
   @req coefficients[end] != 0 "Leading coefficient needs to be non-zero"
 
-  #Number of parts
+  # Cache for some BitVector operations
+  C = Vector{Bool}(undef, Base.bitcache_size)
+
+  # Cache for _findall of BitVector
+  I = Vector{Int}(undef, N)
+
+  # Maximal number of parts
   H_parts = Int(div(2*bound + 1, N))
 
   rest = rem(2*bound + 1, N)
@@ -56,22 +62,16 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
 
   # Define the polynomial because we like to evaluate it
   
-  f = Hecke.Globals.Qx(coefficients)
- #Take the Pfirst primes that are most optimal for sieving
-  best_primes = Tuple{Int, fmpq}[]
-  
-  
-  
-  exclude_denom = []
-  
+  # First decide whether to reverse the polynomial or not
+
   n = length(coefficients)
   odd_degree_original = isodd(n - 1)
-  
   reverse_polynomial = false
-  
-  
-  #If f is of odd degree and the constant term is smaller than the leading coefficient
-  #Reverse the polynomial unless it would result in the polynomial being of odd degree
+  f = Hecke.Globals.Qx(coefficients)
+
+  # If f is of odd degree and the constant term is smaller than the leading
+  # coefficient. Reverse the polynomial unless it would result in the polynomial
+  # being of odd degree.
   if odd_degree_original
     if coefficients[1] < coefficients[n]
       tempcoeff = coefficients
@@ -94,16 +94,18 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
       reverse!(coefficients)
     end
     
-  #TODO: Another check for high divisibility by small non-square primes  
+    #TODO: Another check for high divisibility by small non-square primes  
   end
   
   g = Hecke.Globals.Qx(coefficients)
-  
-  n = length(coefficients)
   odd_degree = isodd(n - 1)
+
+  # Now first compute the primes we will use for sieving
+  # We use those primes such that mod p there are few points
   
-  lead_coeff = coefficients[n]
-  
+  # Take the Pfirst primes that are most optimal for sieving
+  best_primes = Tuple{Int, fmpq}[]
+ 
   for p in primes
     F = GF(p, cached = false)
     order = Hecke.order_via_exhaustive_search(map(F, coefficients))
@@ -117,30 +119,35 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
 
   primes = Int[p for (p,q) in best_primes[1:Pfirst]]
 
+  n = length(coefficients)
+  
+  lead_coeff = coefficients[n]
+ 
   #H[m][n] contains sieving info for the residue class k-1 mod m
-  H = Vector{Vector{Bool}}[]
-  H2_adic_odd = Vector{Vector{Bool}}[]
-  H2_adic_even = Vector{Vector{Bool}}[]
+  H = Vector{BitVector}[]
+  H_temp = BitVector[]
+  H2_adic_odd = Vector{BitVector}[]
+  H2_adic_even = Vector{BitVector}[]
+
   p_starts = Int[]
   for p in primes
-    p_sieve, p_sieve_odd, p_sieve_even = prime_check_arrays(coefficients, p, N)
+    p_sieve, p_sieve_odd, p_sieve_even = prime_check_arrays_2(coefficients, p, N, C)
     push!(H2_adic_odd, p_sieve_odd)
     push!(H2_adic_even,p_sieve_even)
     push!(H, p_sieve)
+    push!(H_temp, copy(p_sieve[1]))
     push!(p_starts, mod(-bound, p))
   end
   
   two_adic_info = mod16_check_arrays(coefficients)
   
   #candidates = fill(trues(N), H_parts)
-  candidates = Vector{Bool}[ones(Bool, N) for i in 1:H_parts]
-  ce = Bool[x <= rest for x = 1:N]
-  push!(candidates, copy(ce))
+  candidates = BitVector[trues(N) for i in 1:(H_parts + 1)]
 
+  exclude_denom = Int[]
 
   #Currently only doesn't have infinity.
   res = Tuple{fmpq, fmpq, fmpq}[]
-
   
   #Determine the set of denumerators b
 
@@ -157,10 +164,7 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
     B = collect(filter!(t -> t <= bound, BB))
   else
     
-    B = []
-    
-    test = function(b)
-    
+    B = filter!(collect(1:bound)) do b
       for p in exclude_denom
         if divisible(b,p)
           return false
@@ -180,10 +184,6 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
       
       return true
     end
-    
-    B = filter!(test, collect(1:bound))
-    
-    #B = collect(1:bound)
   end
 
   shifter = ones(Bool, N)
@@ -196,7 +196,7 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
   intervals = neg_ints[2]
   right = neg_ints[3]
 
-  interval_bounds = []
+  interval_bounds = fmpq[]
   
   if !isempty(left)
     push!(interval_bounds, max(-bound, left[1]))
@@ -229,7 +229,7 @@ function _find_points(coefficients::Vector, bound::Union{Integer, fmpz}, N = 2^1
    end
 
   for i in (1:2:length(interval_bounds))
-    append!(res, _find_points_in_interval(f, coefficients, primes, [H, H2_adic_even, H2_adic_odd], two_adic_info, B, interval_bounds[i], interval_bounds[i + 1], reverse_polynomial, bound,  N))
+    _find_points_in_interval!(res, f, coefficients, primes, (H, H2_adic_even, H2_adic_odd), H_temp, two_adic_info, B, interval_bounds[i], interval_bounds[i + 1], reverse_polynomial, bound, N, candidates, C, I)
   end
   
   #return _find_points_in_interval(coefficients, primes, [H, H2_adic_even, H2_adic_odd], two_adic_info, B, -bound, bound, bound,  N)
@@ -303,11 +303,8 @@ end
 
 Hecke.squarefree_part(x::Int) = Int(squarefree_part(fmpz(x)))
 
-function _find_points_in_interval(f, coefficients::Vector, primes, H_triple, two_adic_info, B, left_bound, right_bound, reverse_polynomial::Bool, bound, N)
+function _find_points_in_interval!(res, f, coefficients::Vector, primes, H_triple, H_temp, two_adic_info, B, left_bound, right_bound, reverse_polynomial::Bool, bound, N, candidates, C, I)
 
-  res = Tuple{fmpq, fmpq, fmpq}[]
-  shifter = ones(Bool, N)
-  shif = view(shifter, 1:N)
   for b in B
     case = two_adic_info[Int(mod(b, 16))+1]
 
@@ -318,10 +315,9 @@ function _find_points_in_interval(f, coefficients::Vector, primes, H_triple, two
     else
       H = H_triple[case]
     end
-    
+
     start_interval = max(ceil(fmpz, b*left_bound), -bound)
     end_interval = min(floor(fmpz, b*right_bound), bound)
-    
     
     # If we only consider odd or even numerators
     if case > 1
@@ -341,9 +337,13 @@ function _find_points_in_interval(f, coefficients::Vector, primes, H_triple, two
     
     rest = rem(numerator_range, N)
     
-    candidates = Vector{Bool}[ones(Bool, N) for i in 1:H_parts]
-    ce = Bool[x <= rest for x = 1:N]
-    push!(candidates, copy(ce))
+    #candidates = Vector{Bool}[ones(Bool, N) for i in 1:H_parts]
+    #ce = Bool[x <= rest for x = 1:N]
+    #push!(candidates, copy(ce))
+    for i in 1:H_parts
+      fill!(candidates[i], true)
+    end
+    candidates[H_parts + 1] = BitVector(x <= rest for x in 1:N)
     
     for i in 1:length(primes)
       p = @inbounds primes[i]
@@ -356,7 +356,7 @@ function _find_points_in_interval(f, coefficients::Vector, primes, H_triple, two
       
       k = mod(b, p)
       if case == 1
-        offset = @inbounds Int(mod(start_interval, p))
+        offset = Int(mod(start_interval, p))
       elseif case == 2
         temp =  Int(mod(start_interval, p))
         if iseven(temp)
@@ -375,19 +375,33 @@ function _find_points_in_interval(f, coefficients::Vector, primes, H_triple, two
       k = mod(b, p)
 
       #Need to shift by 1 as H[i][k] corresponds to k-1 mod p
+      #p_sieve = @inbounds H[i][k + 1]
       p_sieve = @inbounds H[i][k + 1]
+      p_sieve_temp = @inbounds H_temp[i]
+      #@assert p_sieve == _p_sieve
       
-      resize!(shifter, length(p_sieve))
-      fill!(shifter, true)
+      #resize!(shifter, length(p_sieve))
+      #fill!(shifter, true)
+
+      #@assert candidates == _candidates[1:H_parts + 1]
 
       for j in 1:(H_parts + 1)
+        #@assert p_sieve == _p_sieve
         #Storing the shifted p_sieve into shifter is apparently faster
 
-        circshift!(shifter, p_sieve, (j-1)*shift - offset)
-        #Do a broadcasted & on the candidate list with the shifted p_sieve
-        # shif is view(shifter, 1:N) aka shifter[1:N]
-        @inbounds candidates[j] .&= shif
+        # circshift!(shifter, p_sieve, (j-1)*shift - offset)
+        # #Do a broadcasted & on the candidate list with the shifted p_sieve
+        # # shif is view(shifter, 1:N) aka shifter[1:N]
+        # @inbounds candidates[j] .&= shif
+
+        c = candidates[j]
+        # circshift!(x, y,...) is allocating for x === y
+        circshift!(p_sieve_temp, p_sieve, (j - 1)*shift - offset)
+        # p_sieve_temp is slightly to long, so & only up to the length of c
+        _and_with_first_chunks!(c, p_sieve_temp)
+        #@assert candidates[j] == c
       end
+      #@assert p_sieve == _p_sieve
     end
 
     _b = fmpz(b)
@@ -395,13 +409,14 @@ function _find_points_in_interval(f, coefficients::Vector, primes, H_triple, two
     #Consider all integers
     if case == 1
       #Print potential rational points
-      for i in 1:length(candidates)
-      #if candidates[i]!= falses(N)
-        S = findall(candidates[i])
-        if length(S) > 0
+      for i in 1:(H_parts + 1)
+        ci = candidates[i]
+        cnt = count(ci)
+        if cnt > 0
+          I = _findall!(I, ci, cnt)
           _a = (i - 1) * N + start_interval - 1
-          for s in S
-            a = _a + s
+          for j in 1:cnt
+            a = _a + I[j]
             if gcd(a, b) == 1
               if reverse_polynomial 
                 if a != 0 
@@ -419,14 +434,15 @@ function _find_points_in_interval(f, coefficients::Vector, primes, H_triple, two
       end
     #Consider only even integers or only odd integers
     else
-      for i in 1:length(candidates)
+      for i in 1:(H_parts + 1)
         #if candidates[i]!= falses(N)
-        S = findall(candidates[i])
-        if length(S) > 0
-          #Didn't test this case yet
+        ci = candidates[i]
+        cnt = count(ci)
+        if cnt > 0
+          I = _findall!(I, ci, cnt)
           _a = (i - 1) * 2 * N + start_interval
-          for s in S
-            a = _a + 2*(s - 1)
+          for j in 1:cnt
+            a = _a + 2*(I[j] - 1)
             if gcd(a, b) == 1
               if reverse_polynomial 
                 if a != 0 
@@ -504,6 +520,94 @@ function prime_check_arrays(coeff::Vector{<: IntegerUnion}, p::Int, N)
   end
 
   return p_part, p_part_odd, p_part_even
+end
+
+function prime_check_arrays_2(coeff::Vector{<: IntegerUnion}, p::Int, N, C)
+
+  F = GF(p, cached = false)
+  # a contains n+1 elemts : a0, ...., an
+  n = length(coeff) - 1
+
+  a = map(F, coeff)
+
+  p_part = Vector{BitVector}(undef, p)
+  p_part_odd = Vector{BitVector}(undef, p)
+  p_part_even = Vector{BitVector}(undef, p)
+  
+  temp = BitVector(undef, p)
+
+  if p < N
+    p_chunks = div(N, p)
+  else
+    p_chunks = 0
+  end
+
+  az = Vector{elem_type(F)}(undef, n + 1)
+
+  collF = collect(F)
+  
+  temp = BitVector(undef, p)
+  temp2 = BitVector(undef, p)
+  temp3 = BitVector(undef, p)
+  temp4 = Vector{Bool}(undef, p)
+
+  # Compute the indices of the squares once and for all
+  squares = zeros(Bool, p)
+  for t in 0:p-1
+    squares[powermod(t, 2, p) + 1] = true
+  end
+
+  for t in (0:p - 1)
+    z = collF[t + 1]
+    zpowers = powers(z, n)
+    #a[i+1] correponds to a_i above
+    for i in 0:n
+      az[i + 1] = a[i + 1] * zpowers[n - i + 1]
+    end
+        
+    for i in 1:p
+      temp4[i] = squares[_sum_up(az, collF[i], n).data + 1]
+    end
+
+    fill_bitarray_from_itr_cyle!(temp, temp4, length(temp), C)
+    #chunk = BitVector(squares[_sum_up(az, x, n).data] for x in collF)
+    temp2 = BitVector(Iterators.flatten(((temp4[i] for i in 2:2:p), (temp4[i] for i in 1:2:p))))
+    temp3 = BitVector(Iterators.flatten(((temp4[i] for i in 1:2:p), (temp4[i] for i in 2:2:p))))
+
+    #chunk_odd = BitVector(Iterators.flatt((chunk[i] for i in 2:2:p, chunk[i] for i in 1:2:p)))
+    #chunk_even = BitVector(Iterators.flatt((chunk[i] for i in 1:2:p, chunk[i] for i in 2:2:p)))
+    #chunk_odd = vcat(chunk[2:2:p], chunk[1:2:p])    
+    #chunk_even = vcat(chunk[1:2:p], chunk[2:2:p])
+
+    #Pad the BitArray to have chunks that are at least big enough to do a broadcasted & with
+    if p < N
+      # I need p_chunks + 1
+      chunk = _repeated_bitvector(temp, p_chunks + 1)
+      chunk_odd = _repeated_bitvector(temp2, p_chunks + 1)
+      chunk_even = _repeated_bitvector(temp3, p_chunks + 1)
+    else
+      chunk = copy(temp)
+      chunk_odd = copy(temp2)
+      chunk_even = copy(temp3)
+    end
+    p_part[t+1] = chunk
+    p_part_odd[t+1] = chunk_odd
+    p_part_even[t+1] = chunk_even
+  end
+
+  return p_part, p_part_odd, p_part_even
+end
+
+@inline function _sum_up(az, x, n)
+  #ss = sum([az[i + 1]*x^i for i in (0:n)])
+  s = zero(parent(x))
+  y = one(parent(x))
+  @inbounds for i in 0:n
+    s += az[i + 1] * y
+    y *= x
+  end
+  #@assert s == ss
+  return s
 end
 
 #Equation y^2 = an*x^n + a_{n-1}*x^(n-1)*z + ... + a1*x*z^(n - 1) + a0*z^n
@@ -639,7 +743,7 @@ mutable struct NegativityCertificate
     r = sort!(map(real, filter!(isreal, rr)))
 
     # Let's first consider what happens between two roots
-    root_intervals = []
+    root_intervals = Tuple{fmpq, fmpq}[]
     for i in 1:length(r)
       (a, b) = _get_interval(r[i])
       if i < length(r)
@@ -720,6 +824,12 @@ function _get_interval(x::arb)
   b = fmpz()
   e = fmpz()
 
+  if isinteger(x)
+    fl, y = unique_integer(x)
+    @assert fl
+    return y//1, y//1
+  end
+
   ccall((:arb_get_interval_fmpz_2exp, libarb), Nothing, (Ref{fmpz}, Ref{fmpz}, Ref{fmpz}, Ref{arb}), a, b, e, x)
   ee = Int(e)
   @assert ee <= 0
@@ -730,4 +840,86 @@ end
 # Return a collection I = [(a1, a2), ..., ]
 # such that whenever x in (a1, a2) for some element of I, then f(x) < 0
 
+################################################################################
+#
+#  BitVector utils
+#
+################################################################################
 
+# from julia/base/bitarray.jl
+# non-allocating findall function
+function _findall!(I, B, nnzB)
+    nnzB == 0 && return I
+    nnzB == length(B) && (Base.allindices!(I, B); return I)
+    Bc = B.chunks
+    Bs = size(B)
+    Bi = i1 = i = 1
+    irest = ntuple(one, ndims(B) - 1)
+    c = Bc[1]
+    @inbounds while true
+        while c == 0
+            Bi == length(Bc) && return I
+            i1 += 64
+            Bi += 1
+            c = Bc[Bi]
+        end
+
+        tz = trailing_zeros(c)
+        c = Base._blsr(c)
+
+        i1, irest = Base._overflowind(i1 + tz, irest, Bs)
+        I[i] = Base._toind(i1, irest)
+        i += 1
+        i1 -= tz
+    end
+end
+
+function _and_with_first_chunks!(c, cc)
+  for i in 1:length(c.chunks)
+    @inbounds c.chunks[i] &= cc.chunks[i]
+  end
+end
+
+
+# Given B a BitVector, repeat itr_orig (iterator yielding Bool) to fill up
+# the first n values of B
+function fill_bitarray_from_itr_cyle!(B::BitArray, itr_orig, n = length(B), C = Vector{Bool}(undef, Base.bitcache_size)
+)
+    Bc = B.chunks
+    ind = 1
+    cind = 1
+    ind2 = 1
+    itr = Iterators.cycle(itr_orig)
+    y = iterate(itr)
+    while y !== nothing && ind2 <= n
+        ind2 += 1
+        x, st = y
+        @inbounds C[ind] = x
+        ind += 1
+        if ind > Base.bitcache_size
+            Base.dumpbitcache(Bc, cind, C)
+            cind += Base.bitcache_chunks
+            ind = 1
+        end
+        y = iterate(itr, st)
+    end
+    if ind > 1
+        @inbounds C[ind:Base.bitcache_size] .= false
+        Base.dumpbitcache(Bc, cind, C)
+    end
+    return B
+end
+
+function _repeated_bitvector(A::BitVector, k)
+    nargs = k
+    nrows = length(A) * k
+    n = length(A)
+    B = similar(A, nrows)
+    pos = 1
+    for k=1:nargs
+        p1 = pos+n-1
+        B[pos:p1, :] = A
+        pos = p1+1
+    end
+    return B
+end
--- test/EllCrv/RationalPointSearch.jl
+++ test/EllCrv/RationalPointSearch.jl
@@ -1,18 +1,52 @@
 @testset "Rational point search" begin
-  # Some tests for the negativity
-  Qx, x = QQ["x"]
-  f = x^3 - 2*x^2 + 1 # rational root
-  a, b, c = Hecke.negative_intervals(f)
-  @test length(a) == 1
-  @test length(b) == 1
-  @test length(c) == 0
-  @test b[1][1] <= 3//2 <= b[1][2]
+  coefficients = fmpz[10, -7, 0, 1]
+  bounds = [25, 250, 2500, 25000, 250000]
+  cts = [29, 49, 79, 105, 125]
+  for (b, c) in zip(bounds, cts)
+    pts = @inferred Hecke.find_points(coefficients, b)
+    @test length(pts) == c
+  end
 
-  f = (x^3 - 2*x^2 + 1)*(x + 3)
-  a, b, c = Hecke.negative_intervals(f)
-  @test length(a) == 0
-  @test length(b) == 2
-  @test length(c) == 0
-  @test b[1][1] <= -2 <= b[1][2]
-  @test b[2][1] <= 3//2 <= b[2][2]
+  coefficients = fmpz[2, 1, -3, 1]
+  bounds = [10, 100, 1000, 10000]
+  cts = [4, 4, 4, 4]
+  for (b, c) in zip(bounds, cts)
+    pts = @inferred Hecke.find_points(coefficients, b)
+    @test length(pts) == c
+  end
+
+  coefficients = fmpz[10, -1, 0, 1]
+  bounds = [10, 100, 1000, 10000]
+  cts = [9, 9, 19, 23]
+  for (b, c) in zip(bounds, cts)
+    pts = @inferred Hecke.find_points(coefficients, b)
+    @test length(pts) == c
+  end
+
+  @testset "Negativity" begin
+    # Some tests for the negativity
+    Qx, x = QQ["x"]
+    f = x^3 - 2*x^2 + 1 # rational root
+    a, b, c = Hecke.negative_intervals(f)
+    @test length(a) == 1
+    @test length(b) == 1
+    @test length(c) == 0
+    @test b[1][1] <= 3//2 <= b[1][2]
+
+    f = (x^3 - 2*x^2 + 1)*(x + 3)
+    a, b, c = Hecke.negative_intervals(f)
+    @test length(a) == 0
+    @test length(b) == 2
+    @test length(c) == 0
+    @test b[1][1] <= -2 <= b[1][2]
+    @test b[2][1] <= 3//2 <= b[2][2]
+
+    f = x^3 - 3*x^2 + x + 2
+    a, b, c = Hecke.negative_intervals(f)
+    @test length(a) == 1
+    @test length(b) == 1
+    @test length(c) == 0
+    @test -1 < a[1]
+    @test b[1][1] < 1.8 < b[1][2]
+  end
 end
