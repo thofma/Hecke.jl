f8440a622a308de786e028b1512e0cbc5fbc8013
Antony <antonydellavecchia@gmail.com>
2022-07-15 13:17:48 +0200

    another attempt for julia 1.6 test

--- test/NumField/NfAbs/ComplexEmbeddingsNS.jl
+++ test/NumField/NfAbs/ComplexEmbeddingsNS.jl
@@ -75,13 +75,12 @@
 
   K, (a, b) = NumberField([x^2 + 5, x^2 + 7])
   CC = AcbField(64)
-  
-  @test Set(complex_embeddings(K)) == Set([
-    Hecke.complex_embedding(K, [CC(0, 2.24), CC(0, 2.65)]),
-    Hecke.complex_embedding(K, [CC(0, -2.24), CC(0, 2.65)]),
-    Hecke.complex_embedding(K, [CC(0, 2.24), CC(0, -2.65)]),
-    Hecke.complex_embedding(K, [CC(0, -2.24), CC(0, -2.65)])
-  ])
+  r1 = Hecke.complex_embedding(K, [CC(0, 2.24), CC(0, 2.65)])
+  r2 = Hecke.complex_embedding(K, [CC(0, -2.24), CC(0, 2.65)])
+  r3 = Hecke.complex_embedding(K, [CC(0, 2.24), CC(0, -2.65)])
+  r4  = Hecke.complex_embedding(K, [CC(0, -2.24), CC(0, -2.65)])
+
+  @test Set(complex_embeddings(K)) == Set([r1, r2, r3, r4])
   @test_throws ErrorException Hecke.complex_embedding(K, [0.0, 0.0])
 
 end
