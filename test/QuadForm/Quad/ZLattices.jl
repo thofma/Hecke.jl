1dd16f82e1921f9ab459a53cc9199b580c446ab9
StevellM <muller@math.uni-sb.de>
2022-07-16 10:13:05 +0200

    fix mass ZLat

--- test/QuadForm/Quad/ZLattices.jl
+++ test/QuadForm/Quad/ZLattices.jl
@@ -453,4 +453,14 @@ end
   @test [B[4,i] for i in 1:ncols(B)] in L
   @test_throws AssertionError x4 in L
   @test v in l
+
+  # Mass of lattices
+  E8 = root_lattice(:E, 8)
+  @test mass(E8) == 1//automorphism_group_order(E8)
+  
+  F23a = Zlattice(gram = matrix(ZZ,2,2,[2 1; 1 12]))
+  F23b = Zlattice(gram = matrix(ZZ,2,2,[4 1; 1 6]))
+  
+  @test mass(F23a) == mass(F23b) == 3//4
+  
 end
