b463cf57f1713adc46a7b74c1a35e9c68afb9555
Yvonne Neuy <YvonneNeuy@gmail.com>
2022-08-31 15:04:50 +0200

    brochure

--- src/QuadForm/Herm/Intersection.jl
+++ src/QuadForm/Herm/Intersection.jl
@@ -1,12 +1,6 @@
 #Hermitian-lattice database rufen
- 
-hld = Hecke.hermitian_lattice_database()
-L = lattice(hld,75)#78 , 219, 375, 75, 56
-E = base_field(L)
-K = fixed_field(L)
-pm = L.pmat 
-A1,A2,A3 = pm.coeffs
 
+#Could be added in src Numfield 
 function (E::Hecke.NfRel)(x::Vector{fmpq})
 
   abs_basis = absolute_basis(E)
@@ -18,6 +12,7 @@ function (E::Hecke.NfRel)(x::Vector{fmpq})
   return sum(abs_basis[i]*x[i] for i = 1:length(abs_basis))
 end
 
+#_matQQ_to_vec_E
 function (E::Hecke.NfRel)(x::fmpq_mat)
 
   abs_basis = absolute_basis(E)
@@ -43,11 +38,34 @@ function _restrict_scalars_with_map(L)
   return ZLat(Vabs, Mabs), f
 end
 
+function _restrict_scalars_with_respect_to_map(L,f)
+
+  if codomain(f) !== ambient_space(L)
+    error("codomain of f must be equal to the ambient space of L")
+  end
+
+  Babs = absolute_basis(L)
+  Mabs = zero_matrix(FlintQQ, length(Babs), rank(Vabs))
+  for i in 1:length(Babs)
+    v = f\(Babs[i])
+    for j in 1:length(v)
+      Mabs[i, j] = v[j]
+    end
+  end
+  return ZLat(Vabs, Mabs)
+
+end
+
+function _divide(x,y)
+  
+end
+
+
+#rows = rank , cols = degree --> L -> Lherm : degree(L) = degree(Lherm)*abs_deg(E), rank L = rank Lh *absolute deg(E)
 #OE = maximal_order(base_field(L))
-function test(L)
+function reconstruction_herm(L)
   E = base_field(L)
   OE = maximal_order(E)
-  b = gen(E)
   n = absolute_degree(E)
 
   K = base_field(E)
@@ -56,34 +74,65 @@ function test(L)
   LL, f = _restrict_scalars_with_map(L)
   M = basis_matrix(LL)
 
-  a = zeros(E, rank(L), rank(L))
-  
+  a = zeros(E, degree(L), degree(L))
+  coeffs = (Hecke.fractional_ideal_type(OE))[]
   #constructing v_i = \sum A_i e_i -> A_i = B_{i1} y_1 + B_{i2} y_2
   for i = 1:rank(L)
     
     v = [M[1+(i-1)*n, j]  for j = 1:nrows(M)] #Convert type of the rows
-    w = f(v) #
-    lker = left_kernel(matrix(w))[2]
-    rker = right_kernel(lker)[2]
+    a[i,:] = f(v) 
+    #handle cases where first row is 0 @req [] @asset index != nothing
     
-    for j = 1:length(rker)
-      a[i, j] = rker[j]
-    end
- 
-    index = 0
     #Find first element in a which is not zero
-    for k = 1:rank(L)
-      if a[i,k] != E(0)
-        index = k
-        break 
-      end
-    end
-
+    index = findfirst(k -> !iszero(a[i,k]), 1:degree(L))
+    
+    #Ai = B1 y1 + B2 y2 
     y = zeros(E, 2)
-    A = M[1+(i-1)*n:i*n, 1 + (index-1)*n:index*n] 
-    A_E = matrix(E(A).//a[index])
+    A = M[(1+(i-1)*n):i*n, (1+(index-1)*n): index*n] 
+    
+    A_E = matrix(absolute_coordinates.(E(A) .// a[i,index]))
+    
+    y = [E(A_E[1, :])[1], E(A_E[degree(K)+1, :])[1]]
+    A_E[1:degree(K),:] = matrix(absolute_coordinates.(E(A_E[1:degree(K),:]) .// y[1]))
+    A_E[degree(K)+1:n,:] = matrix(absolute_coordinates.(E(A_E[degree(K)+1:n,:]) .// y[2]))  
+
+    fk1 = FakeFmpqMat(A_E[1:degree(K),1:degree(K)])
+    frac_ideal1 = fractional_ideal(OK,fk1)
+    B1 = fractional_ideal(OE, frac_ideal1)
+    
+    fk2 = FakeFmpqMat(A_E[degree(K)+1:n,1:degree(K)])
+    frac_ideal2 = fractional_ideal(OK,fk2)
+    B2 = fractional_ideal(OE, frac_ideal2)
+    #print(typeof(B2))
+    Ai = 0*OE
+    Ai = B1*y[1] + B2*y[2]
+    push!(coeffs,Ai)
+    #print(typeof(Ai))
+  end 
+  
+  pmatrix = Hecke.PMat{Hecke.elem_type(E), Hecke.fractional_ideal_type(OE)}(matrix(a),coeffs)
+  Lnew = lattice(ambient_space(L),pmatrix) #codomain of f
+  return Lnew
+
+end
 
 
+hld = Hecke.hermitian_lattice_database()
+L = lattice(hld,75)#78 , 219, 375, 75, 56
+E = base_field(L)
+K = fixed_field(L)
+pm = L.pmat 
+A1,A2,A3 = pm.coeffs
+LL = _restrict_scalars_with_map(L)
+pmatrix = reconstruction_herm(L)
+#=
+
+#quad and herm reconstruction 
+     #check trivial cases
+    #start with 0*OE and add Bi
+    #PseudoMatrix to construct the lattice at the end coeff -> a , ideals A_i 
+
+    
     lker2 = left_kernel(A_E[[1,degree(K)+1], :])[2]
     rker2 = right_kernel(lker2)[2]
 
@@ -92,13 +141,17 @@ function test(L)
     end
     #Irgendwas mit dem y stimmt nicht ganz
     return A_E, y
-       
-  end 
-  
-  #constructing A_i = \sum B_i v_i
-  #return a, M
-
-end
 
 
-A,y = test(L)
+    Todo: Rank not full
+    no zero block
+    if first line are zero
+    quad lattice
+    test
+    intersection
+    out hermitian lattice
+    input zlat + type of f
+    adjust function
+    matrix / nfelem
+    checks: eine hälfte zero bei den Bi , blöcke vielfache voneinander 
+    =#
