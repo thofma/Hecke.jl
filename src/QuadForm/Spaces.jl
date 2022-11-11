9097fb26bda7b6e2dea8244d571ceaa71ba9858c
simonbrandhorst <51749255+simonbrandhorst@users.noreply.github.com>
2022-11-11 12:26:39 +0100

    Update src/QuadForm/Spaces.jl

--- src/QuadForm/Spaces.jl
+++ src/QuadForm/Spaces.jl
@@ -598,6 +598,7 @@ end
 Given a space $(V, \Phi)$ and a subfield `K` of the base algebra `E` of `V`, return the
 quadratic space `W` obtained by restricting the scalars of $(V, \alpha\Phi)$ to `K`,
 together with the map `f` for extending the scalars back.
+The form on the restriction is given by ``Tr \circ \Phi`` where ``Tr: E \to K`` is the trace form.  
 The rescaling factor $\alpha$ is set to 1 by default.
 
 Note that for now one can only restrict scalars to $\mathbb Q$.
