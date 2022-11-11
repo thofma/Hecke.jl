839628e5df845181089e42e23b0da47f1b9edbab
simonbrandhorst <51749255+simonbrandhorst@users.noreply.github.com>
2022-11-11 12:26:32 +0100

    Update src/QuadForm/Spaces.jl

--- src/QuadForm/Spaces.jl
+++ src/QuadForm/Spaces.jl
@@ -598,7 +598,7 @@ end
 Given a space $(V, \Phi)$ and a subfield `K` of the base algebra `E` of `V`, return the
 quadratic space `W` obtained by restricting the scalars of $(V, \alpha\Phi)$ to `K`,
 together with the map `f` for extending the scalars back.
-The rescaling factor $\aplha$ is set to 1 by default.
+The rescaling factor $\alpha$ is set to 1 by default.
 
 Note that for now one can only restrict scalars to $\mathbb Q$.
 """
