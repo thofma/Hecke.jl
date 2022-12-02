47be80d763970b5985d3b4b322e28fce4f961bdb
Simon Brandhorst <sbrandhorst@web.de>
2022-12-02 16:27:06 +0100

    ...

--- src/QuadForm/Quad/ZGenus.jl
+++ src/QuadForm/Quad/ZGenus.jl
@@ -2640,7 +2640,7 @@ end
 Return a (primitive) embedding of the integral lattice `S` into some lattice in the genus of `G`.
 
 ```jldoctest
-julia> G = genera((8,0), 1, even=true)[1]
+julia> G = genera((8,0), 1, even=true)[1];
 
 julia> L, S, i = embed(root_lattice(:A,5), G);
 
