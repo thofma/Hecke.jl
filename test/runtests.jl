c9a9c051c3498b38b669e2fadec5ad81c169b085
Tommy Hofmann <thofma@gmail.com>
2022-12-11 22:56:34 +0100

    Swap to 1.8 for long tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -292,11 +292,11 @@ else
   end
 
   # Run the doctests
-  if v"1.6.0" <= VERSION < v"1.7.0"
+  if v"1.8.0" <= VERSION < v"1.9.0"
     @info "Running doctests (Julia version is 1.6)"
     DocMeta.setdocmeta!(Hecke, :DocTestSetup, :(using Hecke); recursive = true)
     doctest(Hecke)
   else
-    @info "Not running doctests (Julia version must be 1.6)"
+    @info "Not running doctests (Julia version must be 1.8)"
   end
 end
