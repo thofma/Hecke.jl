7e0173a007b8e8aa80fc09b28759cbd5f0dfbcb0
Simon Brandhorst <sbrandhorst@web.de>
2022-12-14 17:25:19 +0100

    fix is_isotropic_with_vector

--- src/QuadForm/Spaces.jl
+++ src/QuadForm/Spaces.jl
@@ -83,6 +83,7 @@ function preimage(f::AbsSpaceMor, L::ZLat)
     ok, B = can_solve_with_solution(f.matrix, basis_matrix(L2), side=:left)
     @assert ok
   end
+  @show B
   return lattice(V, B)
 end
 
@@ -598,7 +599,7 @@ end
 Given a space $(V, \Phi)$ and a subfield `K` of the base algebra `E` of `V`, return the
 quadratic space `W` obtained by restricting the scalars of $(V, \alpha\Phi)$ to `K`,
 together with the map `f` for extending the scalars back.
-The form on the restriction is given by ``Tr \circ \Phi`` where ``Tr: E \to K`` is the trace form.  
+The form on the restriction is given by ``Tr \circ \Phi`` where ``Tr: E \to K`` is the trace form.
 The rescaling factor $\alpha$ is set to 1 by default.
 
 Note that for now one can only restrict scalars to $\mathbb Q$.
