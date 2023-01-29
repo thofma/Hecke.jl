6577b065336f8f16145b4c32c99ae4217325f983
Claus Fieker <fieker@mathematik.uni-kl.de>
2023-01-29 16:00:10 +0100

    Fix gcd (#953)

--- test/NumField/NfAbs/MPolyGcd.jl
+++ test/NumField/NfAbs/MPolyGcd.jl
@@ -4,4 +4,8 @@
   Kx, (x,) = PolynomialRing(K, ["x"])
   g = gcd(-715//2*a^2*x^3 - 715//2*a^2*x^2 - 365525875//2*a^2*x - 365525875//2*a^2, (-715//2*a^2 + 511225)*x^2 - 365525875//2*a^2 + 261351000625)
   @test g == x^2 + 511225
+
+  K, a = number_field(2*A^2+1)
+  Kx, (x,y) = PolynomialRing(K, 2)
+  @test isone(gcd(x, y))
 end
