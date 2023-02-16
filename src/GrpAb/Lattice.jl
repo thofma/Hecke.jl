8492cfed9a5f5397677311cf90d78b7c805f734a
Tommy Hofmann <thofma@gmail.com>
2023-02-16 08:12:22 +0100

    Tweak for lattice (#968)

--- src/GrpAb/Lattice.jl
+++ src/GrpAb/Lattice.jl
@@ -386,6 +386,7 @@ end
 # Add an object to a lattice of groups.
 function Base.append!(L::RelLattice{T, D}, A::T) where {T, D}
   update!(L)
+
   L.weak_vertices[A] = nothing
   obid = objectid(A)
   L.weak_vertices_rev[obid] = WeakRef(A)
@@ -399,6 +400,8 @@ function Base.append!(L::RelLattice{T, D}, A::T) where {T, D}
     finalizer(x -> finalizer_lattice(L, x), A)
     A.isfinalized = true
   end
+
+  return nothing
 end
 
 # Add a map to a lattice of groups
@@ -427,12 +430,15 @@ function Base.append!(L::RelLattice{T, D}, dom::T, co::T, f::D) where {T, D}
   if L.graph.degrees[objectid(co)] > 1
     L.block_gc[co] = nothing
   end
+
+  return nothing
 end
 
 # Delete the group with objectid u from a lattice of groups.
 function delete_from_lattice!(L::RelLattice, u::UInt)
   Base.delete!(L.graph, u)
   Base.delete!(L.weak_vertices_rev, u)
+  return nothing
 end
 
 function delete_from_lattice!(L::RelLattice, us::Vector{UInt})
@@ -448,6 +454,7 @@ function update!(L::RelLattice)
   #  u = pop!(L.to_delete)
   #  delete_from_lattice!(L, u)
   #end
+
   delete_from_lattice!(L, L.to_delete)
   #L.to_delete = UInt[]
 
@@ -456,6 +463,12 @@ function update!(L::RelLattice)
     # TODO: Why does it crash without the following?
     if !haskey(L.weak_vertices_rev, k)
       delete!(L.graph.new_low_degrees, k)
+      continue
+    end
+    if L.weak_vertices_rev[k].value === nothing
+      delete!(L.graph.new_low_degrees, k)
+      Base.delete!(L.graph, k)
+      Base.delete!(L.weak_vertices_rev, k)
       continue
     end
     @assert L.weak_vertices_rev[k].value != nothing
