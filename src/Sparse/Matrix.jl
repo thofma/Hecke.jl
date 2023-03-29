a50b6c3799ed06578858f242b97f00986e9301b4
Claus Fieker <fieker@mathematik.uni-kl.de>
2023-03-29 14:57:43 +0200

    undo errors

--- src/Sparse/Matrix.jl
+++ src/Sparse/Matrix.jl
@@ -650,7 +650,7 @@ end
 function +(A::SMat{T}, B::SMat{T}) where T
   nrows(A) != nrows(B) && error("Matrices must have same number of rows")
   ncols(A) != ncols(B) && error("Matrices must have same number of columns")
-  C = sparse_matrix(base_ring(A))
+  C = sparse_matrix(base_ring(A), 0, ncols(A))
   m = min(nrows(A), nrows(B))
   for i=1:m
     push!(C, A[i] + B[i])
@@ -667,7 +667,7 @@ end
 function -(A::SMat{T}, B::SMat{T}) where T
   nrows(A) != nrows(B) && error("Matrices must have same number of rows")
   ncols(A) != ncols(B) && error("Matrices must have same number of columns")
-  C = sparse_matrix(base_ring(A))
+  C = sparse_matrix(base_ring(A), 0, ncols(A))
   m = min(nrows(A), nrows(B))
   for i=1:m
     push!(C, A[i]-B[i])
@@ -692,7 +692,7 @@ end
 ################################################################################
 
 function *(b::T, A::SMat{T}) where {T <: RingElem}
-  B = sparse_matrix(base_ring(A))
+  B = sparse_matrix(base_ring(A), 0, ncols(A))
   if iszero(b)
     return B
   end
