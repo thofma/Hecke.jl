d328614a479783087b44e4899d78140ffff60e14
StevellM <muller@math.uni-sb.de>
2023-03-30 15:22:54 +0200

    fix wrong doc

--- src/QuadForm/Lattices.jl
+++ src/QuadForm/Lattices.jl
@@ -877,8 +877,8 @@ Given a prime ideal `p` and a lattice `L`, return a basis matrix of a lattice
 `L`, the completions are taken at the minimum of `p` (which is an ideal in the
 base ring of the order of `p`).
 
-- If `type == :submodule`, the lattice `L` will be a sublattice of `M`.
-- If `type == :supermodule`, the lattice `L` will be a superlattice of `M`.
+- If `type == :submodule`, the lattice `M` will be a sublattice of `L`.
+- If `type == :supermodule`, the lattice `M` will be a superlattice of `L`.
 - If `type == :any`, there may not be any containment relation between `M` and
   `L`.
 """
