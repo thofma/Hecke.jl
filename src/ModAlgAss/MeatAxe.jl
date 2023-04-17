68facd97109725a1e95af8e5b011f19b562cbbb4
simonbrandhorst <brandhorst@math.uni-sb.de>
2023-04-17 08:41:25 +0200

    make closure more restrictive (#1041)

--- src/ModAlgAss/MeatAxe.jl
+++ src/ModAlgAss/MeatAxe.jl
@@ -102,7 +102,7 @@ Given a matrix $C$ representing a subspace of $K^n$ and a list of matrices $G$ r
 the function returns a matrix representing the closure of the subspace under the action, i.e. the smallest
 subspace of $K^n$ invariant under the endomorphisms.
 """
-function closure(C::T, G::Vector{T}) where {T}
+function closure(C::T, G::Vector{T}) where {T <: MatElem}
   if nrows(C) != 1
     rref!(C)
   else
