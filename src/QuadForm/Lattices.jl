7184d6359cd9db0e68a793ee14d238ad060d6473
Tommy Hofmann <thofma@gmail.com>
2023-05-24 08:17:55 +0200

    add doc

--- src/QuadForm/Lattices.jl
+++ src/QuadForm/Lattices.jl
@@ -834,11 +834,11 @@ function is_modular(L::AbstractLat)
 end
 
 @doc raw"""
-    is_modular(L::AbstractLat, p::NfOrdIdl) -> Bool, Int
+    is_modular(L::AbstractLat, p) -> Bool, Int
 
-Return whether the completion $L_{p}$ of the lattice `L` at the prime ideal `p`
-is modular. If it is the case the second returned value is an integer `v` such
-that $L_{p}$ is $p^v$-modular.
+Return whether the completion $L_{p}$ of the lattice `L` at the prime ideal or
+integer `p` is modular. If it is the case the second returned value is an
+integer `v` such that $L_{p}$ is $p^v$-modular.
 """
 is_modular(::AbstractLat, p)
 
