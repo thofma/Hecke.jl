187a46f0a8785a846d64ef0a7a1941b69df61ebb
simonbrandhorst <brandhorst@math.uni-sb.de>
2023-05-29 21:20:10 +0200

    Update src/QuadForm/Torsion.jl

--- src/QuadForm/Torsion.jl
+++ src/QuadForm/Torsion.jl
@@ -2251,7 +2251,7 @@ end
 Return the submodules of `T` stable under the endomorphisms in `act` as
 an iterator. Possible keyword arguments to restrict the submodules:
 - `quotype::Vector{Int}`: only submodules whose quotient are isomorphic as an
-  abelian to `abelian_group(quotype)`.
+  abelian group to `abelian_group(quotype)`.
 """
 function stable_submodules(T::TorQuadModule, act::Vector{TorQuadModuleMor}; kw...)
   A = abelian_group(T)
