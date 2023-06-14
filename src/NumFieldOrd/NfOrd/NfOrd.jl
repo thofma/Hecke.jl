fc61622593ab1314d7cc6f7c658af3ec7d15c503
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-06-14 15:57:45 +0200

    Export `ramified_primes`

--- src/NumFieldOrd/NfOrd/NfOrd.jl
+++ src/NumFieldOrd/NfOrd/NfOrd.jl
@@ -36,7 +36,8 @@ export ==, +, basis, basis_matrix, basis_mat_inv, contains_equation_order,
        discriminant, degree, gen_index, EquationOrder, index,
        is_equation_order, is_index_divisor, lll, lll_basis, nf,
        minkowski_matrix, norm_change_const, Order, parent, different,
-       signature, trace_matrix, codifferent, reduced_discriminant
+       signature, trace_matrix, codifferent, ramified_primes,
+       reduced_discriminant
 
 ################################################################################
 #
