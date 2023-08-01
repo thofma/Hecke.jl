29f63ffabab17d7b7f06e9e104a4859b34993c4c
Tommy Hofmann <thofma@gmail.com>
2023-08-01 20:20:18 +0200

    Fix field_extension

--- src/Map/FqPolyRing.jl
+++ src/Map/FqPolyRing.jl
@@ -5,7 +5,7 @@ function field_extension(h::Union{ fqPolyRepPolyRingElem, FqPolyRepPolyRingElem 
   return codomain(m), m
 end
 
-field_extension(h::FqPolyRingElem) = _residue_field(h)
+field_extension(h::FqPolyRingElem) = Nemo._residue_field(h)
 
 mutable struct FqPolyRingToFqMor{S, T, PolyType, MatType} <: Map{S, T, HeckeMap, FqPolyRingToFqMor}
   header::MapHeader{S, T}
