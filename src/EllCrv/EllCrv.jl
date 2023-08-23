bc31a8577f05792b14af8b9f9b0d787cfb9d2163
Matthias Zach <zach@math.uni-hannover.de>
2023-08-23 14:31:51 +0200

    Add docstring.

--- src/EllCrv/EllCrv.jl
+++ src/EllCrv/EllCrv.jl
@@ -239,6 +239,13 @@ end
 # A constructor to create an elliptic curve from a bivariate polynomial. 
 # One can specify how to interpret the polynomial via the second and the 
 # third argument.
+@doc raw"""
+    elliptic_curve(f::MPolyRingElem, x::MPolyRingElem, y::MPolyRingElem) -> EllCrv
+
+Construct an elliptic curve from a bivariate polynomial `f` in long Weierstrass form. 
+The second and third argument specify variables of the `parent` of `f` so that 
+``f = c ⋅ (-y² + x³ - a₁ ⋅ xy + a₂ ⋅ x² - a₃ ⋅ y + a₄ ⋅ x + a₆)``.
+"""
 function elliptic_curve(f::MPolyRingElem, x::MPolyRingElem, y::MPolyRingElem)
   R = parent(f)
   @assert ngens(R) == 2 "polynomial must be bivariate"
