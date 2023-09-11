25b5e3e2b008724f76ac17db07df0e5dda1f2558
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-09-11 15:43:29 +0200

    Remove hack

--- src/AlgAss/Types.jl
+++ src/AlgAss/Types.jl
@@ -346,13 +346,3 @@ mutable struct AlgMatElem{T, S, Mat} <: AbsAlgAssElem{T}
     return z
   end
 end
-
-################################################################################
-#
-#  Polynomial ring hack
-#
-################################################################################
-
-function AbstractAlgebra.polynomial_ring(A::AbsAlgAss, s::Symbol; cached::Bool = true)
-  return invoke(Generic.polynomial_ring, Tuple{AbstractAlgebra.NCRing, Symbol}, A, s; cached = cached)
-end
