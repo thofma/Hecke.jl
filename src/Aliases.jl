2a1bf35c4c4836be528bab841f4bee16e00e0f28
Max Horn <max@quendi.de>
2023-12-20 18:27:21 +0100

    Re-export deprecated bindings from AA and Nemo

--- src/Aliases.jl
+++ src/Aliases.jl
@@ -1,3 +1,9 @@
+import Nemo: is_cyclo_type
+import Nemo: is_embedded
+import Nemo: is_maxreal_type
+import Nemo: ZZModRing  # FIXME: remove if/once Nemo exports this
+include(joinpath(pathof(Nemo), "..", "Aliases.jl"))
+
 # make some Julia names compatible with our naming conventions
 @alias is_hermitian ishermitian
 
