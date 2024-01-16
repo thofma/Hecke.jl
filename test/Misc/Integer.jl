6e918fa4dfe1f0d13e22287991ce6cf317bd731d
Sreevatsa Sasisekar <sasiseka@mathematik.uni-kl.de>
2024-01-16 12:10:02 +0100

    Added Test for Euler Phi Inverse

--- test/Misc/Integer.jl
+++ test/Misc/Integer.jl
@@ -1,10 +1,14 @@
 @testset "Integers" begin
     
+#Line 7
 @test is_commuatative(ZZ)==true
-
+#Lines 26-35
 @test modord(2,3) == 2
-
+#Lines 15-24 
 @test modord(ZZ(2),ZZ(3)) == ZZ(2)
+#Lines 760-762
+@test sort(euler_phi_inv(ZZ(4))) == [ZZ(5), ZZ(8), ZZ(10), ZZ(12)]
+#Lines 172-179
 
 
 end
