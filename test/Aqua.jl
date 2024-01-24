8f86b36c8935bb55b5e04990998389cbfd3330fd
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-01-24 14:10:20 +0100

    Revert "TO BE REVERTED: Disable some Aqua tests"
    This reverts commit 582277252df04850c59b518b94a858185abb4025.

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -3,7 +3,6 @@ using Aqua
 @testset "Aqua.jl" begin
   Aqua.test_all(
     Hecke;
-    persistent_tasks=false, # TODO: revert before merge
     ambiguities=false,      # TODO: fix ambiguities
     piracies=false          # TODO: fix piracy
   )
