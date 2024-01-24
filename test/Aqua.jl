e968a5df452190ec399a98503dc08a0ab22463f7
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-01-24 11:42:29 +0100

    TO BE REVERTED: Disable some Aqua tests

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -3,6 +3,7 @@ using Aqua
 @testset "Aqua.jl" begin
   Aqua.test_all(
     Hecke;
+    persistent_tasks=false, # TODO: revert before merge
     ambiguities=false,      # TODO: fix ambiguities
     piracies=false          # TODO: fix piracy
   )
