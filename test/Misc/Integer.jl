bf4d469229938f45459349b3c2ddd5e39e023d8a
Sreevatsa Sasisekar <sasiseka@mathematik.uni-kl.de>
2024-01-29 13:54:12 +0100

    Implemented Lars's comments

--- test/Misc/Integer.jl
+++ test/Misc/Integer.jl
@@ -1,7 +1,7 @@
 @testset "Integers" begin
 
     
-    @test is_commutative(ZZ) == true
+    @test is_commutative(ZZ)
 
     @test modord(2,3) == 2
 
@@ -9,17 +9,17 @@
 
     @test sort(euler_phi_inv(ZZ(4))) == [ZZ(5), ZZ(8), ZZ(10), ZZ(12)]
 
-    @test is_prime_power(ZZ(8)) == true
+    @test is_prime_power(ZZ(8))
 
     @test euler_phi(ZZ(5)) == ZZ(4)
 
     @test carmichael_lambda(ZZ(8)) == ZZ(2)
    
-    @test sort(evaluate.(Hecke.euler_phi_inv_fac_elem(ZZ(4)))) == ZZ.([5, 8 ,10 ,12])   
+    @test sort(evaluate.(Hecke.euler_phi_inv_fac_elem(ZZ(4)))) == ZZ.([5, 8 ,10 ,12])
     
-    @test carmichael_lambda(ZZ(1*2^3)) == ZZ(2) 
+    @test carmichael_lambda(ZZ(1*2^3)) == ZZ(2)
 
-    @test carmichael_lambda(8) == 2 
+    @test carmichael_lambda(8) == 2
 
 
 
