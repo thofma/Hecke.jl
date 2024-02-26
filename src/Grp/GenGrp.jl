c299ea7b656b649e32b821ec47c28b966b1eab39
Max Horn <max@quendi.de>
2024-02-26 11:00:25 +0100

    Tweak generic_group docstring
    Don't mention implementation details, but do describe the
    return values. Also add a doctest example.

--- src/Grp/GenGrp.jl
+++ src/Grp/GenGrp.jl
@@ -141,9 +141,36 @@ end
 ################################################################################
 @doc raw"""
      generic_group(G, op)
-Computes group of $G$ with operation $op$, implemented with multiplication
-table 'G.mult_table'.
-The elements are just 1..n and i * j = 'G.mult_table'[i, j].
+
+Compute a group on the set $G$ with operation $op$.
+
+Returns a triple `(Gen, GtoGen, GentoG)`, where `Gen` is a group,
+`GtoGen` is a dictionary mapping elements in `G` to elements of `Gen`,
+and `GentoG` is likewise but the other way around.
+
+# Examples
+```jldoctest
+julia> G = [1, -1, im, -im]
+4-element Vector{Complex{Int64}}:
+  1 + 0im
+ -1 + 0im
+  0 + 1im
+  0 - 1im
+
+julia> Gen, GtoGen, GentoG = generic_group(G, *);
+
+julia> Gen
+Generic group of order 4 with multiplication table
+
+julia> one(Gen)
+(1)
+
+julia> GentoG[one(Gen)]
+1 + 0im
+
+julia> GtoGen[-1]
+(2)
+```
 """
 function generic_group(G, op)
   Gen = MultTableGroup(_multiplication_table(G, op))
