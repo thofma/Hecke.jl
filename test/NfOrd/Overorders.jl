03f511a6c7713e153eea364ff0cfe77661c2929a
Tommy Hofmann <thofma@gmail.com>
2024-03-29 09:29:29 +0100

    fix: avoid duplicates in overorder computation
    - Basis matrices of orders must always be in Hermite normal form,
      either because of (improved) index calculations or for faster
      comparisons of orders.
    - Some places violated the implicit HNF rule
    - Add assertions to the code

--- test/NfOrd/Overorders.jl
+++ test/NfOrd/Overorders.jl
@@ -58,4 +58,15 @@
   A = StructureConstantAlgebra(f)
   O = Order(A, basis(A))
   @test length(overorders(O)) == 2535
+
+  let # issue reported by M. Kirschmer
+    P, x = polynomial_ring(QQ)
+    K, a  = number_field(x^6 - x^5 - x^4 + 2*x^3 - x + 1)
+    R = ring_of_integers(K)
+    P = prime_ideals_over(R, 5)
+    OO = Order(K, basis(P[1]*P[2]^2))
+    X = overorders(OO);
+    S = Set([ basis_matrix(x) for x in X ]);
+    @test length(S) == length(X) == 29
+  end
 end
