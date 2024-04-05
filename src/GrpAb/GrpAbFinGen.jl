3583d7398ed96112a7a9928953e80df4b06d2405
Max Horn <max@quendi.de>
2024-04-05 13:52:44 +0200

    Tweak elementary_divisors docstring

--- src/GrpAb/GrpAbFinGen.jl
+++ src/GrpAb/GrpAbFinGen.jl
@@ -1965,8 +1965,8 @@ end
 @doc raw"""
     elementary_divisors(G::FinGenAbGroup) -> Vector{ZZRingElem}
 
-Given $G$, return the elementary divisors of $G$, that is, the unique positive
-integers $e_1,\dotsc,e_k$ with $e_i \mid e_{i + 1}$ and
+Given $G$, return the elementary divisors of $G$, that is, the unique non-negative
+integers $e_1,\dotsc,e_k$ with $e_i \mid e_{i + 1}$ and $e_i\neq 1$ such that
 $G \cong \mathbf{Z}/e_1\mathbf{Z} \times \dotsb \times \mathbf{Z}/e_k\mathbf{Z}$.
 """
 function elementary_divisors(G::FinGenAbGroup)
