919cfe236a6ee75976e5b38552b58e589e80bad8
Simon Brandhorst <brandhorst@math.uni-sb.de>
2024-06-05 13:10:24 +0000

    cleanup

--- src/EllCrv/NewtonLift.jl
+++ src/EllCrv/NewtonLift.jl
@@ -1,11 +1,8 @@
-import Hecke
-import Hecke.AbstractAlgebra
-
-_valuation(x) = iszero(x) ? QQ(precision(parent(x))) : valuation(x)
-
+#import Hecke.AbstractAlgebra
+export newton_lift
 add_verbosity_scope(:NewtonIteration)
 
-set_verbosity_level(:NewtonIteration, 2)
+_valuation(x::RingElem) = iszero(x) ? QQ(precision(parent(x))) : valuation(x)
 
 
 r"""
@@ -42,14 +39,33 @@ function _singular_points_weierstrass_model(E::EllipticCurve{<:AbstractAlgebra.G
   return sing
 end
 
-r"""
-    newton_lift(E::EllipticCurve, P::EllipticCurvePoint, reduction_map; max_iterations=16) -> EllipticCurvePoint
+@doc r"""
+    newton_lift(E::EllipticCurve, P::EllipticCurvePoint, reduction_map; max_iterations=16) -> Bool, EllipticCurvePoint
+
+Return a point `Q` of `E` which reduces by `reduction_map` to `P` and whether it exists.
+
+Let ``F`` be a number field and ``E`` an elliptic curve over ``F(t)`` which is integral over ``F[t]``.
+Let ``O_F`` be the maximal order of ``F`` and ``p`` be a prime ideal of ``O_F`` with residue field ``k``.
+Suppose that ``E`` is integral over ``(O_F)_p`` and that it has good reduction at ``p``.
+
+The algorithm lifts `P` by a multivariate Newton iteration to a point in the completion of ``F`` at the prime ``p``
+(up to some finite precision). The corresponding point of small height in `F` is computed and the result verified.
+
+Throws an error if `max_iterations` is reached without recognizing the point in `F`.
+Use `set_verbosity_level(:NewtonLift, 2)` for output during the computation.
+
+Input:
+- `E` -- the elliptic curve ``E/F(t)``
+- `p` -- a point of ``E/k(t)``
+- `reduction map` -- the canonical homomorphism $O_F \rightarrow k$
+- max_iterations -- the maximal number of Newton iteration steps
 """
-function newton_lift(E::EllipticCurve, P::EllipticCurvePoint, reduction_map; max_iterations::Int=16)
+function newton_lift(E::EllipticCurve{<:Generic.FracFieldElem{<:PolyRingElem}}, P::EllipticCurvePoint{<:Generic.FracFieldElem{<:PolyRingElem}}, reduction_map::Map; max_iterations::Int=16)
   # O_F -> O_F/m =: k
   # K the completion of F at m
   sing = _singular_points_weierstrass_model(E)
-
+  @req codomain(reduction_map)===base_ring(base_ring(base_field(parent(P)))) "codomain of the reduction map incompatible with the point to be lifted"
+  @req number_field(domain(reduction_map))===base_ring(base_ring(base_field(E))) "codomain of the reduction map incompatible with the point to be lifted"
   @req max_iterations <= 62 " the desired precision needs to fit into an Int64"
   prime = reduction_map.P
   F = number_field(domain(reduction_map))
@@ -69,8 +85,7 @@ function _can_reconstruct_with_reconstruction(E::EllipticCurve, padic_point, F_t
   @vprint :NewtonIteration 2 "trying rational reconstruction\n"
   K = codomain(F_to_K)
   prec0 = precision(K)
-  #setprecision!(F_to_K, prec)
-  #@show prec
+  #setprecision!(F_to_K, prec) # leads to difficult bugs
 
   xnF, ynF, dnF = [map_coefficients(x->preimage(F_to_K,K(x); small_lift=true), a) for a in padic_point]
 
@@ -87,7 +102,7 @@ function _can_reconstruct_with_reconstruction(E::EllipticCurve, padic_point, F_t
   end
 end
 
-function _newton_lift(P::EllipticCurvePoint, E::EllipticCurve, F_to_K, Ft_to_OKt, Ftok, max_iterations, sing, prime, break_condition=_can_reconstruct_with_reconstruction)
+function _newton_lift(P::EllipticCurvePoint, E::EllipticCurve, F_to_K, Ft_to_OKt, F_to_k, max_iterations, sing, prime, break_condition=_can_reconstruct_with_reconstruction)
   ainvs = a_invars(E)
   @assert P[3] == 1
   xn = P[1]
@@ -99,16 +114,15 @@ function _newton_lift(P::EllipticCurvePoint, E::EllipticCurve, F_to_K, Ft_to_OKt
   yn = numerator(yn)
   kt = parent(xn)
 
-  # find the singular points
-  # and the multiplicity that P passes through them
-  extra_points = Tuple{Vector{elem_type(F)},Int,Int}[]
-  k = codomain(Ftok)
+  # find the singular points of the Weierstrass model that P passes through
+  # this must be preserved by the lift
+  k = codomain(F_to_k)
+  F = domain(F_to_k)
+  extra_points = Vector{elem_type(F)}[]
   for S in sing
-    (x0,y0,t0) = Ftok.(S)
+    (x0,y0,t0) = F_to_k.(S)
     if evaluate(P[1], t0)==x0 && evaluate(P[2], t0)==y0
-      lx = valuation(P[1] - x0, gen(kt) - t0)
-      ly = valuation(P[2] - y0, gen(kt)-t0)
-      push!(extra_points, (S,lx,ly))
+      push!(extra_points, S)
     end
   end
 
@@ -116,7 +130,7 @@ function _newton_lift(P::EllipticCurvePoint, E::EllipticCurve, F_to_K, Ft_to_OKt
   OKt = codomain(Ft_to_OKt)
   OK = base_ring(OKt)
   F_to_OK = map_from_func(x->OK(F_to_K(x)), F, OK)
-  point = [map_coefficients(x->F_to_OK(preimage(Ftok,x)), i; parent=OKt) for i in [xn,yn,dn]]
+  point = [map_coefficients(x->F_to_OK(preimage(F_to_k,x)), i; parent=OKt) for i in [xn,yn,dn]]
 
   # setup the ring for bookkeeping
   @assert all(isone(denominator(a)) for a in ainvs) "model must be integral"
@@ -129,24 +143,22 @@ function _newton_lift(P::EllipticCurvePoint, E::EllipticCurve, F_to_K, Ft_to_OKt
   Rt,t = polynomial_ring(R,"t"; cached=false)
 
   ainvsOKt = [Ft_to_OKt(numerator(a)) for a in ainvs]
-  extra_pointsOK = [(F_to_OK.(i[1]),i[2],i[3]) for i in extra_points]
+  extra_pointsOK = [F_to_OK.(i) for i in extra_points]
 
   prec = 1
   minprec = 1
   for s in 1:max_iterations
-    # set the precision of the result
 
-    point = _newton_step(Rt, ainvsOKt, point, extra_pointsOK, prec)
-    #@show precision.(coefficients(point[1]))
-
+    can_lift, point = _newton_step(Rt, ainvsOKt, point, extra_pointsOK, prec)
+    can_lift || return false, infinity(E)
     prec = 2*prec
     s < min(max_iterations,6) && continue
     fl, result = break_condition(E, point, F_to_K, prec)
     if fl
-      return result
+      return true, result
     end
   end
-  error("nothing found")
+  error("could not reconstruct point, you can increase the number of newton iterations and try again")
 end
 
 function _compute_fn(point, ainvs)
@@ -163,20 +175,17 @@ end
 function _newton_step(Rt, ainvsOKt, point, extra_points, prec)
   @assert parent(point[1]) === parent(ainvsOKt[1])
   # increase precision to the desired output precision
-  point = [setprecision!(i, 2*prec) for i in point]
-  # make the section pass through the extra points again
-  # without modifying it modulo prime^prec
-  # we do this in order to perform the newton
-  # iteration in a linear subspace without having
-  # to write it down explicitly
-  #point = _to_hyper(point, Rt, extra_points, prec)
+  point = [setprecision(i, 2*prec) for i in point]
+
   fn = _compute_fn(point, ainvsOKt)
 
+  # check that the input has the desired precision
   @assert minimum(_valuation(i) for i in coefficients(fn);init=inf)>=prec
   @assert minimum(precision(i) for i in coefficients(fn); init=inf)>=prec
   @assert all(minimum(precision(i) for i in coefficients(a); init=inf)>=2*prec for a in ainvsOKt)
-  @assert all(minimum(precision(i) for i in coefficients(a); init=inf)>=2*prec for a in point)
   @vprint :NewtonIteration 3 "input quality: $(prec)\n"
+  @assert all(minimum(precision(i) for i in coefficients(a); init=inf)>=2*prec for a in point)
+
   R = base_ring(Rt)
   OK = base_ring(R)
   xn, yn, dn = point
@@ -204,10 +213,10 @@ function _newton_step(Rt, ainvsOKt, point, extra_points, prec)
 
   EQ0 = elem_type(R)[]
   EQc0 = elem_type(OK)[]
-  for ((x0,y0,t0),lx, ly) in extra_points
-    eqn0 = mod(xn(t0) + xt - (dn(t0)^2+2*dt*dn(t0))*x0, (t - t0))
+  for (x0,y0,t0) in extra_points
+    eqn0 = xn(t0) + xt(t0) - (dn(t0)^2+2*dt*dn(t0))*x0
     push!(equations, eqn0)
-    eqn0 = mod(yn(t0) + yt -(dn(t0)^3+3*dt*dn(t0)^2)*y0, (t - t0))
+    eqn0 = yn(t0) + yt(t0) -(dn(t0)^3+3*dt*dn(t0)^2)*y0
     push!(equations, eqn0)
   end
 
@@ -225,7 +234,9 @@ function _newton_step(Rt, ainvsOKt, point, extra_points, prec)
 
 
   EQc = -matrix(OK, length(EQc), 1, EQc)
-  v = _solve_mod(M, EQc, 2*prec)
+  fl, v = can_solve_with_solution_mod(M, EQc, 2*prec; side=:right)
+
+  fl || return false, point
 
   #@show  minimum(_valuation.(M*v - EQc))
   @assert minimum(_valuation.(M*v - EQc)) >= 2*prec
@@ -241,87 +252,26 @@ function _newton_step(Rt, ainvsOKt, point, extra_points, prec)
   dn1 = dn + dne
   point = [xn1,yn1,dn1]
   fn = _compute_fn(point, ainvsOKt)
-  prec1 =  Int(minimum(_valuation.(coefficients(fn)); init=precision(OK)))
-  prec2 = minimum([Int(minimum(precision.(coefficients(p)); init=precision(OK))) for p in point])
-  prec3 = minimum([Int(minimum(precision.(coefficients(p)); init=precision(OK))) for p in ainvsOKt])
-  prec0 = minimum([prec1,prec2,prec3])
-  @vprint :NewtonIteration 2 "output quality: $(prec0)\n"
-  @assert prec0 >= 2*prec
-  return xn1, yn1, dn1
+  prec0 =  minimum(_valuation.(coefficients(fn)); init=precision(OK))
+  prec1 =  minimum(minimum(precision.(coefficients(i)); init=precision(OK)) for i in point)
+  prec2 =  minimum(minimum(precision.(coefficients(i)); init=precision(OK)) for i in ainvsOKt)
+  prec_out = minimum([prec0,prec1,prec2])
+  @vprint :NewtonIteration 2 "output quality: $(prec_out)\n"
+  @assert prec_out >= 2*prec
+  return true, (xn1, yn1, dn1)
 end
-
-
-function _to_hyper(point, Rt, extra_points, prec)
-  xn, yn, dn = point
-  R = base_ring(Rt)
-  t = gen(Rt)
-  K = coefficient_ring(R)
-  Kt = parent(xn)
-  xd = degree(xn)
-  yd = degree(yn)
-  dd = degree(dn)
-  if length(extra_points)==0
-    return xn,yn,dn
-  end
-  @assert string(t) == "t"
-  EQ0 = elem_type(R)[]
-  EQc0 = elem_type(K)[]
-  xt = sum(gen(R, i+1)*t^i for i in 0:xd; init=Rt(0))
-  yt = sum(gen(R, i+xd+2)*t^i for i in 0:yd; init=Rt(0))
-  dt = sum(gen(R, i+xd+2+yd+1)*t^i for i in 0:dd-1; init=Rt(0))#+t^dd
-  for ((x0,y0,t0),lx, ly) in extra_points
-    eqn0 = mod(xn(t0) + xt - (dn(t0)^2+2*dt*dn(t0))*x0, (t - t0)^lx)
-    append!(EQ0, [coeff(eqn0, i) for i in 0:degree(eqn0) if coeff(eqn0, i)!=0])
-    append!(EQc0, [constant_coefficient(coeff(eqn0, i)) for i in 0:degree(eqn0) if coeff(eqn0, i)!=0])
-
-    eqn0 = mod(yn(t0) + yt -(dn(t0)^3+3*dt*dn(t0)^2)*y0, (t - t0)^ly)
-    append!(EQ0, [coeff(eqn0, i) for i in 0:degree(eqn0) if coeff(eqn0, i)!=0])
-    append!(EQc0, elem_type(K)[constant_coefficient(coeff(eqn0, i)) for i in 0:degree(eqn0) if coeff(eqn0, i)!=0])
-
-  end
-  # each equation is a row
-  n = ngens(R) - 3
-  M = zero_matrix(K, length(EQ0), n)
-  for i in 1:length(EQ0)
-    for j in 1:n
-      M[i,j] = coeff(EQ0[i],gen(R,j))
-    end
-  end
-  EQc0 = -matrix(K, length(EQc0), 1, EQc0)
-  v = _solve_mod(M, EQc0, 2*prec)
-
-  @assert minimum(_valuation.(M*v-EQc0))>=2*prec
-  @assert minimum(_valuation.(v))>=prec
-  t = gen(Kt)
-  xn1 = xn + sum(v[i+1, 1]*t^i for i in 0:xd; init=zero(Kt))
-  yn1 = yn + sum(v[i+xd+2, 1]*t^i for i in 0:yd; init=zero(Kt))
-  dn1 = dn + sum(v[i+xd+2+yd+1, 1]*t^i for i in 0:dd-1; init=zero(Kt))
-  return xn1,yn1,dn1
-end
-
-function Hecke.setprecision!(p::AbstractAlgebra.Generic.Poly{<:Hecke.LocalFieldValuationRingElem}, n::Int)
-  c = coefficient_ring(p)
-  @assert parent(coeff(p,0)) === c
-  pp = map_coefficients(x->setprecision!(x, n), p; parent=parent(p))
-  @assert parent(coeff(pp,0)) === c
-  return pp
-end
-
-#=
-function _setprecision!(M::MatrixElem, prec)
-  for x in eachindex(M)
-    M[x] = setprecision!(M[x], prec)
-  end
-  return M
-end
-=#
 
 
 """
-Solve A x = b mod p^n
-for x
+  _solve_mod(A::T, b::T, n::Int, side) where {T<: MatElem{<:Hecke.LocalFieldValuationRingElem}} -> T
+
+Let `p` be the uniformizer of the coefficient ring of `A`.
+If side = :right
+solve A x = b mod p^n for x
+If side = :left
+Solve x A  = b mod p^n for x
 """
-function _solve_mod(A::T, b::T, n::Int) where {T<: MatElem{<:Hecke.LocalFieldValuationRingElem}}
+function can_solve_with_solution_mod(A::T, b::T, n::Int; side) where {T<: MatElem{<:Hecke.LocalFieldValuationRingElem}}
   OK = base_ring(A)
   @assert base_ring(b) == OK
   pi = uniformizer(OK)
@@ -334,14 +284,12 @@ function _solve_mod(A::T, b::T, n::Int) where {T<: MatElem{<:Hecke.LocalFieldVal
   AR = OK_to_R.(A)
   bR = OK_to_R.(b)
 
-  global A1 = AR
-  global b1 = bR
-  xR = solve(AR,bR; side=:right)
+  fl, xR = can_solve_with_solution(AR,bR; side=side)
   xOK = map_entries(x-> preimage(OK_to_R,x), xR)
-
-  precb = minimum(precision.(b))
+  fl || return fl, xOK
+  # double check the computation
   bb = A*xOK
   @assert minimum(precision.(xOK)) >= n  && minimum(precision.(bb)) >= n && bb==b
-  return xOK #_setprecision!(xK, prec)
+  return fl, xOK #_setprecision!(xK, prec)
 end
 
