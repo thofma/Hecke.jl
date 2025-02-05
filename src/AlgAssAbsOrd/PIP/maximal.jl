640833267f8140110f47bd8e18dc2e3be761356f
Tommy Hofmann <thofma@gmail.com>
2025-02-05 23:44:39 +0100

    feat: fix indexing bug (#1734)

--- src/AlgAssAbsOrd/PIP/maximal.jl
+++ src/AlgAssAbsOrd/PIP/maximal.jl
@@ -52,10 +52,12 @@ function _is_principal_maximal_simple_component(a, M, side = :right)
   ZA, ZAtoA = _as_algebra_over_center(A)
 
   if !isdefined(A, :isomorphic_full_matrix_algebra) && dim(ZA) == 1
+    @assert base_ring(ZA) isa NumField{QQFieldElem}
     B = matrix_algebra(base_ring(ZA), 1)
     img = [preimage(ZAtoA, x) for x in basis(A)]
     m = matrix(base_ring(B), dim(A), dim(B), [(x.coeffs[1])/(one(A).coeffs[1]) for x in img])
-    minv = matrix([ZAtoA(x[1, 1] * one(ZA)).coeffs for x in basis(B)])
+    B1 = basis(B)[1]
+    minv = matrix([ZAtoA(B1[1, 1] * (x * one(ZA))).coeffs for x in absolute_basis(base_ring(ZA))])
     AtoB = AbsAlgAssMorGen(A, B, m, minv)
     A.isomorphic_full_matrix_algebra = B, AtoB
   end
