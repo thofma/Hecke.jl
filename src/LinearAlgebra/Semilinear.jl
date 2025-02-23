607afaefa8406c71af18a5dc3f15236147ba75f7
Tommy Hofmann <thofma@gmail.com>
2025-02-23 22:16:39 +0100

    feat: factorization for polynomial over function fields
    - implement missing squarefree factorization a la Gianni-Trager

--- src/LinearAlgebra/Semilinear.jl
+++ src/LinearAlgebra/Semilinear.jl
@@ -21,7 +21,7 @@ function absolute_frobenius_inverse(x::FinFieldElem)
   return absolute_frobenius(x, -1)
 end
 
-function absolute_frobenius_inverse(x::PolyRingElem{<:FinFieldElem})
+function absolute_frobenius_inverse(x::PolyRingElem)
   Fx = parent(x)
   F = base_ring(Fx)
   p = Int(characteristic(F))
@@ -50,6 +50,21 @@ function absolute_frobenius_inverse(x::Generic.RationalFunctionFieldElem{<:FinFi
   return parent(x)(absolute_frobenius_inverse(data(x)))
 end
 
+_elem_to_polynomial(x) = numerator(x)(gen(parent(defining_polynomial(parent(x)))))/denominator(x)
+
+function absolute_frobenius_inverse(x::Generic.FunctionFieldElem)
+  # We need to write x as a polynomial in alpha^p
+  # we have as a polynomial in alpha
+  # so lets write alpha as a polynomial in alpha^p
+  K = parent(x)
+  k = base_ring(K)
+  d = degree(K)
+  alpha = gen(K)
+  p = Int(characteristic(K))
+  alphappows =  [_elem_to_polynomial(alpha^(p*i)) for i in 0:d-1]
+  fl, xinalphap = can_solve_with_solution(matrix(k, [coeff.(f, 0:d-1) for f in alphappows]), coeff.(_elem_to_polynomial(x),0:d-1))
+  return K(absolute_frobenius_inverse.(xinalphap))
+end
 
 function _intersect_with_pspan(A::MatElem{<:Generic.RationalFunctionFieldElem{<:FinFieldElem, <:PolyRingElem{<:FinFieldElem}}})
   # F is f.g. over perfect field and we have an F-basis of derivations
