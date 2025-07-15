2f3c013c2921e0a3a178cba968e93cef63c1fd32
Claus Fieker <fieker@mathematik.uni-kl.de>
2025-07-15 11:26:22 +0200

    Fix `grunwald_wang` (#1884)
    * fix an old and embarrasing problem in the GrunwaldWang
    
    * remove old import

--- test/RCF/rcf.jl
+++ test/RCF/rcf.jl
@@ -393,3 +393,11 @@ let
   D[C] = 1
   @test haskey(D, CC)
 end
+
+@testset "Grunwald Wang" begin
+  Q = rationals_as_number_field()[1]
+  Z = maximal_order(Q)
+  @test degree(Hecke.grunwald_wang(Dict(2*Z => 2, 5*Z => 2))) == 2
+  @test degree(Hecke.grunwald_wang(Dict(2*Z => 2, 5*Z => 3))) == 6
+end
+
