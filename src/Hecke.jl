287bced8af789ccbbef73838d6bb2087446a4a17
Claus Fieker <fieker@mathematik.uni-kl.de>
2025-09-24 16:52:16 +0200

    typo

--- src/Hecke.jl
+++ src/Hecke.jl
@@ -32,7 +32,6 @@ using Pkg
 
 import AbstractAlgebra
 import AbstractAlgebra: get_cached!, @alias
-import AbstractAlgebra: xxgcd, make_exact
 
 import AbstractAlgebra: pretty, Lowercase, LowercaseOff, Indent, Dedent, terse, is_terse
 
