8a7894d826ee57fe3b492843b557beb4518b67df
Matthias Zach <zach@mathematik.uni-kl.de>
2025-09-26 22:19:00 +0200

    Add tests.

--- test/Sparse/Row.jl
+++ test/Sparse/Row.jl
@@ -32,6 +32,15 @@
   @test_throws ErrorException sparse_row(ZZ, [2, 2], [ZZRingElem(1), ZZRingElem(2)])
   @test_throws ErrorException sparse_row(ZZ, [2, 2], [ZZRingElem(1), ZZRingElem(2)])
 
+  G = @inferred sparse_row(1, one(R))
+  @test base_ring(G) === R
+  H = @inferred sparse_row(1, zero(R))
+  @test is_empty(H)
+  # incorrectly stored element at the user's responsibility
+  H = @inferred sparse_row(1, zero(R); check=false)
+  @test !is_empty(H)
+  @test all(is_zero(c) for (_, c) in H)
+
   # Equality
 
   @test @inferred ==(D, E)
