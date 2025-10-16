e7a3e4ca2bc6cecaefccfba2f7df1b94f83ef7e8
Tobias Schnieders <178478878+Tobias271828@users.noreply.github.com>
2025-10-16 15:15:38 +0200

    Improved performance by reducing the number of allocs down to 0 in the iteration step

--- src/Misc/PartitionWithCondition.jl
+++ src/Misc/PartitionWithCondition.jl
@@ -29,17 +29,22 @@
   [3, 0, 0, 4]
   ```
 """
-struct PartitionWithCondition
+mutable struct PartitionWithCondition
   n::Int
   k::Int
   l::Int
+  sum::Int
+  weighted_sum::Int
+  vect::Vector{Int}
+
   function PartitionWithCondition(n::Int, k::Int, l::Int)
     # Catch non-meaningful input
     @req n>=0 "n should be >= 0"
     @req k>0 "k should be > 0"
     @req l>=0 "l should be >= 0"
-    return new(n, k, l)
+    return new(n, k, l, 0, 0, zeros(Int, k))
   end
+
 end
 
 partition_with_condition(n::Int, k::Int, l::Int) = PartitionWithCondition(n, k, l)
@@ -48,7 +53,7 @@ function Base.iterate(parti::PartitionWithCondition)
   if parti.k == 1
     # If k is 1, the problem is trivial
     if parti.l == 0
-      return Int[parti.n], (Int[parti.n],Int[parti.n])
+      return Int[parti.n], nothing
     else
       return nothing
     end
@@ -59,43 +64,49 @@ function Base.iterate(parti::PartitionWithCondition)
   # and W = 2*a_2 + 3*a_3 + ... + (k-1)*a_{k-1}.
   firstlist = zeros(Int, parti.k-1)
   pushfirst!(firstlist, parti.n)
-  iter = (firstlist, pushfirst!(pushfirst!(firstlist[3:end], Int(0)), Int(0)))
+  #iter = (firstlist, pushfirst!(pushfirst!(firstlist[3:end], Int(0)), Int(0)))
+  parti.sum = 0
+  parti.weighted_sum = 0
   if parti.l == 0
-    return firstlist, iter
+    return firstlist, nothing
   else
-    return iterate(parti, iter)
+    return iterate(parti, nothing)
   end
 end
 
-function Base.iterate(parti::PartitionWithCondition, iter::Tuple{Vector{Int}, Vector{Int}})
+function Base.iterate(parti::PartitionWithCondition, xxx::Nothing)
   if parti.k == Int(1)
     return nothing
   end
   current_position = Int(3)
-  weighted_sum = iter[2][1]
-  sum = iter[2][2]
+  # weighted_sum = iter[2][1]
+  # sum = iter[2][2]
   # For every choice of integers a_2, a_3, ... a_{k-1} there exists a unique pair of integers
   # (a_0, a_1) fulfilling the two equations. Hence, we can iterate over all such choices and
   # calculate the corresponding pair (a_0, a_1) and check, whether a_0 and a_1 are both >= 0.
   while current_position < parti.k+1
-    sum += 1
-    weighted_sum += current_position - 1
-    @inbounds iter[2][current_position] += 1
-    a1 = parti.l - weighted_sum
+    parti.sum += 1
+    parti.weighted_sum += current_position - 1
+    # @inbounds iter[2][current_position] += 1
+    parti.vect[current_position] += 1
+    a1 = parti.l - parti.weighted_sum
     if a1 >= 0
-      a0 = parti.n - sum - a1
+      a0 = parti.n - parti.sum - a1
       if a0 >= 0
-        iter[1][3:end] = view(iter[2], 3:parti.k)
-        @inbounds iter[1][2] = a1
-        @inbounds iter[1][1] = a0
-        @inbounds iter[2][1] = weighted_sum
-        @inbounds iter[2][2] = sum
-        return iter[1], iter
+        @inbounds parti.vect[1] = a0
+        @inbounds parti.vect[2] = a1
+        # iter[1][3:end] = view(iter[2], 3:parti.k)
+        # @inbounds iter[1][2] = a1
+        # @inbounds iter[1][1] = a0
+        # @inbounds iter[2][1] = parti.weighted_sum
+        # @inbounds iter[2][2] = parti.sum
+        return parti.vect, nothing
       end
     else
-      @inbounds sum -= iter[2][current_position]
-      @inbounds weighted_sum -= (current_position - 1) * iter[2][current_position]
-      @inbounds iter[2][current_position] = 0
+      @inbounds parti.sum -= parti.vect[current_position]
+      @inbounds parti.weighted_sum -= (current_position - 1) * parti.vect[current_position]
+      # @inbounds iter[2][current_position] = 0
+      parti.vect[current_position] = 0
       current_position += 1
     end
   end
