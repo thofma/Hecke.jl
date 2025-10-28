5cdf901d6f69af36ec206bc7f4cbbbbfb8b59a41
Tobias Schnieders <178478878+Tobias271828@users.noreply.github.com>
2025-10-28 11:52:38 +0100

    Removed small mistake

--- test/RieSrf/RiemannSurface.jl
+++ test/RieSrf/RiemannSurface.jl
@@ -14,20 +14,17 @@
   RS = RiemannSurface(f, v)
   tau = small_period_matrix(RS)
   R = base_ring(tau)
-  x = R("0.5 +/- 1e-10") + R("0.86602540378443864676372317 +/- 1.91e-10")*im
-  @test contains(x, tau[1,1])
+  expected_result = R("0.5 +/- 1e-10") + R("0.86602540378443864676372317 +/- 1.91e-10")*im
+  @test contains(expected_result, tau[1,1])
 
   # another elliptic curve
-  @test_broken f = x^3 + 2*x^2*y + 3*x - 4 + 5*y^2 # Redefining Kxy helps somehow? I don't understand what is going on here ...
-  Kxy, (x,y) = polynomial_ring(K, ["x","y"])
   f = x^3 + 2*x^2*y + 3*x - 4 + 5*y^2
   RS = RiemannSurface(f, v)
   tau = small_period_matrix(RS)
-  x = R("0.4968045031 +/- 1e-10") + R("0.2904966226 +/- 1e-10")*im
-  @test contains(x, tau[1,1])
+  expected_result = R("0.4968045031 +/- 1e-10") + R("0.2904966226 +/- 1e-10")*im
+  @test contains(expected_result, tau[1,1])
 
   # ERROR: A larger example -- this throws an ArgumentError although it works perfectly fine in Magma
-  Kxy, (x,y) = polynomial_ring(K, ["x","y"])
   f = x^8 + 2 * x^7 + 2 * x^6 + x^5 - 10 * x + 1 + x^3 * y^2 - y^3 + 2 * y^8
   RS = RiemannSurface(f, v)
   @test_broken small_period_matrix(RS)
