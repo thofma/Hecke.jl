a71d512079b468f0a1077c2a3399b447992a78e6
Simon Brandhorst <brandhorst@math.uni-sb.de>
2025-10-31 22:22:34 +0100

    adapt to a bugfix for add_row! in nemo and fix the test

--- test/RieSrf/RiemannSurface.jl
+++ test/RieSrf/RiemannSurface.jl
@@ -3,19 +3,19 @@
   K = rationals_as_number_field()[1]
 
   Kxy, (x,y) = polynomial_ring(K, ["x","y"])
-  f = x^5 + x^4 + x^3 -x + 4 - y^2
+  f = x^5 + x^4 + x^3 - x + 4 - y^2
 
   v = real_places(K)[1]
   RS = RiemannSurface(f, v)
   tau = small_period_matrix(RS)
 
   # an Elliptic curve
-  f = x^3+1 - y^2
+  f = x^3 + 1 - y^2
   RS = RiemannSurface(f, v)
   tau = small_period_matrix(RS)
   R = base_ring(tau)
-  x = R("0.5 +/- 1e-10") + R("0.86602540378443864676372317 +/- 1.91e-10")*im
-  @test contains(x, tau[1,1])
+  t = R("0.5 +/- 1e-10") + R("0.86602540378443864676372317 +/- 1.91e-10")*im
+  @test contains(t, tau[1,1])
 
   # the same but different
   f = x^3-1 - y^2
