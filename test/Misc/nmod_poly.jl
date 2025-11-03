f6111f7a8949baea62acdbcdb2d54ab8a76925a2
Max Horn <max@quendi.de>
2025-11-03 13:51:41 +0100

    Fix a bug in gcd_sircana
    So far, we had:
    
        julia> using Hecke
    
        julia> R = residue_ring(ZZ,9)[1]
        Integers modulo 9
    
        julia> S, x = R[:x]
        (Univariate polynomial ring in x over R, x)
    
        julia> f = x^2 ; g = x^2-5*x+6 ;
    
        julia> Hecke.gcd_sircana(f,g)
        (x + 6, x + 3, x + 7)
    
        julia> Hecke.gcd_sircana(f,g*x)
        ERROR: Not an element of the domain
        ...
    
    The last error was due to "lifting" from Z/p to Z/p^2 "directly"
    does not work anymore. We now lift to ZZ and project down.
    
    Alas, this is only gets us to the next error. But at least it is
    some minor progress.

---
+++ test/Misc/nmod_poly.jl
@@ -0,0 +1,18 @@
+@testset "gcd_sircana" begin
+    R = residue_ring(ZZ,9)[1]
+    S, x = R[:x]
+    f = x^2
+    g = x^2-5*x+6
+
+    a, u, v = Hecke.gcd_sircana(f,g)
+    d = u*f + v*g
+    @test is_zero(rem(d, a))
+
+#=
+    g *= x
+    a, u, v = Hecke.gcd_sircana(f,g)
+    d = u*f + v*g
+    @test is_zero(rem(d, a))
+=#
+@end
+
