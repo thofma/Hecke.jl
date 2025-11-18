7a2ab24e776d27cc6db1e5c13fb5e4c5cf7ec642
JohnAAbbott <124266874+JohnAAbbott@users.noreply.github.com>
2025-11-18 14:29:50 +0100

    Update src/Deprecations.jl
    Co-authored-by: Lars Göttgens <lars.goettgens@gmail.com>

--- src/Deprecations.jl
+++ src/Deprecations.jl
@@ -7,7 +7,7 @@
 # ALL aliases here are only a temporary measure to allow for a smooth transition downstream.
 # they will be replaced by deprecations eventually
 
-#= End of list =#
+#= currently none =#
 
 ###############################################################################
 #
