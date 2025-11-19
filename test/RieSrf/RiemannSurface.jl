8ea7080fd4d91fe605f23e9c02cee70c48682dbe
JHanselman <hanselmanj@hotmail.com>
2025-11-19 12:48:30 +0100

    Adapted test

--- test/RieSrf/RiemannSurface.jl
+++ test/RieSrf/RiemannSurface.jl
@@ -1,17 +1,16 @@
 @testset "RiemannSurface" begin
   using Hecke.RiemannSurfaces
-  K = rationals_as_number_field()[1]
+  K = QQ
 
   Kxy, (x,y) = polynomial_ring(K, ["x","y"])
-  f = x^5 + x^4 + x^3 - x + 4 - y^2
 
-  v = real_places(K)[1]
+  f = x^5 + x^4 + x^3 - x + 4 - y^2
   RS = RiemannSurface(f, v)
   tau = small_period_matrix(RS)
 
   # an Elliptic curve
   f = x^3 + 1 - y^2
-  RS = RiemannSurface(f, v)
+
   tau = small_period_matrix(RS)
   R = base_ring(tau)
   t = R("0.5 +/- 1e-10") + R("0.86602540378443864676372317 +/- 1.91e-10")*im
@@ -22,7 +21,6 @@
   RS = RiemannSurface(f, v)
   small_period_matrix(RS)
 
-   Kxy, (x,y) = polynomial_ring(QQ, ["x","y"])
-   f = x^8 + 2 * x^7 + 2 * x^6 + x^5 - 10 * x + 1 + x^3 * y^2 - y^3 + 2 * y^8
+  #f = x^8 + 2 * x^7 + 2 * x^6 + x^5 - 10 * x + 1 + x^3 * y^2 - y^3 + 2 * y^8
 
 end
