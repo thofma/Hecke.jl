e30d768cadd84b57f817507d82b2145744f4d1bf
JHanselman <hanselmanj@hotmail.com>
2025-11-20 17:26:48 +0100

    Update RiemannSurface.jl

--- test/RieSrf/RiemannSurface.jl
+++ test/RieSrf/RiemannSurface.jl
@@ -5,7 +5,7 @@
   Kxy, (x,y) = polynomial_ring(K, ["x","y"])
 
   f = x^5 + x^4 + x^3 - x + 4 - y^2
-  RS = RiemannSurface(f, v)
+  RS = RiemannSurface(f)
   tau = small_period_matrix(RS)
 
   # an Elliptic curve
@@ -18,7 +18,7 @@
 
   # the same but different
   f = x^3-1 - y^2
-  RS = RiemannSurface(f, v)
+  RS = RiemannSurface(f)
   small_period_matrix(RS)
 
   #f = x^8 + 2 * x^7 + 2 * x^6 + x^5 - 10 * x + 1 + x^3 * y^2 - y^3 + 2 * y^8
