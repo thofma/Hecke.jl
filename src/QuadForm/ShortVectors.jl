bd1d45bb20e04e66f421fa372e13c2a3fc2e0316
Kazak-11 <kazakov.artem2@yandex.ru>
2026-01-12 14:56:39 +0100

    added docstring

--- src/QuadForm/ShortVectors.jl
+++ src/QuadForm/ShortVectors.jl
@@ -319,6 +319,29 @@ function short_vectors_affine(
   return _short_vectors_affine(gram, v, alpha, d)
 end
 
+@doc raw"""
+    short_vectors_affine_iterator
+        gram::MatrixElem{T},
+        v::MatrixElem{T},
+        alpha::RationalUnion,
+        d::RationalUnion
+      ) where T <: Union{ZZRingElem, QQFieldElem} -> Vector{MatrixElem{T}}
+
+Given the Gram matrix `gram` of a hyperbolic or negative definite
+$\mathbb{Z}$-lattice ``S``, return iterator that returns vectors
+
+```math
+\{x \in S : x^2=d, x.v=\alpha \}.
+```
+where ``v`` is a given vector of positive square, represented by its coordinates
+in the standard basis of the rational span of ``S``, and ``\alpha`` and ``d``
+are rational numbers.
+
+The vectors in output are given in terms of their coordinates in the rational
+span of ``S``.
+
+The implementation is based on Algorithm 2.2 in [Shi15](@cite)
+"""
 function short_vectors_affine_iterator(
     gram::MatrixElem{T},
     v::MatrixElem{T},
