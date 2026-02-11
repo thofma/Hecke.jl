9ea97d2924ac7e464dd458de7bcb9e6bb6bcc318
Berenike <BerenikeDieterle@gmail.com>
2026-02-11 10:27:38 +0100

    changed single_rows_to_top! and smaller stuff

--- src/Sparse/StructuredGauss_allTypes.jl
+++ src/Sparse/StructuredGauss_allTypes.jl
@@ -1,7 +1,18 @@
+#TODO: input/output types and return
+
+
+#=
+add_verbosity_scope(:StructGauss)
+set_verbosity_level(:StructGauss, 1)
+
+add_assertion_scope(:StructGauss)
+set_assertion_level(:StructGauss, 1)
+=#
+
 mutable struct data_StructGauss{T}
  A::SMat{T}
  Y::SMat{T}
- R::T #correct type declaration?
+ R::Ring #TODO: type declaration
  base::Int64
  single_row_limit::Int64
  nlight::Int64
@@ -14,7 +25,7 @@ mutable struct data_StructGauss{T}
  is_pivot_col::Vector{Bool}
  pivot_col::Vector{Int64} #pivot_col[i] = c >= 0 if col c has its only entry in row i, i always recent position
 
- function data_StructGauss(A::SMat{T}) where T
+ function data_StructGauss(A::SMat{T}) where T <: RingElem
   Y = sparse_matrix(base_ring(A), 0, ncols(A))
   col_list = _col_list(A)
   return new{T}(A,
@@ -38,19 +49,20 @@ end
 mutable struct data_Det
  scaling #tracks scaling -> divide det by product in the end
  divisions #tracks divisions -> multiply det by product
+ sign #tracks swaps -> multiply det by -1 or do nothing
  #content #tracks reduction of polys by content -> multiply det by prod
  pivprod #product of pivot elements
  npiv #number of pivots
  function data_Det(R, _det=false)
   if _det
-   return new(one(R), one(R), one(R), 0)
+   return new(one(R), one(R), one(R), one(R), 0)
   else
-   return new(zero(R), zero(R), zero(R), 0)
+   return new(zero(R), zero(R), zero(R), zero(R) -1)
   end
  end
 end
 
-function _col_list(A::SMat{T})::Vector{Vector{Int64}} where T
+function _col_list(A::SMat{<:RingElem})::Vector{Vector{Int64}}
  n = nrows(A)
  m = ncols(A)
  col_list = [Array{Int64}([]) for i = 1:m]
@@ -72,8 +84,8 @@ end
 #Build an upper triangular matrix for as many columns as possible compromising
 #the loss of sparsity during this process.
 
-function part_echelonize!(A::SMat{T}, _det=false; pivbound=ncols(A), trybound=ncols(A))::Tuple{data_StructGauss{ZZPolyRingElem}, data_Det} where T <: RingElem
- A = delete_zero_rows!(A)
+function part_echelonize!(A::SMat{T}, _det=false; pivbound=ncols(A), trybound=ncols(A))::Tuple{data_StructGauss{T}, data_Det} where T <: RingElem
+ A = delete_zero_rows!(A) #TODO: track number or move into base
  n = nrows(A)
  m = ncols(A)
  SG = data_StructGauss(A)
@@ -99,22 +111,18 @@ function part_echelonize!(A::SMat{T}, _det=false; pivbound=ncols(A), trybound=nc
  return SG, Det
 end
 
-function single_rows_to_top!(SG::data_StructGauss)::data_StructGauss
+#assertions removed
+function single_rows_to_top!(SG::data_StructGauss{T}, Det::data_Det)::data_StructGauss{T} where T<:RingElem
  for i = 1:nrows(SG.A)
-  len = length(SG.A[i])
-  @assert !iszero(len)
-  if len == 1
-   @assert SG.single_row_limit <=i
-   if i != SG.single_row_limit
-    swap_rows_perm(i, SG.single_row_limit, SG)
-   end
+  if length(SG.A[i]) == 1
+   swap_rows_perm(i, SG.single_row_limit, SG, Det)
    SG.single_row_limit +=1
   end
  end
  return SG
 end
 
-function build_part_ref!(SG::data_StructGauss, Det::data_Det)
+function build_part_ref!(SG::data_StructGauss{<:RingElem}, Det::data_Det)
  queue = collect(ncols(SG.A):-1:1)
  while !isempty(queue)
   queue_new = Int[]
@@ -146,6 +154,10 @@ function build_part_ref!(SG::data_StructGauss, Det::data_Det)
   queue = queue_new
  end
 end
+
+#find_best_single_row (type dependent)
+
+#TODO: rename to find_elimination_pivot_row
 
 # first prio: 1 as pivot entry
 # second prio: length of row
@@ -188,7 +200,6 @@ function find_best_single_row(SG::data_StructGauss{ZZRingElem})::Int64
  return best_single_row
 end
 
-#TODO: rename to find_elimination_pivot_row
 #prio: shortest row
 function find_best_single_row(SG::data_StructGauss{<:FieldElem})::Int64
  best_single_row = -1
@@ -201,6 +212,50 @@ function find_best_single_row(SG::data_StructGauss{<:FieldElem})::Int64
   if best_single_row < 0 || single_row_len < best_len
    best_single_row = i
    best_len = single_row_len
+  end
+ end
+ return best_single_row
+end
+
+#1st prio: 
+#2nd prio: 
+
+#TODO: optional parameters in Poly Case?
+function find_best_single_row(SG::data_StructGauss{PolyRingElem})::Int64
+ best_single_row = -1
+ best_col = NaN
+ best_val = NaN
+ best_len = -1
+ best_is_one = false
+ for i = SG.base:SG.single_row_limit-1
+  single_row = SG.A[i]
+  single_row_len = length(single_row)
+  w = SG.light_weight[i]
+  @assert w == 1
+  light_idx = find_light_entry(single_row.pos, SG.is_light_col)
+  j_light = single_row.pos[light_idx]
+  single_row_val = SG.A[i, j_light]
+  @assert length(SG.col_list[j_light]) > 1
+  is_one = isone(single_row_val)||isone(-single_row_val)
+  #TODO: look for useful equivalent in polyrings
+  if best_single_row < 0
+   best_single_row = i
+   best_col = j_light
+   best_len = single_row_len
+   best_is_one = is_one
+   best_val = single_row_val
+  elseif !best_is_one && is_one
+   best_single_row = i
+   best_col = j_light
+   best_len = single_row_len
+   best_is_one = true
+   best_val = single_row_val
+  elseif (is_one == best_is_one && single_row_len < best_len)
+   best_single_row = i
+   best_col = j_light
+   best_len = single_row_len
+   best_is_one = is_one
+   best_val = single_row_val
   end
  end
  return best_single_row
@@ -242,19 +297,19 @@ function handle_new_light_weight!(i::Int64, SG::data_StructGauss)::data_StructGa
  elseif w == 1
   #println("Case2")
   if i > SG.single_row_limit
-   swap_rows_perm(i, SG.single_row_limit, SG)
+   swap_rows_perm(i, SG.single_row_limit, SG, Det)
   end
   SG.single_row_limit += 1
  elseif SG.base <= i < SG.single_row_limit #TODO: check if necessary
   #println("Case3")
-  swap_rows_perm(i, SG.single_row_limit-1, SG)
+  swap_rows_perm(i, SG.single_row_limit-1, SG, Det)
   SG.single_row_limit-=1
  end
  return SG
 end
 
 #best_single_row = row to eliminate with (next pivot row)
-function eliminate_and_update!(best_single_row::Int64, SG::data_StructGauss{T}, Det::data_Det)::data_StructGauss{T}
+function eliminate_and_update!(best_single_row::Int64, SG::data_StructGauss, Det::data_Det)::data_StructGauss
  @assert !iszero(best_single_row)
  L_best = deepcopy(SG.col_list_perm[best_single_row]) #old index 
  light_idx = find_light_entry(SG.A[best_single_row].pos, SG.is_light_col) #idx in pos array (not col idx in matrix)
@@ -382,7 +437,7 @@ function add_to_eliminate!(L_row::Int64, row_idx::Int64, best_single_row::Int64,
  end
  return SG
 end
-=#
+
 
 ################################################################################
 #
@@ -404,30 +459,33 @@ function swap_entries(v::Vector{Int64}, i::Int64, j::Int64)
  v[i],v[j] = v[j],v[i]
 end
 
-function swap_rows_perm(i::Int64, j, SG::data_StructGauss)
+#return Det?
+function swap_rows_perm(i::Int64, j, SG::data_StructGauss{<:RingElem}, Det::data_Det)
  if i != j
   swap_rows!(SG.A, i, j)
   swap_entries(SG.pivot_col, i, j)
   swap_entries(SG.col_list_perm, i, j)
   swap_entries(SG.col_list_permi, SG.col_list_perm[i], SG.col_list_perm[j])
   swap_entries(SG.light_weight, i, j)
+  Det.sign*=-1
  end
 end 
 
 function swap_i_into_base(i::Int64, SG::data_StructGauss)
  if i < SG.single_row_limit
-  swap_rows_perm(i, SG.base, SG)
+  swap_rows_perm(i, SG.base, SG, Det)
  else
    if i != SG.single_row_limit 
-    swap_rows_perm(SG.base, SG.single_row_limit, SG)
+    swap_rows_perm(SG.base, SG.single_row_limit, SG, Det)
    end
    SG.single_row_limit +=1
-   swap_rows_perm(SG.base, i, SG)
+   swap_rows_perm(SG.base, i, SG, Det)
  end
 end
 
+#returns index of light entry in position_array
 function find_light_entry(position_array::Vector{Int64}, is_light_col::Vector{Bool})::Int64
- position_array[findfirst(is_light_col)]
+ return findfirst(x->is_light_col[x], position_array)
 end
 
 function move_into_Y(i::Int64, SG::data_StructGauss)
