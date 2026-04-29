df0ecb7b51859df2ef987ac8b430d4c1638f573b
Alexey Orlov <alexey.orlov@posteo.net>
2026-04-29 11:28:15 +0200

    FunField: drop redundant outer testset wrapper in test entry point

--- test/FunField.jl
+++ test/FunField.jl
@@ -1,9 +1,6 @@
-@testset "Function fields" begin
-  include("FunField/DegreeLocalization.jl")
-  include("FunField/Divisor.jl")
-  include("FunField/Differential.jl")
-  include("FunField/Factor.jl")
-  include("FunField/HessQR.jl")
-  include("FunField/IntClsZx.jl")
-end
-
+include("FunField/DegreeLocalization.jl")
+include("FunField/Divisor.jl")
+include("FunField/Differential.jl")
+include("FunField/Factor.jl")
+include("FunField/HessQR.jl")
+include("FunField/IntClsZx.jl")
