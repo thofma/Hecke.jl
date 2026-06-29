3e2e8988aaac4a0b039f01a8ae147d919365e2da
Morgan Rodgers <morgan.joaquin@gmail.com>
2026-06-29 15:29:13 +0200

    revert overly zealous removal of commented code

--- src/Map/MapType.jl
+++ src/Map/MapType.jl
@@ -11,6 +11,15 @@ preimage_function(f) = a -> preimage(f, a)
 image_function(f) = a -> image(f, a)
 
 
+#function _compose(f, g)
+#  return AbstractAlgebra.Generic.compose(g, f)
+#end
+
+#function compose(f::Map(HeckeMap){R, C}, g::Map(HeckeMap){D, R}) where {D, C, R}
+#  return CompositeMap{D, C, R}(f, g)
+#end
+
+
 mutable struct InverseMap{D, C} <: Map{D, C, HeckeMap, InverseMap}
   header::MapHeader{D, C}
   origin::Map{C, D}
