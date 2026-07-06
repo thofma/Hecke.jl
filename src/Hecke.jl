ac3389092c44a9ecf35c92ef65d0fd7b407e6c37
Morgan Rodgers <morgan.joaquin@gmail.com>
2026-07-06 11:08:51 +0200

    no longer need to exclude AA `map_from_func` from imports

--- src/Hecke.jl
+++ src/Hecke.jl
@@ -72,7 +72,7 @@ import Pkg
 
 exclude = [:Nemo, :AbstractAlgebra, :zz, :qq, :call,
            :factors, :parseint, :strongequal, :window, :xgcd, :rows, :cols,
-           :set_entry!, :RDF, :map_from_func]
+           :set_entry!, :RDF]
 
 for i in names(Nemo)
   (i in exclude || !isdefined(Nemo, i)) && continue
