1d0ae3eaa5cb63bfa44b773377050dae4a98de1a
JHanselman <hanselmanj@hotmail.com>
2026-07-13 01:21:26 +0200

    Adds Weierstrass places and gap numbers

---
+++ test/FunField/WeierstrassPlaces.jl
@@ -0,0 +1,29 @@
+@testset "Gap numbers and Weierstrass places" begin
+  kx, x = rational_function_field(GF(49), :x)
+  kxy, y = kx[:y]
+  F, a = function_field(y^7 + y - x^4)
+  Ofin = finite_maximal_order(F)
+  p1, _ = first(factor(ideal(Ofin, x-1)))
+  D = divisor(p1)
+
+  gaps, R = Hecke._gaps_and_ramification_divisor(D, false)
+  @test gaps == [1, 2, 3, 4, 5, 8, 9, 10]
+  @test degree(R) == 664
+
+  kx, x = rational_function_field(QQ, :x)
+  kxy, y = kx[:y]
+  F, a = function_field(y^2 - x^7 - 1)
+  Ofin = finite_maximal_order(F, a)
+  Oinf = infinite_maximal_order(F, a)
+
+
+  @test gap_numbers(F) == [1, 2, 3]
+  Ps = weierstrass_places(F)
+  test_Ps = [ideal(Ofin, x+1, Ofin(a)),
+    ideal(Ofin, x^6 - x^5 + x^4 - x^3 + x^2 - x + 1, Ofin(a)), 
+    ideal(Oinf, (1//x), Oinf(1//x^4*a))]
+  for P in Ps
+    @test P in test_Ps
+  end
+
+end
