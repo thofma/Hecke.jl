782d743ac2122013e52d89708f32e5f1eee76bab
JHanselman <hanselmanj@hotmail.com>
2026-07-13 14:15:49 +0200

    Tests

--- test/FunField/WeierstrassPlaces.jl
+++ test/FunField/WeierstrassPlaces.jl
@@ -6,12 +6,10 @@
   p1, _ = first(factor(ideal(Ofin, x-1)))
   D = divisor(p1)
 
-  @test gaps_numbers(F) == [1, 2, 3, 4, 5, 8, 9, 10, 15]
+  @test gap_numbers(F) == [1, 2, 3, 4, 5, 8, 9, 10, 15]
   @test degree(ramification_divisor(F)) == 912
-  @test gaps == [1, 2, 3, 4, 5, 8, 9, 10]
-  @test degree(R) == 664
 
-  gaps, R = Hecke._gaps_and_ramification_divisor(D, false)
+  gaps, R = @inferred Hecke._gaps_and_ramification_divisor(D, false)
   @test gaps == [1, 2, 3, 4, 5, 8, 9, 10]
   @test degree(R) == 664
 
@@ -23,7 +21,7 @@
 
 
   @test gap_numbers(F) == [1, 2, 3]
-  Ps = @inferred weierstrass_places(F)
+  Ps = weierstrass_places(F)
   test_Ps = [ideal(Ofin, x+1, Ofin(a)),
     ideal(Ofin, x^6 - x^5 + x^4 - x^3 + x^2 - x + 1, Ofin(a)), 
     ideal(Oinf, (1//x), Oinf(1//x^4*a))]
@@ -32,8 +30,8 @@
   end
 
   P = test_Ps[1]
-  @test gap_numbers(P) == [1,2]
+  @test gap_numbers(divisor(P)) == [1,2]
   P = test_Ps[2]
-  @test gap_numbers(P) == []
-  @inferred weierstrass_places(F) == []
+  @test gap_numbers(divisor(P)) == []
+  weierstrass_places(divisor(P)) == []
 end
