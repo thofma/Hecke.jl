78e5c0b778183b3297d5ed89cae28086c001f411
Simon Brandhorst <brandhorst@math.uni-sb.de>
2026-07-15 16:46:53 +0200

    fixfix test

--- test/QuadForm/OrbitsMod2.jl
+++ test/QuadForm/OrbitsMod2.jl
@@ -15,16 +15,14 @@
     F2 = GF(2)
     GZ = [ZZ.(i) for i in automorphism_group_generators(L)]
     GF2 = [F2.(i) for i in GZ]
-    GF2_grp = matrix_group(GF2)
-    ord = order(GF2_grp)
     orb_len2 = sort!(first.(Hecke.line_orbits_mod_2(UInt32, GZ)))[2:end]
     orb_len1 = [i[2] for i in Hecke._line_orbits(GF2)]
     sort!(orb_len1)
     return orb_len1 == orb_len2
   end
-  LL = [integer_lattice(gram=matrix(ZZ,i)) for i in [[2 0 1 -1 -1 0; 0 4 2 0 0 -1; 1 2 5 -2 -2 -1; -1 0 -2 4 0 0; -1 0 -2 0 5 2; 0 -1 -1 0 2 7], [2 0 0 0 -1 1; 0 2 1 0 -1 1; 0 1 3 1 0 0; 0 0 1 4 2 -1; -1 -1 0 2 5 -2; 1 1 0 -1 -2 16], [532 265 527 1 530 1054; 265 133 264 0 265 527; 527 264 528 0 527 1054; 1 0 0 1 0 0; 530 265 527 0 530 1054; 1054 527 1054 0 1054 2108], [1 0 0 0 0 0; 0 2 0 1 1 1; 0 0 2 1 -1 0; 0 1 1 4 0 2; 0 1 -1 0 9 -1; 0 1 0 2 -1 18], [1 0 0 0 0 0; 0 2 0 1 0 0; 0 0 2 1 0 1; 0 1 1 4 0 2; 0 0 0 0 9 1; 0 0 1 2 1 16]]]
+  LL = [integer_lattice(gram=matrix(QQ,i)) for i in [[2 0 1 -1 -1 0; 0 4 2 0 0 -1; 1 2 5 -2 -2 -1; -1 0 -2 4 0 0; -1 0 -2 0 5 2; 0 -1 -1 0 2 7], [2 0 0 0 -1 1; 0 2 1 0 -1 1; 0 1 3 1 0 0; 0 0 1 4 2 -1; -1 -1 0 2 5 -2; 1 1 0 -1 -2 16], [532 265 527 1 530 1054; 265 133 264 0 265 527; 527 264 528 0 527 1054; 1 0 0 1 0 0; 530 265 527 0 530 1054; 1054 527 1054 0 1054 2108], [1 0 0 0 0 0; 0 2 0 1 1 1; 0 0 2 1 -1 0; 0 1 1 4 0 2; 0 1 -1 0 9 -1; 0 1 0 2 -1 18], [1 0 0 0 0 0; 0 2 0 1 0 0; 0 0 2 1 0 1; 0 1 1 4 0 2; 0 0 0 0 9 1; 0 0 1 2 1 16]]]
   for L in LL
-    @test test_mod2_orbs(L)
+    @test test_mod2_line_orbits(L)
   end
 end
 
@@ -53,10 +51,9 @@ end
 
   F = GF(2)
   grp = [F[0 1 0 0; 0 0 1 0; 0 0 0 1; 1 0 0 0], F[0 1 0 0; 1 0 0 0; 0 0 1 0; 0 0 0 1]]  #S_4
-  Hecke.orbit_representatives_and_stabilizers_mod_2(grp,2)
-  Hecke.orbit_representatives_and_stabilizers_mod_2(grp,2; group_order=factorial(4))
-
-
+  a = Hecke.orbit_representatives_and_stabilizers_mod_2(grp,2)
+  b = Hecke.orbit_representatives_and_stabilizers_mod_2(grp,2; group_order=factorial(4))
+  @test sort!([i[2] for i in a]) == sort!([i[2] for i in b]) == [3, 4, 4, 6, 6, 12] # same orbit sizes
 
 
 
