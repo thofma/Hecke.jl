275105fcc7f77bf89e03684dc3b169b63f0319c1
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-09-07 10:06:01 +0200

    Clean up old GAP workaround

--- ext/GAPExt/meataxe.jl
+++ ext/GAPExt/meataxe.jl
@@ -82,20 +82,7 @@ function _ring_iso_oscar_gap(F::T) where T <: Union{Nemo.fqPolyRepField, Nemo.Fq
    end
 
    # compute matching bases of both fields
-   if GAP.Globals.IsAlgebraicExtension(G)
-#FIXME:
-# As soon as the problem from https://github.com/gap-system/gap/issues/4694
-# is fixed, go back to `Basis_G = GAP.Globals.Basis(G)` also in this case.
-# Note that the above `GF` call delegates to `FieldExtension`,
-# and we want a basis in the filter `IsCanonicalBasisAlgebraicExtension`.
-      Basis_G = GAP.Globals.Objectify(GAP.Globals.NewType(
-                GAP.Globals.FamilyObj(G),
-                GAP.Globals.IsCanonicalBasisAlgebraicExtension),
-                GAP.NewPrecord(0))
-      GAP.Globals.SetUnderlyingLeftModule(Basis_G, G)
-   else
-      Basis_G = GAP.Globals.Basis(G)
-   end
+   Basis_G = GAP.Globals.Basis(G)
    Basis_F = Vector{elem_type(F)}(undef, d)
    Basis_F[1] = F(1)
    for i = 2:d
