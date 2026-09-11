d883d743c202301f0408b3d129845b1c4b226c6c
Simon Brandhorst <brandhorst@math.uni-sb.de>
2026-09-11 14:11:57 +0200

    forward keyword arguments from representatives(::ZZGenus) to enumerate_definite_genus

--- src/QuadForm/IntegerLattices/ZGenus.jl
+++ src/QuadForm/IntegerLattices/ZGenus.jl
@@ -2160,9 +2160,9 @@ is_definite(G::ZZGenus) = any(is_zero, signature_pair(G))
 
 Return a list of representatives of the isometry classes in this genus.
 """
-@attr Vector{ZZLat} function representatives(G::ZZGenus)
+@attr Vector{ZZLat} function representatives(G::ZZGenus; kwargs...)
   L = representative(G)
-  rep = genus_representatives(L)
+  rep = genus_representatives(L; kwargs...)
   @hassert :Lattice 2 !is_definite(G) || mass(G) == sum(QQFieldElem[1//automorphism_group_order(S) for S in rep]; init=QQ(0))
   return rep
 end
