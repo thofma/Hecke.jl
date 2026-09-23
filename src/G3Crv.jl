aaf16409cdcc52a4785a7d8d2289d595453c8fcb
JHanselman <hanselmanj@hotmail.com>
2026-09-23 07:11:56 +0200

    feat: invariants for G3/G4 curves and reconstruction (#2300)

---
+++ src/G3Crv.jl
@@ -0,0 +1,3 @@
+include("G3Crv/Invariants.jl")
+include("G3Crv/ReconstructionData.jl")
+include("G3Crv/G3Rec.jl")
---
+++ test/G4Crv.jl
@@ -0,0 +1,3 @@
+@testset "Genus 4 curves" begin
+  include("G4Crv/Invariants.jl")
+end
